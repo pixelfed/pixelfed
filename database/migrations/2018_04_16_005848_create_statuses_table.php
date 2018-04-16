7221f44e4a3376c79eb0f54c7b96f2bc438ca4bc
Daniel Supernault <danielsupernault@gmail.com>
2018-04-15 19:18:59 -0600

    Add status model, migration, controller

---
+++ database/migrations/2018_04_16_005848_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('profile_id')->nullable();
+            $table->string('caption')->nullable();
+            $table->boolean('is_nsfw')->default(false);          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
