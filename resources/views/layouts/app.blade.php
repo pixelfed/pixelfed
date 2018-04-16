411d4b9ab99ca307863d4c09c1ad4c2766bb3107
Daniel Supernault <danielsupernault@gmail.com>
2018-04-15 19:55:24 -0600

    Update app blade template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
+    @stack('meta')
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @stack('styles')
+
 </head>
 <body>
     <div id="app">
@@ -71,5 +64,7 @@
             @yield('content')
         </main>
     </div>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+    @stack('scripts')
 </body>
 </html>
