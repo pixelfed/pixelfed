4694382ed8f1763362ada397aee79efeb79e0a00
Daniel Supernault <danielsupernault@gmail.com>
2018-04-16 19:25:05 -0600

    Update personal timeline blade template

--- resources/views/timeline/personal.blade.php
+++ resources/views/timeline/personal.blade.php
@@ -4,22 +4,39 @@
 
 <div class="container">
   <div class="col-12 col-md-6 offset-md-3">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+    @endif
     
     <div class="card">
       <div class="card-header font-weight-bold">New Status Post</div>
       <div class="card-body">
-        <form>
+        <form method="post" action="/timeline" enctype="multipart/form-data">
+          @csrf
           <div class="form-group">
             <label class="font-weight-bold text-muted small">Upload Image</label>
-            <input type="file" class="form-control-file">
+            <input type="file" class="form-control-file" name="photo">
           </div>
           <div class="form-group">
             <label class="font-weight-bold text-muted small">Caption</label>
-            <input type="text" class="form-control" placeholder="Add a caption here">
+            <input type="text" class="form-control" name="caption" placeholder="Add a caption here">
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>  
+    </div>
+
+    <div class="timeline-feed my-5" data-timeline="personal">
+  @foreach($timeline as $item)
+    @include('status.template')
+
+  @endforeach
     </div>
 
   </div>
