df45744daec051d8996091a3bdc7703f3d535af4
Daniel Supernault <danielsupernault@gmail.com>
2018-04-17 20:17:30 -0600

    Add follower model, migration, controller

--- app/Follower.php
+++ app/Follower.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class Follower extends Model
 {
     //
 }
---
+++ database/migrations/2018_04_18_021047_create_followers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('profile_id')->unsigned();
+            $table->bigInteger('following_id')->unsigned();
+            $table->unique(['profile_id', 'following_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
