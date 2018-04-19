184097893e22144846b5c3ddc6683b945020a36a
Daniel Supernault <danielsupernault@gmail.com>
2018-04-18 23:56:33 -0600

    Update ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\{Profile};
 
 class ProfileController extends Controller
 {
-    //
+    public function show(Request $request, $username)
+    {
+      $user = Profile::whereUsername($username)->firstOrFail();
+      $timeline = $user->statuses()->orderBy('id','desc')->paginate(10);
+      return view('profile.show', compact('user', 'timeline'));
+    }
 }
