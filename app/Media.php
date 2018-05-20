cb0780898e35a4e3c6616db5b1f3cb3833518b77
Daniel Supernault <danielsupernault@gmail.com>
2018-05-19 20:59:59 -0600

    Add url() method to Media model

--- app/Media.php
+++ app/Media.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class Media extends Model
 {
-    //
+    public function url()
+    {
+      $path = $this->media_path;
+      $url = Storage::url($path);
+      return url($url);
+    }
 }
