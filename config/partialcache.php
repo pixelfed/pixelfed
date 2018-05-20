950fddf9e3cfd3c04c1e9636b60fe57c23c75020
Daniel Supernault <danielsupernault@gmail.com>
2018-05-19 21:06:07 -0600

    Add partial cache config

---
+++ config/partialcache.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    // Enable or disable partialcache alltogether
+    'enabled' => true,
+
+    // The name of the blade directive to register
+    'directive' => 'cache',
+
+    // The base key that used for cache items
+    'key' => 'partialcache',
+
+    // The default cache duration in minutes, set null to remember forever 
+    'default_duration' => null
+
+];
