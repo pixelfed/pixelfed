4c75aa04a2d1ded249834fd8317957bc741f9aab
Daniel Supernault <danielsupernault@gmail.com>
2018-05-20 15:51:11 -0600

    Add Avatar model/migration

---
+++ database/migrations/2018_05_07_021835_create_avatars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAvatarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avatars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('profile_id')->unsigned()->unique()->index();
+            $table->string('media_path')->nullable();
+            $table->string('thumb_path')->nullable();
+            $table->unsignedInteger('change_count')->default(0);
+            $table->timestamp('last_processed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avatars');
+    }
+}
