76f6b7ceaccf4ec6ea5146dd7e71a52c7dabcf35
Daniel Supernault <danielsupernault@gmail.com>
2018-05-19 21:15:45 -0600

    Update webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .js('resources/assets/js/timeline.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .version();
