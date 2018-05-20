05df1c6a5fbc6b506b6b49be899b00e41b27ec4f
Daniel Supernault <danielsupernault@gmail.com>
2018-05-19 20:58:45 -0600

    Add admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'admin' => \App\Http\Middleware\Admin::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth, Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check() == false || Auth::user()->is_admin == false) {
+            return redirect(config('app.url'));
+        }
+
+        return $next($request);
+    }
+}
