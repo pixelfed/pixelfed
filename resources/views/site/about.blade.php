8631669dc32f0b8fcf211a95b9913b7e30a088e9
Daniel Supernault <danielsupernault@gmail.com>
2018-05-26 16:40:52 -0600

    Add about view

---
+++ resources/views/site/about.blade.php
@@ -0,0 +1,12 @@
+@extends('site.partial.template')
+
+@section('section')
+
+  <div class="title">
+    <h3 class="font-weight-bold">About</h3>
+  </div>
+  <hr>
+  <section>
+    <p class="lead">PixelFed is a federated image sharing platform, powered by the <a href="#">ActivityPub</a> protocol.</p>
+  </section>
+@endsection
