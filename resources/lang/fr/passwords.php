686cec073fa07494612f9858b82847a893bdc003
OpenAlgeria <39639882+OpenAlgeria@users.noreply.github.com>
2018-05-26 22:14:35 +0200

    Translate French

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Les mots de passe doivent avoir au moins six caractères et correspondre à la confirmation.',
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous avons envoyé par e-mail votre lien de réinitialisation de mot de passe!',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne pouvons pas trouver un utilisateur avec cette adresse e-mail.",
+];
