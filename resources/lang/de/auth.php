1d83c857ac217f471d1e72f0fc4250d66b387d6f
timmyRS <github@timmyrs.de>
2018-05-26 22:18:47 +0200

    Add German Translation

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Diese Daten stimmen nicht mit unserem Register &uuml;berein.',
+    'throttle' => 'Zu viele Login-Versuche. Versuche es in :seconds Sekunden erneut.',
+
+];
