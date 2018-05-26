e52f81fc2a22c5fe55433319f9fcebaaa90a91c4
Daniel Supernault <danielsupernault@gmail.com>
2018-05-26 16:58:22 -0600

    Add Notification model/migration

---
+++ database/migrations/2018_05_07_025743_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('profile_id')->unsigned()->index();
+            $table->bigInteger('actor_id')->unsigned()->nullable();
+            $table->string('action')->nullable();
+            $table->string('message')->nullable();
+            $table->string('rendered')->nullable();
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
