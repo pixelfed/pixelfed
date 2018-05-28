c6a602e32408e660f1812321d82f1c7abc527278
Daniel Supernault <danielsupernault@gmail.com>
2018-05-28 17:48:59 -0600

    Update pixelfed.php config

--- config/pixelfed.php
+++ config/pixelfed.php
@@ -2,19 +2,74 @@
 
 return [
 
-  'version' => '0.1.0',
+    /*
+    |--------------------------------------------------------------------------
+    | Domains
+    |--------------------------------------------------------------------------
+    |
+    | Application domains used for routing
+    |
+    */
+    'domain' => [
+      'admin' => env('ADMIN_DOMAIN'),
+      'app' => env('APP_DOMAIN'),
+    ],
 
-  'nodeinfo' => [
-    'url' => config('app.url') . '/' . 'api/nodeinfo/2.0.json'
-  ],
+    /*
+    |--------------------------------------------------------------------------
+    | PixelFed Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your PixelFed instance.
+    |
+    */
+    'version' => '0.1.0',
 
-  'memory_limit' => '1024M',
+    /*
+    |--------------------------------------------------------------------------
+    | NodeInfo Route Path
+    |--------------------------------------------------------------------------
+    |
+    | Do not change this value unless you know what you are doing.
+    |
+    */
+    'nodeinfo' => [
+      'url' => config('app.url') . '/' . 'api/nodeinfo/2.0.json'
+    ],
 
-  'restricted_names' => [
-    'reserved_routes' => true,
-    'use_blacklist' => false
-  ],
-  
-  'open_registration' => env('OPEN_REGISTRATION', true),
-  
+    /*
+    |--------------------------------------------------------------------------
+    | PHP/ImageMagic/GD Memory Limit
+    |--------------------------------------------------------------------------
+    |
+    | This memory_limit value is only used for image processing. The
+    | default memory_limit php.ini is used for the rest of the app.
+    |
+    */
+    'memory_limit' => '1024M',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Restricted Usernames
+    |--------------------------------------------------------------------------
+    |
+    | Optional blacklist to prevent registering usernames that could
+    | be confused for admin or system services.
+    |
+    */
+    'restricted_names' => [
+      'reserved_routes' => true,
+      'use_blacklist' => false
+    ],
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Allow New Registrations
+    |--------------------------------------------------------------------------
+    |
+    | Enable/disable new local account registrations.
+    |
+    */
+    'open_registration' => env('OPEN_REGISTRATION', true),
+    
 ];
