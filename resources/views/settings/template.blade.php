870ac92a0a8ff686da8faa7296f1f8f6f5f4a2ce
Daniel Supernault <danielsupernault@gmail.com>
2018-05-29 20:35:31 -0600

    Update settings blade template

--- resources/views/settings/template.blade.php
+++ resources/views/settings/template.blade.php
@@ -14,6 +14,15 @@
                     {{ session('status') }}
                 </div>
             @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             @yield('section')
           </div>
         </div>
