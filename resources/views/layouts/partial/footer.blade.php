c2f1af51b268361b4d32ac9fc12f41e2a829f386
Daniel Supernault <danielsupernault@gmail.com>
2018-05-29 20:42:46 -0600

    Add footer view

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,17 @@
+  <footer class="pt-5 mt-5">
+    <div class="container mt-5 pt-5">
+        <p class="mt-5 text-uppercase font-weight-bold small">
+          <a href="{{route('site.about')}}" class="text-primary pr-2">About Us</a>
+          <a href="{{route('site.help')}}" class="text-primary pr-2">Support</a>
+          <a href="" class="text-primary pr-2">API</a>
+          <a href="{{route('site.opensource')}}" class="text-primary pr-2">Open Source</a>
+          <a href="{{route('site.privacy')}}" class="text-primary pr-2">Privacy</a>
+          <a href="{{route('site.terms')}}" class="text-primary pr-2">Terms</a>
+          <a href="#" class="text-primary pr-2">Directory</a>
+          <a href="#" class="text-primary pr-2">Profiles</a>
+          <a href="#" class="text-primary pr-2">Hashtags</a>
+          <a href="#" class="text-primary ">Language</a>
+          <a href="#" class="text-dark float-right">© {{date('Y')}} PixelFed.org</a>
+        </p>
+    </div>
+  </footer>
