f1c1c9e6cc6a835df110fbe897c14665788c5e46
Daniel Supernault <danielsupernault@gmail.com>
2018-05-29 20:30:39 -0600

    Update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,10 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>PixelFed</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .subtitle {
-                font-size: 2rem;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    PixelFed
-                </div>
-                <p class="subtitle">Federated Image Sharing</p>
-
-                <div class="links">
-                    <a href="#">My Timeline</a>
-                    <a href="#">Local Timeline</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+    <div class="jumbotron jumbotron-fluid bg-pixelfed text-white my-0">
+      <div class="container text-center my-5 py-5">
+        <h1 class="display-4">Federated Image Sharing</h1>
+        <p class="lead">Powered by <a href="https://en.wikipedia.org/wiki/ActivityPub" class="text-white font-weight-bold">ActivityPub</a> and the <a href="https://en.wikipedia.org/wiki/Fediverse" class="text-white font-weight-bold">fediverse</a>.</p>
+      </div>
+    </div>
+@endsection
