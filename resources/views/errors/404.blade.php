42897aab14a753224ab7bc224e9d69ce2e9de62d
Daniel Supernault <danielsupernault@gmail.com>
2018-05-31 15:59:05 -0600

    Add error views

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="error-page py-5 my-5">
+    <div class="card mx-5">
+      <div class="card-body p-5">
+        <h1 class="text-center">404 - Page Not Found</h1>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="error-page py-5 my-5">
+    <div class="card mx-5">
+      <div class="card-body p-5 text-center">
+        <h1>503 - Service Unavailable</h1>
+        <p class="lead mb-0">Our services are overloaded at the moment, please try again later.</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
