b2362776ccd490f4fdb10dd81c6d6a6b5afb950f
Daniel Supernault <danielsupernault@gmail.com>
2018-05-31 21:23:00 -0600

    Add report views

---
+++ resources/views/report/not-interested.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container mt-4 mb-5 pb-5">
+  <div class="col-12 col-md-8 offset-md-2">
+    <div class="card">
+      <div class="card-header lead font-weight-bold">
+        I'm not interested in this content
+      </div>
+      <div class="card-body">
+        <div class="p-5 text-center">
+          <p class="lead">You can <b class="font-weight-bold">unfollow</b> or <b class="font-weight-bold">mute</b> a user or hashtag from appearing in your timeline. Unless the content violates our terms of service, there is nothing we can do to remove it.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
