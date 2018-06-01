b961ed1adab28d156e37514a71b3f47d545328b6
bsky <git@bsky.moe>
2018-06-02 04:28:37 +0900

    Add ja translation of passwords.php

--- resources/lang/ja/passwords.php
+++ resources/lang/ja/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'パスワードは6文字以上で、確認と一致している必要があります。',
+    'reset' => 'パスワードをリセットしました！',
+    'sent' => 'パスワードリセットのためのリンクをメールで送信しました！',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => "このメールアドレスと一致するユーザーを見つけることが出来ません。",
 
 ];
