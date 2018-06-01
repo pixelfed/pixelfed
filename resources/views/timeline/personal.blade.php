5ac4d6f42392ef119eb212e97b7c6be2229e4a9e
Stasiek Michalski <hellcp@opensuse.org>
2018-06-01 10:57:06 +0200

    Add HTML for modal timeline

--- resources/views/timeline/personal.blade.php
+++ resources/views/timeline/personal.blade.php
@@ -17,22 +17,34 @@
           </ul>
       </div>
     @endif
-    <div class="card">
-      <div class="card-header font-weight-bold">New Post</div>
-      <div class="card-body" id="statusForm">
-        <form method="post" action="/timeline" enctype="multipart/form-data">
-          @csrf
-          <div class="form-group">
-            <label class="font-weight-bold text-muted small">Upload Image</label>
-            <input type="file" class="form-control-file" name="photo">
-          </div>
-          <div class="form-group">
-            <label class="font-weight-bold text-muted small">Caption</label>
-            <input type="text" class="form-control" name="caption" placeholder="Add a caption here">
-          </div>
-          <button type="submit" class="btn btn-outline-primary btn-block">Post</button>
-        </form>
-      </div>  
+    <div id="modal-text" class="d-none">
+      <div class="d-flex justify-content-between align-items-center">
+        <h4>Timeline</h4>
+        <button type="button" class="btn btn-link text-dark" data-toggle="modal" data-target="#modal-post">
+          <span class="icon-plus"></span>
+        </button>
+      </div>
+    </div>
+    <div tabindex="-1" role="dialog" id="modal-post" aria-hidden="true">
+      <div role="document" id="modal-dialog">
+        <div class="card">
+          <div class="card-header font-weight-bold">New Post</div>
+          <div class="card-body" id="statusForm">
+            <form method="post" action="/timeline" enctype="multipart/form-data">
+              @csrf
+              <div class="form-group">
+                <label class="font-weight-bold text-muted small">Upload Image</label>
+                <input type="file" class="form-control-file" name="photo">
+              </div>
+              <div class="form-group">
+                <label class="font-weight-bold text-muted small">Caption</label>
+                <input type="text" class="form-control" name="caption" placeholder="Add a caption here">
+              </div>
+              <button type="submit" class="btn btn-outline-primary btn-block">Post</button>
+            </form>
+          </div>  
+        </div>
+      </div>
     </div>
 
     <div class="timeline-feed my-5" data-timeline="personal">
