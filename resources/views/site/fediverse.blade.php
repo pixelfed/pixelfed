b222186527c64431f64166b5e42eafa18dc8b5f4
Daniel Supernault <danielsupernault@gmail.com>
2018-05-31 21:23:43 -0600

    Update fediverse view

--- resources/views/site/fediverse.blade.php
+++ resources/views/site/fediverse.blade.php
@@ -6,4 +6,12 @@
     <h3 class="font-weight-bold">Fediverse</h3>
   </div>
   <hr>
+  <section>
+    <p class="lead">Fediverse is a portmanteau of "federation" and "universe". It is a common, informal name for a somewhat broad federation of social network servers.</p>
+    <p class="lead font-weight-bold text-muted mt-4">Supported Fediverse Projects</p>
+    <ul class="lead pl-4">
+      <li>Mastodon - A federated twitter alternative.</li>
+      <li>Pleroma - A federated twitter alternative.</li>
+    </ul>
+  </section>
 @endsection
