2a08d0a2e7a6b7c2ed62eaa393274e720cdfdf4a
bsky <git@bsky.moe>
2018-06-02 04:06:29 +0900

    Add ja translation of auth.php

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'これらの認証情報は正しくありません。',
+    'throttle' => 'ログイン試行回数が多すぎます。 :seconds 秒後にもう一度お試しください。',
 
 ];
