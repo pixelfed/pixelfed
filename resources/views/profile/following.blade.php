412c3c81225a771cc67476a643e4f55486aad9f4
Stasiek Michalski <hellcp@opensuse.org>
2018-06-03 18:21:54 +0200

    Adapt following to partial

--- resources/views/profile/following.blade.php
+++ resources/views/profile/following.blade.php
@@ -2,46 +2,9 @@
 
 @section('content')
 
+@include('profile.partial.user-info')
+
 <div class="container following-page" style="min-height: 60vh;">
-
-  <div class="profile-header row my-5">
-    <div class="col-12 col-md-4 d-flex">
-      <div class="profile-avatar mx-auto">
-        <img class="img-thumbnail" src="{{$profile->avatarUrl()}}" style="border-radius:100%;" width="172px">
-      </div>
-    </div>
-    <div class="col-12 col-md-8 d-flex align-items-center">
-      <div class="profile-details">
-        <div class="username-bar pb-2  d-flex align-items-center">
-          <span class="font-weight-ultralight h1">{{$profile->username}}</span>
-        </div>
-        <div class="profile-stats pb-3 d-inline-flex lead">
-          <div class="font-weight-light pr-5">
-            <a class="text-dark" href="{{$profile->url()}}">
-              <span class="font-weight-bold">{{$profile->statuses()->whereNull('in_reply_to_id')->count()}}</span> 
-              Posts
-            </a>
-          </div>
-          <div class="font-weight-light pr-5">
-            <a class="text-dark" href="{{$profile->url('/followers')}}">
-              <span class="font-weight-bold">{{$profile->followerCount(true)}}</span> 
-              Followers
-            </a>
-          </div>
-          <div class="font-weight-light pr-5">
-            <a class="text-dark" href="{{$profile->url('/following')}}">
-              <span class="font-weight-bold">{{$profile->followingCount(true)}}</span> 
-              Following
-            </a>
-          </div>
-        </div>
-        <p class="lead font-weight-bold">
-          {{$profile->name}}
-        </p>
-      </div>
-    </div>
-  </div>
-
   <div class="col-12 col-md-8 offset-2">
     @if($following->count() !== 0)
     <ul class="list-group mt-4">
