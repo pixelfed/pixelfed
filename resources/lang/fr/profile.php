7a57c3cd42ed64b2bd30c411b3682b62577646fb
OpenAlgeria <39639882+OpenAlgeria@users.noreply.github.com>
2018-06-04 01:18:42 +0200

    Update French language profile.php

--- resources/lang/fr/profile.php
+++ resources/lang/fr/profile.php
@@ -1,4 +1,7 @@
 <?php
 return [
   'emptyTimeline' => 'Cet utilisateur n\'a pas encore de messages !',
+  'emptyFollowers' => 'Cet utilisateur n`\'a pas encore d\'abonné-e-s!',
+  'emptyFollowing' => 'Cet utilisateur ne suit pas encore quelqu\'un!',
+  'savedWarning'  => 'Vous seul pouvez voir ce que vous avez enregistré',
 ];
