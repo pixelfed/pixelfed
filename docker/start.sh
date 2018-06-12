9cc3406a12285d60e546efaaca031c24b6ef248b
Pierre Jaury <pierre@jaury.eu>
2018-06-12 22:48:43 +0200

    Create missing directories in storage

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mkdir -p storage/framework/{cache,sessions,views,logs}
+mkdir -p storage/app/public/avatars
+mkdir -p storage/{debugbar,logs}
+mkdir -p storage/framework/{cache,sessions,views,testing}
 php artisan migrate
 
 exec apache2-foreground
