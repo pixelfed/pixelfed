25e19ddb95be64566ff41bea521cfb1ae9c4e077
vilbi <40290127+vilbi@users.noreply.github.com>
2018-06-15 17:26:49 +0200

    New lang/de navmenu.php
    Translation of new navmenu into German (de)

---
+++ resources/lang/de/navmenu.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'viewMyProfile' => 'Mein Profil öffnen',
+    'myTimeline' => 'Meine Timeline',
+    'publicTimeline' => '&Ouml;ffentliche Timeline',
+    'remoteFollow' => 'Remote folgen',
+    'settings' => 'Einstellungen'
+    'admin' => 'Administator',
+    'logout' => 'Ausloggen',
+
+];
