583abdf37a1718725860eccd5c07f5a0729b8cda
Pierre Jaury <pierre@jaury.eu>
2018-06-16 22:27:45 +0200

    Include missing dependencies for image optimization, slim the image and run horizon

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cp -r storage.skel/* storage/
+chown -R www-data:www-data storage/
 php artisan migrate --force
+php artisan storage:link
 
+php artisan horizon &
 exec apache2-foreground
