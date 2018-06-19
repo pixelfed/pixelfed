3b9eac49f13d3d28ac711a8c0caea11c394b5284
Stasiek Michalski <hellcp@opensuse.org>
2018-06-19 03:57:35 +0200

    Improve menu for mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,15 @@
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8 p-sm-3 p-0">
-            <a class="card font-weight-bold text-center h-100" href="{{Auth::user()->url()}}">
+            <a class="card font-weight-bold text-center h-100 card-md-rounded-0" href="{{Auth::user()->url()}}">
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-3">
-                            <img class="img-thumbnail rounded-circle pr-1 w-25" src="{{Auth::user()->profile->avatarUrl()}}">
-                        </div>
-                        <h1 class="col-9 my-auto">&commat;{{Auth::user()->username}}</h1>
-                    </div>
+                    <img class="img-thumbnail rounded-circle d-inline-block mr-2" src="{{Auth::user()->profile->avatarUrl()}}" width="60">
+                    <h1 class="d-inline-block mb-0">&commat;{{Auth::user()->username}}</h1>
                 </div>
             </a>
         </div>
         <div class="col-md-4 p-sm-3 p-0">
-            <a class="card font-weight-bold text-center h-100" href="{{route('timeline.personal')}}">
+            <a class="card font-weight-bold text-center h-100 card-md-rounded-0" href="{{route('timeline.personal')}}">
                 <div class="card-body d-flex flex-column text-dark">
                     <h2 class="fas fa-list-alt mt-auto"></h2>
                     <p class="mb-auto">{{__('navmenu.myTimeline')}}</p>
@@ -24,7 +20,7 @@
             </a>
         </div>
         <div class="col-md-4 p-sm-3 p-0">
-            <a class="card font-weight-bold text-center h-100" href="{{route('timeline.public')}}">
+            <a class="card font-weight-bold text-center h-100 card-md-rounded-0" href="{{route('timeline.public')}}">
                 <div class="card-body d-flex flex-column text-dark">
                     <h2 class="fas fa-list mt-auto"></h2>
                     <p class="mb-auto">{{__('navmenu.publicTimeline')}}</p>
@@ -32,7 +28,7 @@
             </a>
         </div>
         <div class="col-md-4 p-sm-3 p-0">
-            <a class="card font-weight-bold text-center h-100" href="{{route('remotefollow')}}">
+            <a class="card font-weight-bold text-center h-100 card-md-rounded-0" href="{{route('remotefollow')}}">
                 <div class="card-body d-flex flex-column text-dark">
                     <h2 class="fas fa-user-plus mt-auto"></h2>
                     <p class="mb-auto">{{__('navmenu.remoteFollow')}}</p>
@@ -40,7 +36,7 @@
             </a>
         </div>
         <div class="col-md-4 p-sm-3 p-0">
-            <a class="card font-weight-bold text-center h-100" href="{{route('settings')}}">
+            <a class="card font-weight-bold text-center h-100 card-md-rounded-0" href="{{route('settings')}}">
                 <div class="card-body d-flex flex-column text-dark">
                     <h2 class="fas fa-cog mt-auto"></h2>
                     <p class="mb-auto">{{__('navmenu.settings')}}</p>
@@ -48,7 +44,7 @@
             </a>
         </div>
         <div class="col-md-4 p-sm-3 p-0">
-            <a class="card text-white font-weight-bold text-center h-100 bg-danger" href="{{ route('logout') }}">
+            <a class="card text-white font-weight-bold text-center h-100 bg-danger card-md-rounded-0" href="{{ route('logout') }}">
                 <div class="card-body d-flex flex-column">
                     <h2 class="fas fa-sign-out-alt mt-auto"></h2>
                     <p class="mb-auto">{{ __('navmenu.logout') }}</p>
