5d3b66f77853b6fb61dc90541449ff5346b97af7
Daniel Supernault <danielsupernault@gmail.com>
2018-08-10 00:56:38 -0600

    Update site sidebar

--- resources/views/site/partial/sidebar.blade.php
+++ resources/views/site/partial/sidebar.blade.php
@@ -1,25 +1,27 @@
   <div class="col-12 col-md-3 py-3" style="border-right:1px solid #ccc;">
     <ul class="nav flex-column settings-nav">
       <li class="nav-item pl-3 {{request()->is('site/about')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.about')}}">About</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.about')}}">About</a>
       </li>
-      <li class="nav-item pl-3 {{request()->is('site/features')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.features')}}">Features</a>
-      </li>
+      {{--
+        <li class="nav-item pl-3 {{request()->is('site/features')?'active':''}}">
+          <a class="nav-link lead text-muted" href="{{route('site.features')}}">Features</a>
+        </li>
+      --}}
       <li class="nav-item pl-3 {{request()->is('site/help')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.help')}}">Help</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.help')}}">Help</a>
       </li>
       <li class="nav-item pl-3 {{request()->is('site/language')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.language')}}">Language</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.language')}}">Language</a>
       </li>
       <li class="nav-item">
         <hr>
       </li>
       <li class="nav-item pl-3 {{request()->is('site/fediverse')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.fediverse')}}">Fediverse</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.fediverse')}}">Fediverse</a>
       </li>
       <li class="nav-item pl-3 {{request()->is('site/open-source')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.opensource')}}">Open Source</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.opensource')}}">Open Source</a>
       </li>
       {{-- <li class="nav-item pl-3 {{request()->is('site/banned-instances')?'active':''}}">
         <a class="nav-link lead text-muted" href="{{route('site.bannedinstances')}}">Banned Content</a>
@@ -31,16 +33,18 @@
         <hr>
       </li>
       <li class="nav-item pl-3 {{request()->is('site/terms')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.terms')}}">Terms</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.terms')}}">Terms</a>
       </li>
       <li class="nav-item pl-3 {{request()->is('site/privacy')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.privacy')}}">Privacy</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('site.privacy')}}">Privacy</a>
       </li>
-      <li class="nav-item pl-3 {{request()->is('site/platform')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.platform')}}">Platform</a>
-      </li>
-      <li class="nav-item pl-3 {{request()->is('site/libraries')?'active':''}}">
-        <a class="nav-link lead text-muted" href="{{route('site.libraries')}}">Libraries</a>
-      </li>
+      {{--
+        <li class="nav-item pl-3 {{request()->is('site/platform')?'active':''}}">
+          <a class="nav-link lead text-muted" href="{{route('site.platform')}}">Platform</a>
+        </li>
+        <li class="nav-item pl-3 {{request()->is('site/libraries')?'active':''}}">
+          <a class="nav-link lead text-muted" href="{{route('site.libraries')}}">Libraries</a>
+        </li>
+      --}}
     </ul>
   </div>
