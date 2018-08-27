35e7cd574a64059871a84f4c29687e8ce4ad3fce
Daniel Supernault <danielsupernault@gmail.com>
2018-08-26 21:23:46 -0600

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -135,6 +135,8 @@ Route::domain(config('pixelfed.domain.app'))->middleware('validemail')->group(fu
 
   Route::get('p/{username}/{id}/c/{cid}', 'CommentController@show');
   Route::get('p/{username}/{id}/c', 'CommentController@showAll');
+  Route::get('p/{username}/{id}/edit', 'StatusController@edit');
+  Route::post('p/{username}/{id}/edit', 'StatusController@editStore');
   Route::get('p/{username}/{id}', 'StatusController@show');
   Route::get('{username}/saved', 'ProfileController@savedBookmarks');
   Route::get('{username}/followers', 'ProfileController@followers');
