336deae05b20d8cb219f2b1e13cbb4b7bf4f475f
Daniel Supernault <danielsupernault@gmail.com>
2018-09-09 14:22:15 -0600

    Update AccountController

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -278,4 +278,23 @@ class AccountController extends Controller
 
         return response()->json(['msg' => 'success'], 200);
     }
+
+    public function sudoMode(Request $request)
+    {
+        return view('auth.sudo');
+    }
+
+    public function sudoModeVerify(Request $request)
+    {
+        $this->validate($request, [
+            'password' => 'required|string|max:500'
+        ]);
+        $user = Auth::user();
+        $password = $request->input('password');
+        $next = $request->cookie('redirectNext') ?:'/';
+        if(password_verify($password, $user->password) === true) {
+            return redirect($next)->withCookie('sudoMode', time());
+        }
+        return redirect($next);
+    }
 }
