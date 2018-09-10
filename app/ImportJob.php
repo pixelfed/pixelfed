fbc15e8a0760e6b8f824144863c8a9be98af7ecb
Daniel Supernault <danielsupernault@gmail.com>
2018-09-09 22:43:42 -0600

    Update ImportJob model

--- app/ImportJob.php
+++ app/ImportJob.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImportJob extends Model
 {
-    //
+    public function url()
+    {
+    	return url("/i/import/job/{$this->uuid}/{$this->stage}");
+    }
+
+    public function files()
+    {
+    	return $this->hasMany(ImportData::class, 'job_id');
+    }
+
+    public function mediaJson()
+    {
+    	$path = storage_path("app/$this->media_json");
+    	return json_decode(file_get_contents($path), true);
+    }
 }
