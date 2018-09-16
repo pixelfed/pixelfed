dd134ba5e35ef5433608e33a0f75e4dd3ab351b5
Daniel Supernault <danielsupernault@gmail.com>
2018-09-16 14:21:17 -0600

    Update 2FA partial views

---
+++ resources/views/settings/security/2fa/partial/disabled-panel.blade.php
@@ -0,0 +1,18 @@
+<ul class="list-group">
+	<li class="list-group-item bg-light">
+		<div class="text-center py-5 px-4">
+			<p class="text-muted">
+				<i class="fas fa-lock fa-2x"></i>
+			</p>
+			<p class="text-muted h4 font-weight-bold">
+				Two factor authentication is not enabled yet.
+			</p>
+			<p class="text-muted">
+				Two-factor authentication adds an additional layer of security to your account by requiring more than just a password to log in. <a href="#">Learn more</a>.
+			</p>
+			<p class="mb-0">
+				<a class="btn btn-success font-weight-bold" href="{{route('settings.security.2fa.setup')}}">Enable two-factor authentication</a>
+			</p>
+		</div>
+	</li>
+</ul>
