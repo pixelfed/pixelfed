8f2e764913f2417c80431d1c0e9785225ddc717a
Aditoo17 <42938951+Aditoo17@users.noreply.github.com>
2018-09-16 20:42:20 +0200

    L10n: Add Czech translation

---
+++ resources/lang/cs/navmenu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'viewMyProfile'  => 'Zobrazit můj profil',
+    'myTimeline'     => 'Moje časová osa',
+    'publicTimeline' => 'Veřejná časová osa',
+    'remoteFollow'   => 'Vzdálené sledování',
+    'settings'       => 'Nastavení',
+    'admin'          => 'Administrace',
+    'logout'         => 'Odhlásit',
+    'directMessages' => 'Přímé zprávy',
+
+];
