8b08983c3802962c8fedb32a2147271d791b0b7e
Daniel Supernault <danielsupernault@gmail.com>
2018-09-15 18:36:04 -0600

    Update Auth Events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,11 +13,11 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        'Illuminate\Auth\Events\Login' => [
+            'App\Listeners\AuthLogin',
         ],
-        'auth.login' => [
-            'App\Events\AuthLoginEvent',
+        'Illuminate\Auth\Events\Failed' => [
+            'App\Listeners\LogFailedLogin',
         ],
     ];
 
