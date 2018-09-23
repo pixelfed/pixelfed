3328726f6333f9ebf3217b2a62d435717a95629a
Daniel Supernault <danielsupernault@gmail.com>
2018-09-22 20:41:26 -0600

    Update Discover Controller

--- app/Http/Controllers/DiscoverController.php
+++ app/Http/Controllers/DiscoverController.php
@@ -27,10 +27,12 @@ class DiscoverController extends Controller
 
         $pid = Auth::user()->profile->id;
 
-        $following = Cache::remember('feature:discover:following:'.$pid, 15, function() use($pid) {
-          $following = Follower::whereProfileId($pid)
-          ->pluck('following_id');
-          $filtered = UserFilter::whereUserId($pid)
+        $following = Cache::remember('feature:discover:following:'.$pid, 720, function() use($pid) {
+          $following = Follower::select('following_id')
+                      ->whereProfileId($pid)
+                      ->pluck('following_id');
+          $filtered = UserFilter::select('filterable_id')
+                    ->whereUserId($pid)
                     ->whereFilterableType('App\Profile')
                     ->whereIn('filter_type', ['mute', 'block'])
                     ->pluck('filterable_id');
@@ -43,7 +45,7 @@ class DiscoverController extends Controller
         });
 
         $people = Cache::remember('feature:discover:people:'.$pid, 15, function() use($following) {
-            return Profile::inRandomOrder()
+            return Profile::select('id', 'name', 'username')->inRandomOrder()
                 ->whereHas('statuses')
                 ->whereNull('domain')
                 ->whereNotIn('id', $following)
@@ -52,13 +54,14 @@ class DiscoverController extends Controller
                 ->get();
         });
 
-        $posts = Status::whereHas('media')
+        $posts = Status::select('id', 'caption', 'profile_id')
+          ->whereHas('media')
           ->whereHas('profile', function($q) {
             $q->where('is_private', false);
           })
           ->whereIsNsfw(false)
           ->whereVisibility('public')
-          ->where('profile_id', '!=', $pid)
+          ->where('profile_id', '<>', $pid)
           ->whereNotIn('profile_id', $following)
           ->withCount(['comments', 'likes'])
           ->orderBy('created_at', 'desc')
