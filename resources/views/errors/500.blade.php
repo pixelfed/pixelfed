3050d21612064d7b1a62f4f6177866f454455f76
Giovanni Smecca <gsmecca@gmail.com>
2018-10-05 18:25:45 -0500

    grammar adjustments

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -6,7 +6,7 @@
     <div class="card mx-5">
       <div class="card-body p-5 text-center">
         <h1>Whoops! Something went wrong.</h1>
-        <p class="mb-0 text-muted lead">Please try again, if this error keeps happening please contact an admin.</p>
+        <p class="mb-0 text-muted lead">If you keep seeing this message, please contact an admin.</p>
         <img src="/img/fred1.gif" class="img-fluid">
       </div>
     </div>
