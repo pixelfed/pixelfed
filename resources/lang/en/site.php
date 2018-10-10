5089d50bf08e1f7119713295726206c6d21ba1ea
Daniel Supernault <danielsupernault@gmail.com>
2018-10-09 19:30:19 -0600

    Add new localizations

---
+++ resources/lang/en/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	'about'			=> 'About',
+	'help'			=> 'Help',
+	'language'		=> 'Language',
+	'fediverse'		=> 'Fediverse',
+	'opensource'	=> 'Open Source',
+	'terms'			=> 'Terms',
+	'privacy'		=> 'Privacy',
+	'l10nWip'		=> 'We’re still working on localization support',
+	'currentLocale' => 'Current locale',
+	'selectLocale'  => 'Select from one of the supported languages',
+
+];
