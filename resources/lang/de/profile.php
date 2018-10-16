588860066d52ccffee49fd356befbe8990b4237e
Tim Speckhals <github@timmyrs.de>
2018-10-16 04:07:00 +0200

    Update German Translation

--- resources/lang/de/profile.php
+++ resources/lang/de/profile.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
-  'emptyTimeline'  => 'Dieser Benutzer hat noch nichts gepostet!',
-  'emptyFollowers' => 'Diesem Benutzer folgt noch niemand!',
-  'emptyFollowing' => 'Dieser Benutzer folgt noch niemanden!',
-  'savedWarning'   => 'Nur du kannst sehen was du gespeichert hast',
+  'emptyTimeline'         => 'Dieser Benutzer hat noch nichts gepostet!',
+  'emptyFollowers'        => 'Diesem Benutzer folgt noch niemand!',
+  'emptyFollowing'        => 'Dieser Benutzer folgt noch niemanden!',
+  'emptySaved'            => 'Du hast noch keinen Post gespeichert!',
+  'savedWarning'          => 'Nur du kannst sehen was du gespeichert hast',
+  'privateProfileWarning' => 'Dieser Account ist privat',
+  'alreadyFollow'         => ':username bereits folgen?',
+  'loginToSeeProfile'     => 'um deren Bilder und Videos zu sehen.',
 ];
