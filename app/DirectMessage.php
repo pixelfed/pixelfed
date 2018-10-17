0fba6e16a786112d6db24feb798457b6d9eebb46
Daniel Supernault <danielsupernault@gmail.com>
2018-10-17 12:21:39 -0600

    Add DirectMessage model

---
+++ app/DirectMessage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Auth;
+use Illuminate\Database\Eloquent\Model;
+
+class DirectMessage extends Model
+{
+    public function status()
+    {
+    	return $this->hasOne(Status::class, 'id', 'status_id');
+    }
+
+    public function url()
+    {
+    	return url('/i/message/' . $this->to_id . '/' . $this->id);
+    }
+
+    public function author()
+    {
+    	return $this->hasOne(Profile::class, 'id', 'from_id');
+    }
+
+    public function me()
+    {
+    	return Auth::user()->profile->id === $this->from_id;
+    }
+}
