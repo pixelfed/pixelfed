37713f33d04ee1d880be067685f21aa30e3d4a47
kaiyou <pierre@jaury.eu>
2018-10-18 08:26:32 +0200

    Run pixelfed update tasks upon startup

--- contrib/docker/start.sh
+++ contrib/docker/start.sh
@@ -8,6 +8,9 @@ php artisan storage:link
 # Migrate database if the app was upgraded
 php artisan migrate --force
 
+# Run other specific migratins if required
+php artisan update
+
 # Run a worker if it is set as embedded
 if [ $HORIZON_EMBED = true ]; then
 	php artisan horizon &
