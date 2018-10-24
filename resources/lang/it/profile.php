4e9e6c49933788c6c112eda248ffdf611a00a39d
Sylke Vicious <41053887+silkevicious@users.noreply.github.com>
2018-10-24 17:17:20 +0200

    Italian translations

---
+++ resources/lang/it/profile.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'emptyTimeline'         => 'Questo utente non ha ancora nessun post!',
+  'emptyFollowers'        => 'Questo utente non ha ancora seguaci!',
+  'emptyFollowing'        => 'Questo utente non segue ancora nessuno!',
+  'emptySaved'            => 'Non hai ancora salvato nessun post!',
+  'savedWarning'          => 'Solo tu puoi vedere cosa hai salvato',
+  'privateProfileWarning' => 'Questo Account è Privato',
+  'alreadyFollow'         => 'Segui già :username?',
+  'loginToSeeProfile'     => 'per vedere le sue foto e video.',
+];
---
+++ resources/lang/it/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	'about'			=> 'A Proposito',
+	'help'			=> 'Aiuto',
+	'language'		=> 'Lingua',
+	'fediverse'		=> 'Fediverso',
+	'opensource'	=> 'Open Source',
+	'terms'			=> 'Termini',
+	'privacy'		=> 'Privacy',
+	'l10nWip'		=> 'Stiamo ancora lavorando sul supporto per la traduzione',
+	'currentLocale' => 'Lingua attuale',
+	'selectLocale'  => 'Seleziona una tra le lingue supportate',
+
+];
