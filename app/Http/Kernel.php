f4603207c783ae6739a0fd7827bf22aace053bd6
Daniel Supernault <danielsupernault@gmail.com>
2018-11-03 21:58:21 -0600

    Add new middleware to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can'           => \Illuminate\Auth\Middleware\Authorize::class,
         'dangerzone'    => \App\Http\Middleware\DangerZone::class,
+        'localization'    => \App\Http\Middleware\Localization::class,
         'guest'         => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed'        => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
