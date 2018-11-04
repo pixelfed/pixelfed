91ec751802821f7453ccfef3be7fc3f38ce3a002
Daniel Supernault <danielsupernault@gmail.com>
2018-11-03 22:32:16 -0600

    Update settings import view

--- resources/views/settings/import/home.blade.php
+++ resources/views/settings/import/home.blade.php
@@ -12,12 +12,11 @@
   </section>
   <section class="mt-5">
     <p class="small text-muted font-weight-bold text-uppercase mb-3">Supported Services</p>
-    <p class="font-weight-bold text-muted">No services are supported at the moment.</p>
+    <p class="">
+      <a class="btn btn-outline-primary font-weight-bold" href="{{route('settings.import.ig')}}">Import from Instagram</a>
+    </p>
     <hr>
     <p class="small text-muted font-weight-bold text-uppercase mb-3">Coming Soon</p>
-    <p class="">
-      <a class="btn btn-outline-secondary font-weight-bold disabled" href="#">Import from Instagram</a>
-    </p>
     <p class="">
       <a class="btn btn-outline-secondary font-weight-bold disabled" href="#">Import from Mastodon</a>
     </p>
