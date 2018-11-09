8148f80c6bb748d6717d7426eb1acfdfca3fe8da
Daniel Supernault <danielsupernault@gmail.com>
2018-11-08 20:43:32 -0700

    Update bootstrap/components js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,22 +1,7 @@
 window._ = require('lodash');
 window.Popper = require('popper.js').default;
-import swal from 'sweetalert';
 window.pixelfed = window.pixelfed || {};
 window.$ = window.jQuery = require('jquery');
 require('bootstrap');
-window.InfiniteScroll = require('infinite-scroll');
-window.filesize = require('filesize');
 window.typeahead = require('./lib/typeahead');
 window.Bloodhound = require('./lib/bloodhound');
-window.Vue = require('vue');
-import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue);
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
--- resources/assets/js/components.js
+++ resources/assets/js/components.js
@@ -1,3 +1,20 @@
+window.Vue = require('vue');
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+window.InfiniteScroll = require('infinite-scroll');
+window.filesize = require('filesize');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import swal from 'sweetalert';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
 require('./components/localstorage');
 require('./components/likebutton');
 require('./components/commentform');
