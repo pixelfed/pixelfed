7d152a7b54eb8cf2c158e3746ea017a14fd5c46e
Daniel Supernault <danielsupernault@gmail.com>
2018-11-08 22:01:59 -0700

    Update bootstrap.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -5,3 +5,12 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap');
 window.typeahead = require('./lib/typeahead');
 window.Bloodhound = require('./lib/bloodhound');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
