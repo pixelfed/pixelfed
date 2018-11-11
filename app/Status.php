905c2417aed3b0df3d6b1185b2e66df8b5fdcae9
Daniel Supernault <danielsupernault@gmail.com>
2018-11-10 20:15:07 -0700

    Update Status model

--- app/Status.php
+++ app/Status.php
@@ -64,11 +64,6 @@ class Status extends Model
         $id = $this->id;
         $username = $this->profile->username;
         $path = config('app.url')."/p/{$username}/{$id}";
-        if (!is_null($this->in_reply_to_id)) {
-            $pid = $this->in_reply_to_id;
-            $path = "{$this->parent()->url()}#comment-{$id}";
-        }
-
         return url($path);
     }
 
