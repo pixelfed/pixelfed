a7657199f5740488934cfc52c551775d73732f06
Daniel Supernault <danielsupernault@gmail.com>
2018-11-18 21:37:47 -0700

    Update PostComments vue component

--- resources/assets/js/components/PostComments.vue
+++ resources/assets/js/components/PostComments.vue
@@ -18,11 +18,14 @@
   <div class="postCommentsContainer d-none">
     <p class="mb-1 text-center load-more-link"><a href="#" class="text-muted" v-on:click="loadMore">Load more comments</a></p>
     <div class="comments" data-min-id="0" data-max-id="0">
-      <p class="mb-0 read-more" v-for="(comment, index) in results" :data-id="comment.id" v-bind:key="comment.id" style="overflow: hidden;">
-        <span class="d-flex justify-content-between align-items-center ">
-          <span class="pr-3">
-            <span class="font-weight-bold pr-1"><bdi><a class="text-dark" :href="comment.account.url" :title="comment.account.username">{{l(comment.account.username)}}</a></bdi></span>
-            <span class="comment-text" v-html="comment.content" style="overflow: hidden;"></span>
+      <p class="mb-1" v-for="(comment, index) in results" :data-id="comment.id" v-bind:key="comment.id">
+        <span class="d-flex justify-content-between align-items-center">
+          <span class="pr-3" style="overflow: hidden;">
+            <div class="font-weight-bold pr-1"><bdi><a class="text-dark" :href="comment.account.url" :title="comment.account.username">{{l(comment.account.username)}}</a></bdi>
+            </div>
+            <div class="read-more" style="overflow: hidden;">
+              <span class="comment-text" v-html="comment.content" style="overflow: hidden;"></span>
+            </div>
           </span>
           <b-dropdown :id="comment.uri" variant="link" no-caret class="float-right">
             <template slot="button-content">
