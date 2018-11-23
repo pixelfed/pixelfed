be081c6427d5b5e9f7b2f4d32beaa39746221a55
Urtzi Odriozola <uodriozola@codesyntax.com>
2018-11-23 17:24:12 +0100

    Basque localization

---
+++ resources/lang/eu/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    'failed'   => 'Sartutako datuek ez dute balio.',
+    'throttle' => 'Saiakera gehiegi. Mesedez, saitu zaitez :seconds segundo barru.',
+
+];
