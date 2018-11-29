bc52949ecdad1d383dca53e9a2189ba129f6377a
Daniel Supernault <danielsupernault@gmail.com>
2018-11-29 01:33:45 -0700

    Bump version

--- config/pixelfed.php
+++ config/pixelfed.php
@@ -23,7 +23,7 @@ return [
     | This value is the version of your PixelFed instance.
     |
     */
-    'version' => '0.2.1',
+    'version' => '0.3.0',
 
     /*
     |--------------------------------------------------------------------------
