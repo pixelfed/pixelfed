382fcea64e0e8baaa819cc15c1e9daa23c89249f
Daniel Supernault <danielsupernault@gmail.com>
2018-12-03 20:41:12 -0700

    Update closed registration page with links to other instances

--- resources/views/site/closed-registration.blade.php
+++ resources/views/site/closed-registration.blade.php
@@ -7,6 +7,8 @@
       <div class="card-body p-5 text-center">
         <h1>Registration is closed</h1>
         <p class="lead mb-0">We have closed registrations on this instance.</p>
+        <hr>
+        <p class="lead">You can find a list of active pixelfed instances on <a href="https://the-federation.info/pixelfed" rel="external nofollow noopener">https://the-federation.info/pixelfed</a> or <a href="https://fediverse.network/pixelfed" rel="external nofollow noopener">https://fediverse.network/pixelfed</a></p>
       </div>
     </div>
   </div>
