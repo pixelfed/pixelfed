997a913aa1ac2bb501f7446b016f7d5965a132a3
Urtzi Odriozola <uodriozola@codesyntax.com>
2018-12-11 22:44:58 +0100

    Create site.php

---
+++ resources/lang/eu/site.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'about'			=> 'Honi buruz',
+	'help'			=> 'Laguntza',
+	'language'		=> 'Hizkuntza',
+	'fediverse'		=> 'Fedibertsoa',
+	'opensource'	=> 'Kode irekia',
+	'terms'			=> 'Lege oharra',
+	'privacy'		=> 'Pribatutasuna',
+	'l10nWip'		=> 'Oraindik lokalizazioen sustenguan lanean gaude',
+	'currentLocale' => 'Uneko hizkuntza',
+	'selectLocale'  => 'Aukeratu onartutako hizkuntzetako bat',
+];
