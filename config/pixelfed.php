5a8b919f25f37f0a9fa3554fcce54c845363839e
Daniel Supernault <danielsupernault@gmail.com>
2018-12-15 16:54:54 -0700

    Update compiled assets, bump version

--- config/pixelfed.php
+++ config/pixelfed.php
@@ -23,7 +23,7 @@ return [
     | This value is the version of your PixelFed instance.
     |
     */
-    'version' => '0.5.5',
+    'version' => '0.5.6',
 
     /*
     |--------------------------------------------------------------------------
