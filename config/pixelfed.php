ab0b39afeb591487b4b29409cc01ab90d317bb50
Daniel Supernault <danielsupernault@gmail.com>
2018-12-18 00:13:24 -0700

    Bump version to 0.7.0

--- config/pixelfed.php
+++ config/pixelfed.php
@@ -23,7 +23,7 @@ return [
     | This value is the version of your PixelFed instance.
     |
     */
-    'version' => '0.6.1',
+    'version' => '0.7.0',
 
     /*
     |--------------------------------------------------------------------------
