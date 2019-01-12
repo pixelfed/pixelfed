84960d701cf873bc7d33e6c937d32bf89b40bd90
Daniel Supernault <danielsupernault@gmail.com>
2019-01-12 13:58:35 -0700

    Add API Search ResultsTransformer

--- app/Transformer/Api/ResultsTransformer.php
+++ app/Transformer/Api/ResultsTransformer.php
@@ -8,17 +8,35 @@ class ResultsTransformer extends Fractal\TransformerAbstract
 {
 
 	protected $defaultIncludes = [
-		'account',
-		'mentions',
-		'media_attachments',
-		'tags',
+		'accounts',
+		'statuses',
+		'hashtags',
 	];
-	public function transform()
+
+	public function transform($results)
 	{
 		return [
 			'accounts' => [],
 			'statuses' => [],
 			'hashtags' => []
 		];
+	}
+
+	public function includeAccounts($results)
+	{
+		$accounts = $results->accounts;
+		return $this->collection($accounts, new AccountTransformer());
+	}
+
+	public function includeStatuses($results)
+	{
+		$statuses = $results->statuses;
+		return $this->collection($statuses, new StatusTransformer());
+	}
+
+	public function includeTags($results)
+	{
+		$hashtags = $status->hashtags;
+		return $this->collection($hashtags, new HashtagTransformer());
 	}
 }
