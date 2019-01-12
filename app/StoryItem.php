d9f61d0424d80dbf2edafe8dfd69db59d08a182e
Daniel Supernault <danielsupernault@gmail.com>
2019-01-12 00:43:53 -0700

    Add StoryItem Model

---
+++ app/StoryItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StoryItem extends Model
+{
+    public function story()
+    {
+    	return $this->belongsTo(Story::class);
+    }
+}
