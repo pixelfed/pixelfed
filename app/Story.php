ae78addab4549038f403a7eb6fb0d6d296b04799
Daniel Supernault <danielsupernault@gmail.com>
2019-01-12 00:42:26 -0700

    Update Story Model

--- app/Story.php
+++ app/Story.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Story extends Model
 {
-    //
+	public function items()
+	{
+		return $this->hasMany(StoryItem::class);
+	}
+
+	public function reactions()
+	{
+		return $this->hasMany(StoryReaction::class);
+	}
+
+	public function views()
+	{
+		return $this->hasMany(StoryView::class);
+	}
 }
