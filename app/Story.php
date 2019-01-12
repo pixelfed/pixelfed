d7d1303a63197e8cb7a46a70a68048bbee1466a0
Daniel Supernault <danielsupernault@gmail.com>
2019-01-12 12:38:09 -0700

    Update Story Model

--- app/Story.php
+++ app/Story.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Story extends Model
 {
+	protected $visible = ['id'];
+	
+	public function profile()
+	{
+		return $this->belongsTo(Profile::class);
+	}
+
 	public function items()
 	{
 		return $this->hasMany(StoryItem::class);
