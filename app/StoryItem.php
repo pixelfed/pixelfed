e764b6e8b33985e13029dc18478eb6f1d5dc62c0
Daniel Supernault <danielsupernault@gmail.com>
2019-01-12 13:34:10 -0700

    Update StoryItem model

--- app/StoryItem.php
+++ app/StoryItem.php
@@ -3,11 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class StoryItem extends Model
 {
 	public function story()
 	{
 		return $this->belongsTo(Story::class);
+	}
+
+	public function url()
+	{
+		return Storage::url($this->media_path);
 	}
 }
