07a04a5e616e79c6ec6382083d7d53f3b1c78157
j1t <j1t@users.noreply.github.com>
2019-01-13 14:16:33 +0000

    Update profile.php

--- resources/lang/he/profile.php
+++ resources/lang/he/profile.php
@@ -6,4 +6,10 @@ return [
   'emptyFollowing' => 'משתמש זה עדיין אינו עוקב אחרי אף אחד!',
   'emptySaved'     => 'עדיין לא שמרת שום פוסט!',
   'savedWarning'   => 'רק אתם יכולים לראות את מה ששמרתם',
+  'privateProfileWarning' => 'חשבון זה פרטי',
+  'alreadyFollow'         => 'כבר עוקב :username?',
+  'loginToSeeProfile'     => 'על מנת לראות את התמונות והסרטונים שלהם.',
+
+  'status.disabled.header' 	  => 'פרופיל לא זמין',
+  'status.disabled.body'	  => 'מצטערים, פרופיל זה אינו זמין כרגע. אנא נסו שוב בקרוב.',
 ];
