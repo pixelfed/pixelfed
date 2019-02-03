0fcb5c9dd1ea0cc78aaf6e72121f0184d5d19469
Daniel Supernault <danielsupernault@gmail.com>
2019-02-03 14:53:13 -0700

    Update webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,21 @@
 let mix = require('laravel-mix');
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .js('resources/assets/js/activity.js', 'public/js')
-   .js('resources/assets/js/components.js', 'public/js')
-   .js('resources/assets/js/timeline.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .version();
+.js('resources/assets/js/activity.js', 'public/js')
+.js('resources/assets/js/components.js', 'public/js')
+.js('resources/assets/js/timeline.js', 'public/js')
+.sass('resources/assets/sass/app.scss', 'public/css', {
+	implementation: require('node-sass')
+})
+.version();
