ec189fc045e3a1d059917fe9e992637c6d87422f
Daniel Supernault <danielsupernault@gmail.com>
2019-02-10 17:18:28 -0700

    Update admin maintenance settings view

--- resources/views/admin/settings/maintenance.blade.php
+++ resources/views/admin/settings/maintenance.blade.php
@@ -1,9 +1,14 @@
 @extends('admin.partial.template')
 
-@section('section')
-  <div class="title">
-    <h3 class="font-weight-bold">Maintenance</h3>
-  </div>
-  <hr>
+@include('admin.settings.sidebar')
 
+@section('section')
+<div class="title">
+	<h3 class="font-weight-bold">Maintenance</h3>
+	<p class="lead">Enable maintenance mode</p>
+</div>
+<hr>
+<p class="alert alert-warning">
+	<strong>Feature Unavailable:</strong> This feature will be released in v0.9.0.
+</p>
 @endsection
