e5ffd8f80abfebd3546ec19a510eb6702d0edb53
Daniel Supernault <danielsupernault@gmail.com>
2019-02-10 22:40:22 -0700

    Update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,26 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
 .js('resources/assets/js/activity.js', 'public/js')
 .js('resources/assets/js/components.js', 'public/js')
+
+// Discover component
+.js('resources/assets/js/discover.js', 'public/js')
+
+// Profile component
+.js('resources/assets/js/profile.js', 'public/js')
+
+// Status component
+.js('resources/assets/js/status.js', 'public/js')
+
+// Timeline component
 .js('resources/assets/js/timeline.js', 'public/js')
+
+// MicroUI component
+.js('resources/assets/js/micro.js', 'public/js')
+
 .sass('resources/assets/sass/app.scss', 'public/css', {
+	implementation: require('node-sass')
+})
+.sass('resources/assets/sass/appdark.scss', 'public/css', {
 	implementation: require('node-sass')
 })
 .version();
