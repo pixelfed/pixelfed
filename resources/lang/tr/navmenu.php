af695dff9f77721e531257a374a43ce223908b4f
Batu Berk <batuberkshn@gmail.com>
2019-02-17 23:12:51 +0300

    All files translated to Turkish

---
+++ resources/lang/tr/navmenu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'viewMyProfile'  => 'Profilimi görüntüle',
+    'myTimeline'     => 'Benim Akışım',
+    'publicTimeline' => 'Akış',
+    'remoteFollow'   => 'Takip',
+    'settings'       => 'Ayarlar',
+    'admin'          => 'Yönetici',
+    'logout'         => 'Çıkış',
+    'directMessages' => 'Direkt Mesaj',
+
+];
---
+++ resources/lang/tr/notification.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+  'likedPhoto'          => 'gönderini beğendi.',
+  'startedFollowingYou' => 'seni takip etmeye başladı.',
+  'commented'           => 'gönderine yorum yaptı.',
+  'mentionedYou'        => 'seni etiketledi.',
+  'shared'				=> 'gönderini paylaştı.',
+
+];
