70a9bc4bd1213eb98a4aedcd8180db7ad3f7eaae
Daniel Supernault <danielsupernault@gmail.com>
2019-02-24 12:16:18 -0700

    Update ASP, enforce https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        URL::forceScheme('https');
         Schema::defaultStringLength(191);
 
         Avatar::observe(AvatarObserver::class);
