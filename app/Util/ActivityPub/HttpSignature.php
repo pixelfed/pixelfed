d2823e64fa108a00ed03fafde1d1f3b73057bce4
Daniel Supernault <danielsupernault@gmail.com>
2019-02-25 14:23:54 -0700

    Update ActivityPub

--- app/Util/ActivityPub/HttpSignature.php
+++ app/Util/ActivityPub/HttpSignature.php
@@ -108,7 +108,8 @@ class HttpSignature {
       '(request-target)' => 'post '.parse_url($url, PHP_URL_PATH),
       'Date' => $date->format('D, d M Y H:i:s \G\M\T'),
       'Host' => parse_url($url, PHP_URL_HOST),
-      'Content-Type' => 'application/activity+json',
+      'Accept' => 'application/activity+json, application/json',
+      'Content-Type' => 'application/activity+json'
     ];
 
     if($digest) {
