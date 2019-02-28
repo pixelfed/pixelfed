35dc16176edb26141f9805eecdcee4a5de155c98
Daniel Supernault <danielsupernault@gmail.com>
2019-02-27 20:40:49 -0700

    Update l10n

--- resources/lang/en/navmenu.php
+++ resources/lang/en/navmenu.php
@@ -2,13 +2,17 @@
 
 return [
 
-    'viewMyProfile'  => 'View my profile',
-    'myTimeline'     => 'My Timeline',
-    'publicTimeline' => 'Public Timeline',
-    'remoteFollow'   => 'Remote Follow',
-    'settings'       => 'Settings',
-    'admin'          => 'Admin',
-    'logout'         => 'Logout',
-    'directMessages' => 'Direct Messages',
+	'search'			=> 'Search',
+	'home'				=> 'Home',
+	'local'				=> 'Local',
+	'discover'			=> 'Discover',
+	'viewMyProfile'		=> 'View my profile',
+	'myTimeline'		=> 'My Timeline',
+	'publicTimeline'	=> 'Public Timeline',
+	'remoteFollow'		=> 'Remote Follow',
+	'settings'			=> 'Settings',
+	'admin'				=> 'Admin',
+	'logout'			=> 'Logout',
+	'directMessages'	=> 'Direct Messages',
 
 ];
