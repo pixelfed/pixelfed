644e34ada6501a817f5f3bea1ef5342809e99c4b
Marcin Mikołajczak <me@m4sk.in>
2019-02-28 13:48:43 +0100

    i18n:🇵🇱
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

---
+++ resources/lang/pl/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	'about'			=> 'O nas',
+	'help'			=> 'Pomoc',
+	'language'		=> 'Język',
+	'fediverse'		=> 'Fediwersum',
+	'opensource'	=> 'Otwarte źródło',
+	'terms'			=> 'Zasady',
+	'privacy'		=> 'Prywatność',
+	'l10nWip'		=> 'Wciąż pracujemy nad obsługą wielu języków',
+	'currentLocale' => 'Obecny język',
+	'selectLocale'  => 'Wybierz jeden z dostępnych języków',
+
+];
