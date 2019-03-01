fca45629334a4a10a15292b3a12c008af6af700c
Daniel Supernault <danielsupernault@gmail.com>
2019-03-01 13:29:35 -0700

    Add user:table command

---
+++ app/Console/Commands/UserTable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+
+class UserTable extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:table {limit=10}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Display latest users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $limit = $this->argument('limit');
+
+        $headers = ['ID', 'Username', 'Name', 'Registered'];
+
+        $users = User::orderByDesc('id')->take($limit)->get(['id', 'username', 'name', 'created_at'])->toArray();
+
+        $this->table($headers, $users);
+    }
+}
