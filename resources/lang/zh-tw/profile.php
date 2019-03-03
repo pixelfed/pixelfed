f7526e3a4c0b011f8042f31b1ba2cbdf91e59cae
Jeff Huang <s8321414@chakralinux.org>
2019-03-03 10:22:39 +0800

    Add zh-tw translations.

---
+++ resources/lang/zh-tw/profile.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  'emptyTimeline'         => '這個使用者還沒有貼文！',
+  'emptyFollowers'        => '這個使用者還沒有追蹤者！',
+  'emptyFollowing'        => '這個使用者還沒有追蹤任何人！',
+  'emptySaved'            => '您還沒有儲存任何貼文！',
+  'savedWarning'          => '您可以看到的只有您已儲存的貼文',
+  'privateProfileWarning' => '這個帳號是私密帳號',
+  'alreadyFollow'         => '已經追蹤 :username?',
+  'loginToSeeProfile'     => '來檢視他們的照片與影片。',
+
+  'status.disabled.header' 	  => '個人檔案不可用',
+  'status.disabled.body'	  => '抱歉，此個人檔案暫時不可用。請稍後再試。',
+];
