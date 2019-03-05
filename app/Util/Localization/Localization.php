c7e4e14b7eb434b4ff674be2dc5e1b1be23b3c84
Daniel Supernault <danielsupernault@gmail.com>
2019-03-05 12:16:21 -0700

    Update Localization cache time, closes #959

--- app/Util/Localization/Localization.php
+++ app/Util/Localization/Localization.php
@@ -9,7 +9,7 @@ class Localization {
 
 	public static function languages()
 	{
-		return Cache::remember('core:localization:languages', now()->addDays(14), function() {
+		return Cache::remember('core:localization:languages', now()->addDays(1), function() {
 			$dir = resource_path('lang');
 			return Arr::flatten(array_diff(scandir($dir), array('..', '.', 'vendor')));
 		});
