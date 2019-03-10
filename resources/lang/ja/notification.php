6a7de88ba5b57796ea5218248c1850d666688183
yoheizuho <yuuman002@gmail.com>
2019-03-11 04:22:02 +0900

    Japanese translate update

--- resources/lang/ja/notification.php
+++ resources/lang/ja/notification.php
@@ -4,5 +4,8 @@ return [
 
   'likedPhoto'          => '写真がお気に入りされました。',
   'startedFollowingYou' => 'フォローされました。',
+  'commented'           => '投稿にコメントされました。',
+  'mentionedYou'        => 'メンションされました。',
+  'shared'              => '投稿が再共有されました。',
 
 ];
---
+++ resources/lang/ja/site.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'about'         => 'このサーバーについて',
+	'help'          => 'ヘルプ',
+	'language'      => '言語',
+	'fediverse'     => '分散',
+	'opensource'	=> 'オープンソース',
+	'terms'         => '規約',
+	'privacy'       => 'プライバシー',
+	'l10nWip'       => '私達はローカライズサポートに取り組んでいます。',
+	'currentLocale' => '現在のロケール',
+	'selectLocale'  => 'リストの中からロケール（言語）を選択してください',
+];
