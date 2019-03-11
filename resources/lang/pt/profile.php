f4ede056189867209219a4c456e8fc5073196d8f
Rond <rondnelly@autistici.org>
2019-03-11 13:43:15 -0300

    portuguese translation

---
+++ resources/lang/pt/profile.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  'emptyTimeline'         => 'Este usuário não fez nenhum post!',
+  'emptyFollowers'        => 'Este usuário não tem seguidores!',
+  'emptyFollowing'        => 'Este usuário ainda não segue ninguém!',
+  'emptySaved'            => 'Você ainda não salvou nenhum post!',
+  'savedWarning'          => 'Só você pode ver o que foi salvo',
+  'privateProfileWarning' => 'Esta conta é fechada',
+  'alreadyFollow'         => 'Já segue :username?',
+  'loginToSeeProfile'     => 'para ver suas fotos e vídeos.',
+
+  'status.disabled.header' 	  => 'Perfil Indisponível',
+  'status.disabled.body'	  => 'Desculpe, este perfil não está disponível no momento. Por favor, tente novamente em breve.',
+];
---
+++ resources/lang/pt/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	'about'			=> 'Sobre',
+	'help'			=> 'Ajuda',
+	'language'		=> 'Idioma',
+	'fediverse'		=> 'Fediverso',
+	'opensource'	=> 'Código Aberto',
+	'terms'			=> 'Termos',
+	'privacy'		=> 'Privacidade',
+	'l10nWip'		=> 'Ainda estamos trabalhando na tradução',
+	'currentLocale' => 'Idioma atual',
+	'selectLocale'  => 'Escolha um dos idiomas disponíveis',
+
+];
