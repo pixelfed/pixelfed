4d690e19d9da57b22460953a5b5f3a0a3c909db8
yoheizuho <yuuman002@gmail.com>
2019-03-11 09:01:39 +0900

    Japanese translate update & fix typo

--- resources/lang/ja/navmenu.php
+++ resources/lang/ja/navmenu.php
@@ -2,18 +2,18 @@
 
 return [
 
-	'search'         => '検索',
-	'home'           => 'ホーム',
-	'local'          => 'ローカル',
-        'network'        => 'ネットワーク',
-	'discover'       => '見つける',
-	'viewMyProfile'  => '自分のプロフィールを見る',
-	'myTimeline'     => 'タイムライン',
+	'search' 	 => '検索',
+	'home'		 => 'ホーム',
+	'local'		 => 'ローカル',
+	'network'	 => 'ネットワーク',
+	'discover'	 => '見つける',
+	'viewMyProfile'	 => '自分のプロフィールを見る',
+	'myTimeline'	 => 'タイムライン',
 	'publicTimeline' => 'パブリックタイムライン',
-	'remoteFollow'   => 'リモートフォロー',
-	'settings'       => '設定',
-	'admin'          => '管理者',
-	'logout'         => 'ログアウト',
+	'remoteFollow'	 => 'リモートフォロー',
+	'settings'	 => '設定',
+	'admin'		 => '管理者',
+	'logout'	 => 'ログアウト',
 	'directMessages' => 'ダイレクトメッセージ',
 
 ];
