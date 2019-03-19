22dd2aea210961b06d56282dfb65c3cb84938420
Daniel Supernault <danielsupernault@gmail.com>
2019-03-18 22:05:56 -0600

    Update Help Center

--- resources/views/site/help/partial/sidebar.blade.php
+++ resources/views/site/help/partial/sidebar.blade.php
@@ -9,59 +9,59 @@
       <li class="nav-item {{request()->is('*/your-profile')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.your-profile')}}">{{__('helpcenter.profile')}}</a>
       </li>
-      <li class="nav-item {{request()->is('*/stories')?'active':''}}">
+      {{-- <li class="nav-item {{request()->is('*/stories')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.stories')}}">{{__('helpcenter.stories')}}</a>
-      </li>
+      </li> --}}
       <li class="nav-item {{request()->is('*/hashtags')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.hashtags')}}">{{__('helpcenter.hashtags')}}</a>
       </li>
-      <li class="nav-item {{request()->is('*/discover')?'active':''}}">
+      {{-- <li class="nav-item {{request()->is('*/discover')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.discover')}}">{{__('helpcenter.discover')}}</a>
-      </li>
-      <li class="nav-item {{request()->is('*/direct-messages')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/direct-messages')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.dm')}}">{{__('helpcenter.directMessages')}}</a>
-      </li>
-      <li class="nav-item {{request()->is('*/timelines')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/timelines')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.timelines')}}">{{__('helpcenter.timelines')}}</a>
-      </li>
-      <li class="nav-item {{request()->is('*/embed')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/embed')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.embed')}}">{{__('helpcenter.embed')}}</a>
-      </li>
+      </li> --}}
       <li class="nav-item">
         <hr>
       </li>
-      <li class="nav-item {{request()->is('*/community-guidelines')?'active':''}}">
+      {{-- <li class="nav-item {{request()->is('*/community-guidelines')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.community-guidelines')}}">
           {{__('helpcenter.communityGuidelines')}}
         </a>
-      </li>
-      <li class="nav-item {{request()->is('*/what-is-the-fediverse')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/what-is-the-fediverse')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.what-is-fediverse')}}">{{__('helpcenter.whatIsTheFediverse')}}</a>
-      </li>
-      <li class="nav-item {{request()->is('*/controlling-visibility')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/controlling-visibility')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.controlling-visibility')}}">
           {{__('helpcenter.controllingVisibility')}}
         </a>
-      </li>
-      <li class="nav-item {{request()->is('*/blocking-accounts')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/blocking-accounts')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.blocking-accounts')}}">
           {{__('helpcenter.blockingAccounts')}}
         </a>
-      </li>
+      </li> --}}
       <li class="nav-item {{request()->is('*/safety-tips')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.safety-tips')}}">
           {{__('helpcenter.safetyTips')}}
         </a>
       </li>
-      <li class="nav-item {{request()->is('*/report-something')?'active':''}}">
+      {{-- <li class="nav-item {{request()->is('*/report-something')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.report-something')}}">
           {{__('helpcenter.reportSomething')}}
         </a>
-      </li>
-      <li class="nav-item {{request()->is('*/data-policy')?'active':''}}">
+      </li> --}}
+      {{-- <li class="nav-item {{request()->is('*/data-policy')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('help.data-policy')}}">
           {{__('helpcenter.dataPolicy')}}
         </a>
-      </li>
+      </li> --}}
     </ul>
   </div>
