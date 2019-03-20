2b76db6421a4a7f92c43b27189446af6c2d9c579
Daniel Supernault <danielsupernault@gmail.com>
2019-03-19 20:51:45 -0600

    Update LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,7 +49,7 @@ class LoginController extends Controller
     public function validateLogin($request)
     {
         $rules = [
-            $this->username() => 'required|string',
+            $this->username() => 'required|email',
             'password'        => 'required|string',
         ];
 
@@ -70,6 +70,10 @@ class LoginController extends Controller
      */
     protected function authenticated($request, $user)
     {
+        if($user->status == 'deleted') {
+            return;
+        }
+
         $log = new AccountLog();
         $log->user_id = $user->id;
         $log->item_id = $user->id;
