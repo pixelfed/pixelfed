da80eef070aa1c7d9eb3f4f2c22e2fc5a43cb28e
Daniel Supernault <danielsupernault@gmail.com>
2019-04-01 23:38:10 -0600

    Update nav bar

--- resources/views/layouts/partial/nav.blade.php
+++ resources/views/layouts/partial/nav.blade.php
@@ -7,9 +7,9 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             @auth
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav mx-auto pr-3">
               <form class="form-inline search-bar" method="get" action="/i/results">
-                <div class="input-group mr-sm-2">
+                <div class="input-group">
                     <input class="form-control" name="q" placeholder="{{__('navmenu.search')}}" aria-label="search" autocomplete="off">
                     <div class="input-group-append">
                         <button class="btn btn-outline-primary" type="submit"><i class="fas fa-search"></i></button>
