24358c1acbc8b5166cfbb0eee037d0688fd6a535
Shleeble <ashley.hull@versent.com.au>
2019-04-19 01:32:36 +0800

    Update start.sh

--- contrib/docker/start.sh
+++ contrib/docker/start.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 # Create the storage tree if needed and fix permissions
-cp -r storage.skel/* storage/
-chown -R www-data:www-data storage/ bootstrap/cache/
+#cp -r storage.skel/* storage/
+chown -R www-data:www-data storage/ bootstrap/
 php artisan storage:link
 
 # Migrate database if the app was upgraded
-gosu www-data:www-data php artisan migrate --force
+# gosu www-data:www-data php artisan migrate --force
 
 # Run other specific migratins if required
-gosu www-data:www-data php artisan update
-
-# Run a worker if it is set as embedded
-if [ "$HORIZON_EMBED" = "true" ]; then
-  gosu www-data:www-data php artisan horizon &
-fi
+# gosu www-data:www-data php artisan update
 
 # Finally run Apache
 exec apache2-foreground
