cea44f9877e6e9cc0c2f6c5d1070273e1a039d1b
Daniel Supernault <danielsupernault@gmail.com>
2019-04-18 22:44:55 -0600

    Update settings partial

--- resources/views/settings/partial/sidebar.blade.php
+++ resources/views/settings/partial/sidebar.blade.php
@@ -6,37 +6,17 @@
       <li class="nav-item pl-3 {{request()->is('settings/password')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('settings.password')}}">Password</a>
       </li>
-      {{-- <li class="nav-item pl-3 {{request()->is('settings/email')?'active':''}}">
-        <a class="nav-link font-weight-light text-muted" href="{{route('settings.email')}}">Email</a>
-      </li>
-      <li class="nav-item pl-3 {{request()->is('settings/notifications')?'active':''}}">
-        <a class="nav-link font-weight-light text-muted" href="{{route('settings.notifications')}}">Notifications</a>
-      </li> --}}
-  
       <li class="nav-item pl-3 {{request()->is('settings/privacy*')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('settings.privacy')}}">Privacy</a>
       </li>
       <li class="nav-item pl-3 {{request()->is('settings/security*')?'active':''}}">
         <a class="nav-link font-weight-light text-muted" href="{{route('settings.security')}}">Security</a>
       </li>
-      {{-- <li class="nav-item">
+      <li class="nav-item">
         <hr>
       </li>
-      <li class="nav-item pl-3 {{request()->is('*import*')?'active':''}}">
-        <a class="nav-link font-weight-light text-muted" href="{{route('settings.import')}}">Import</a>
-      </li>
       <li class="nav-item pl-3 {{request()->is('settings/data-export')?'active':''}}">
-        <a class="nav-link font-weight-light text-muted" href="{{route('settings.dataexport')}}">Export</a>
+        <a class="nav-link font-weight-light text-muted" href="{{route('settings.dataexport')}}">Data Export</a>
       </li>
-      </li>
-      <li class="nav-item">
-      <hr>
-      </li>
-      <li class="nav-item pl-3 {{request()->is('settings/applications')?'active':''}}">
-        <a class="nav-link font-weight-light text-muted" href="#">Applications</a>
-      </li>
-      <li class="nav-item pl-3 {{request()->is('settings/developers')?'active':''}}">
-        <a class="nav-link font-weight-light text-muted" href="#">Developers</a>
-      </li> --}}
     </ul>
   </div>
