b43588d32089fcd6d20e79a9984179be12e98e36
Shleeble <ashley.hull@versent.com.au>
2019-04-22 10:36:58 +0800

    Update start.sh

--- contrib/docker/start.sh
+++ contrib/docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the storage tree if needed and fix permissions
-#cp -r storage.skel/* storage/
+cp -r storage.skel/* storage/
 chown -R www-data:www-data storage/ bootstrap/
 php artisan storage:link
 
