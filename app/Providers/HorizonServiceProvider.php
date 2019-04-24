fdfd090001eddd111bd0bba19475de50b2e6165d
Shleeble <ashley.hull@versent.com.au>
2019-04-24 22:23:25 +0800

    night mode was moved to boot in horizon PR..
    https://github.com/laravel/horizon/pull/537

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -20,6 +20,10 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         // Horizon::routeSmsNotificationsTo('15556667777');
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+        
+        if(config('horizon.darkmode') == true) {
+            Horizon::night();
+        }
     }
 
     /**
@@ -43,8 +47,6 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     public function register()
     {
-        if(config('horizon.darkmode') == true) {
-            Horizon::night();
-        }
+
     }
 }
