4f14a7dd749753bd1295561037572608c80aa56f
Daniel Supernault <danielsupernault@gmail.com>
2019-04-25 19:56:04 -0600

    Add snowflake ids to Collection models

--- app/Collection.php
+++ app/Collection.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Pixelfed\Snowflake\HasSnowflakePrimary;
 
 class Collection extends Model
 {
+	use HasSnowflakePrimary;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
 	public function profile()
 	{
 		return $this->belongsTo(Profile::class);
--- app/CollectionItem.php
+++ app/CollectionItem.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Pixelfed\Snowflake\HasSnowflakePrimary;
 
 class CollectionItem extends Model
 {
+	use HasSnowflakePrimary;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    
 	public function collection()
 	{
 		return $this->belongsTo(Collection::class);
