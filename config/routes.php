48089d83559629aaa7029bc86f501d06c9939640
Ashley Hull <ashley.hull@versent.com.au>
2019-05-02 17:40:27 +0800

    remove pusher

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,6 @@ return [
     'api' => [
       'base'        => config('app.url').'/api/1/',
       'sharedInbox' => config('app.url').'/api/sharedInbox',
-      'search'      => config('app.url').env('MIX_API_SEARCH'),
     ],
 
     'hashtag' => [
