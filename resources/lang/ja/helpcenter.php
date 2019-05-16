329d8d0811b26270f43a2dd7b3531c4027754d9b
yoheizuho <yuuman002@gmail.com>
2019-05-17 00:00:22 +0900

    japanese translation update

---
+++ resources/lang/ja/helpcenter.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+	'helpcenter' => 'ヘルプセンター',
+	'whatsnew' => 'What\'s New',
+
+	'gettingStarted' => 'はじめに',
+	'sharingMedia' => 'メディアを共有する',
+	'profile' => 'プロフィール',
+	'stories' => 'ストーリー',
+	'hashtags' => 'ハッシュダグ',
+	'discover' => '見つける',
+	'directMessages' => 'ダイレクトメッセージ',
+	'timelines' => 'タイムライン',
+
+	'embed'	=> '埋め込む',
+	'communityGuidelines' => 'コミュニティガイドライン',
+	'whatIsTheFediverse' => 'fediverseとは?',
+	'controllingVisibility' => '可視性制御',
+	'blockingAccounts' => 'ブロックしたアカウント',
+	'safetyTips' => '安全のためのヒント',
+	'reportSomething' => '報告する',
+	'dataPolicy' => 'データポリシー'
+
+];
