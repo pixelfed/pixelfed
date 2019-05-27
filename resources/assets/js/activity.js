08a96d47262367bf43e770997f553d932d7a8fb6
Daniel Supernault <danielsupernault@gmail.com>
2019-05-27 13:54:45 -0600

    Update activity.js

--- resources/assets/js/activity.js
+++ resources/assets/js/activity.js
@@ -1,10 +1,4 @@
-$(document).ready(function() {
-  $('.pagination').hide();
-  let elem = document.querySelector('.notification-page .list-group');
-  let infScroll = new InfiniteScroll( elem, {
-    path: '.pagination__next',
-    append: '.notification-page .list-group',
-    status: '.page-load-status',
-    history: true,
-  });
-});
+Vue.component(
+    'activity-component',
+    require('./components/Activity.vue').default
+);
