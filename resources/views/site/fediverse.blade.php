f411da95493890495b3cf2625c00315863686eb5
techknowlogick <hello@techknowlogick.com>
2019-06-03 20:02:16 -0400

    Fix missing tag start

--- resources/views/site/fediverse.blade.php
+++ resources/views/site/fediverse.blade.php
@@ -7,7 +7,7 @@
   </div>
   <hr>
   <section>
-    <p class="lead">,a href="http://fediverse.party/">Fediverse</a> is a portmanteau of “federation” and “universe”. It is a common, informal name for a federation of social network servers, specializing in different types of media.</p>
+    <p class="lead"><a href="http://fediverse.party/">Fediverse</a> is a portmanteau of “federation” and “universe”. It is a common, informal name for a federation of social network servers, specializing in different types of media.</p>
     <p class="lead font-weight-bold text-muted mt-4 mb-0">Supported Fediverse Projects</p>
     <p class="small text-muted">Some of the better known fediverse projects include:</p>
     <ul class="lead pl-4">
