6cbd9bbea0937d62783323dbba7a077daf44ddf2
Daniel Supernault <danielsupernault@gmail.com>
2019-06-06 21:25:48 -0600

    Update admin contact email view

--- resources/views/emails/contact/admin.blade.php
+++ resources/views/emails/contact/admin.blade.php
@@ -3,11 +3,18 @@
 
 <br>
 
+@if($contact->response_requested)
+**This user has requested a response from you.**
+@endif
+
+<br>
+
 [**{{$contact->user->username}}**]({{$contact->user->url()}}) has sent the following message:
 
 @component('mail::panel')
 {{ $contact->message }}
 @endcomponent
+
 
 @component('mail::button', ['url' => $contact->adminUrl(), 'color' => 'primary'])
 View Message
