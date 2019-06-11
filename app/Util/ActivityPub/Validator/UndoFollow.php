3efd27f847dd527207413a59702edab63b7c1274
Daniel Supernault <danielsupernault@gmail.com>
2019-06-11 14:12:16 -0600

    Add Undo Follow validator

---
+++ app/Util/ActivityPub/Validator/UndoFollow.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Util\ActivityPub\Validator;
+
+use Validator;
+use Illuminate\Validation\Rule;
+
+class UndoFollow {
+
+	public static function validate($payload)
+	{
+		$valid = Validator::make($payload, [
+			'@context' => 'required',
+			'id' => 'required|string',
+			'type' => [
+				'required',
+				Rule::in(['Undo'])
+			],
+			'actor' => 'required|url',
+			'object.actor' => 'required|url',
+			'object.object' => 'required|url',
+			'object.type' => [
+				'required',
+				Rule::in(['Follow'])
+			],
+		])->passes();
+
+		return $valid;
+	}
+}
