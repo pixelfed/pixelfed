1f8fd484f7745d3d6dcea6137dca1f837310e849
Daniel Supernault <danielsupernault@gmail.com>
2019-06-10 22:33:35 -0600

    Update FixUsernames command to skip action by default

--- app/Console/Commands/FixUsernames.php
+++ app/Console/Commands/FixUsernames.php
@@ -71,7 +71,7 @@ class FixUsernames extends Command
             foreach($affected as $u) {
                 $old = $u->username;
                 $this->info("Found user: {$old}");
-                $opt = $this->choice('Select fix method:', $opts, 0);
+                $opt = $this->choice('Select fix method:', $opts, 3);
 
                 switch ($opt) {
                     case $opts[0]:
