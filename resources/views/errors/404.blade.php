7282e428912e369dc853491aca01fabb2e9cd424
Daniel Supernault <danielsupernault@gmail.com>
2019-07-07 22:16:32 -0600

    Update error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,13 +2,9 @@
 
 @section('content')
 <div class="container">
-  <div class="error-page py-5 my-5">
-    <div class="card mx-5">
-      <div class="card-body p-5 text-center">
-        <h1>Page Not Found</h1>
-        <img src="/img/fred1.gif" class="img-fluid">
-      </div>
-    </div>
+  <div class="error-page py-5 my-5 text-center">
+    <h3 class="font-weight-bold">Sorry, this page isn't available.</h3>
+    <p class="lead">The link you followed may be broken, or the page may have been removed. <a href="/">Go back to Pixelfed.</a></p>
   </div>
 </div>
 @endsection
