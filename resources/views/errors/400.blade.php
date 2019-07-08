9d0720e295b258df8774372fa56e8d5623fbfaa2
Daniel Supernault <danielsupernault@gmail.com>
2019-07-08 00:14:20 -0600

    Update error views

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="error-page py-5 my-5 text-center">
+    <h3 class="font-weight-bold">Sorry, this page isn't available.</h3>
+    <p class="lead">The link you followed may be broken, or the page may have been removed. <a href="/">Go back to Pixelfed.</a></p>
+  </div>
+</div>
+@endsection
