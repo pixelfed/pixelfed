3b0801589d9d3d7657eab2e6833a8f9e03fc27c8
Daniel Supernault <danielsupernault@gmail.com>
2019-07-11 13:15:24 -0600

    Fixes #1496

---
+++ resources/lang/oc/helpcenter.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+	'helpcenter' => 'Seccion d\'ajuda',
+	'whatsnew' => 'Qué de nòu',
+
+	'gettingStarted' => 'Començar',
+	'sharingMedia' => 'Partatge de mèdias',
+	'profile' => 'Perfil',
+	'stories' => 'Stories',
+	'hashtags' => 'Etiquetas',
+	'discover' => 'Descobrir',
+	'directMessages' => 'Messatges dirèctes',
+	'timelines' => 'Cronologias',
+	'embed'	=> 'Integrar',
+
+	'communityGuidelines' => 'Linhas directrises de la comunitat',
+	'whatIsTheFediverse' => 'Qu\'es aquò lo Fedivers ?',
+	'controllingVisibility' => 'Contraròtle de la visibilitat',
+	'blockingAccounts' => 'Blocatge de compte',
+	'safetyTips' => 'Astúcias de seguretat',
+	'reportSomething' => 'Senhalar quicòm',
+	'dataPolicy' => 'Politica de donadas'
+
+];
