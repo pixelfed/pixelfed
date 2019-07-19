b88d020818684c7eb0cdaba913285d2b1bad9bf4
Daniel Supernault <danielsupernault@gmail.com>
2019-07-18 19:41:53 -0600

    Update config

--- config/federation.php
+++ config/federation.php
@@ -17,7 +17,7 @@ return [
 		'inbox' => env('AP_INBOX', true),
 		'sharedInbox' => env('AP_SHAREDINBOX', false),
 
-		'remoteFollow' => env('AP_REMOTE_FOLLOW'),
+		'remoteFollow' => env('AP_REMOTE_FOLLOW', false),
 
 		'delivery' => [
 			'timeout' => env('ACTIVITYPUB_DELIVERY_TIMEOUT', 2.0),
