223ee74c801ec1f3d035b22195b1afb7c997799f
Daniel Supernault <danielsupernault@gmail.com>
2019-07-27 00:05:41 -0600

    Update 503 view, allow custom text

--- config/instance.php
+++ config/instance.php
@@ -29,5 +29,11 @@ return [
 		]
 	],
 
+	'page' => [
+		'503' => [
+			'header' => env('PAGE_503_HEADER', 'Service Unavailable'),
+			'body' => env('PAGE_503_BODY', 'Our service is in maintenance mode, please try again later.')
+		]
+	],
 
 ];
