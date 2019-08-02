5a1aba18bcda165a3ef6d92b3ddaa14fcfdb52f1
Daniel Supernault <danielsupernault@gmail.com>
2019-08-01 22:39:50 -0600

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,5 +11,11 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error('CSRF token not found.');
+}
+
+window.App = {};
+
+window.App.boot = function() {
+	new Vue({ el: '#content'});
 }
