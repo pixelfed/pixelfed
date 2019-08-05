3d656aac21c11b8afc6072f6601c1399756165bb
Daniel Supernault <danielsupernault@gmail.com>
2019-08-04 23:53:27 -0600

    Update discover view

--- resources/views/discover/home.blade.php
+++ resources/views/discover/home.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5 pt-3">
-  <section>
-    <discover-component></discover-component>
-  </section>
-</div>
+<discover-component></discover-component>
 @endsection
 
 @push('scripts')
