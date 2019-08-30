37b88bcb1b8ed28eccdf3bad20d64fc72768fe27
Daniel Supernault <danielsupernault@gmail.com>
2019-08-29 19:27:34 -0600

    Update PlaceController, add place directory methods

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -12,12 +12,33 @@ class PlaceController extends Controller
 {
     public function show(Request $request, $id, $slug)
     {
-        // TODO: Replace with vue component + apis
     	$place = Place::whereSlug($slug)->findOrFail($id);
     	$posts = Status::wherePlaceId($place->id)
+            ->whereNull('uri')
     		->whereScope('public')
     		->orderByDesc('created_at')
-    		->paginate(10);
+    		->simplePaginate(10);
+
     	return view('discover.places.show', compact('place', 'posts'));
+    }
+
+    public function directoryHome(Request $request)
+    {
+        $places = Place::select('country')
+            ->distinct('country')
+            ->simplePaginate(48);
+
+        return view('discover.places.directory.home', compact('places'));
+    }
+
+    public function directoryCities(Request $request, $country)
+    {
+        $country = urldecode($country);
+        $places = Place::whereCountry($country)
+            ->orderBy('name', 'asc')
+            ->distinct('name')
+            ->simplePaginate(48);
+
+        return view('discover.places.directory.cities', compact('places'));
     }
 }
