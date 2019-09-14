ec24dec2dfe446747842a91ebeeca1c40fd84f9f
Daniel Supernault <danielsupernault@gmail.com>
2019-09-13 21:48:16 -0600

    Update ApiController

--- app/Http/Controllers/Api/ApiV1Controller.php
+++ app/Http/Controllers/Api/ApiV1Controller.php
@@ -8,18 +8,32 @@ use Illuminate\Support\Str;
 use App\Jobs\StatusPipeline\StatusDelete;
 use Laravel\Passport\Passport;
 use Auth, Cache, DB;
-use Carbon\Carbon;
 use App\{
     Like,
     Media,
     Profile,
     Status
 };
+use League\Fractal;
+use App\Transformer\Api\{
+    AccountTransformer,
+    RelationshipTransformer,
+    StatusTransformer,
+};
+use League\Fractal\Serializer\ArraySerializer;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 use App\Services\NotificationService;
 
-class ApiV1Controller extends Controller {
+class ApiV1Controller extends Controller 
+{
+	protected $fractal;
 
+	public function __construct()
+	{
+		$this->fractal = new Fractal\Manager();
+		$this->fractal->setSerializer(new ArraySerializer());
+	}
 	public function apps(Request $request)
 	{
 		abort_if(!config('pixelfed.oauth_enabled'), 404);
@@ -53,5 +67,14 @@ class ApiV1Controller extends Controller {
         	'vapid_key' => null
         ];
         return $res;
+	}
+
+	public function accountById(Request $request, $id)
+	{
+		$profile = Profile::whereNull('status')->findOrFail($id);
+		$resource = new Fractal\Resource\Item($profile, new AccountTransformer());
+		$res = $this->fractal->createData($resource)->toArray();
+
+		return response()->json($res);
 	}
 }
