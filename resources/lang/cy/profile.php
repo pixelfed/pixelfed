28dafdc767c421e19d32ce0b4d58593159643d70
mynameismonkey <3419832+mynameismonkey@users.noreply.github.com>
2019-09-16 16:43:02 -0400

    Update profile.php

--- resources/lang/cy/profile.php
+++ resources/lang/cy/profile.php
@@ -7,7 +7,7 @@ return [
   'emptyFollowing'        => 'Nid yw\'r defnyddiwr hwn yn dilyn unrhyw un eto!',
   'emptySaved'            => 'Nid ydych wedi arbed unrhyw bostiadau eto!',
   'savedWarning'          => 'Dim ond chi sy\'n gallu gweld beth rydych chi wedi\'i arbed',
-  'privateProfileWarning' => 'Mae'r Cyfrif hwn yn Breifat',
+  'privateProfileWarning' => 'Mae\'r Cyfrif hwn yn Breifat',
   'alreadyFollow'         => 'Eisoes yn dilyn :username?',
   'loginToSeeProfile'     => 'i weld eu lluniau a\'u fideos.',
   
