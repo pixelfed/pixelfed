9dc2234bacd73d7a173dfeb60153be16b348a799
Daniel Supernault <danielsupernault@gmail.com>
2019-09-20 21:25:02 -0600

    Update api routes

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,18 @@ use Illuminate\Http\Request;
 
 Route::post('/users/{username}/inbox', 'FederationController@userInbox');
 
-Route::post('/api/v1/apps', 'Api\ApiV1Controller@apps');
+
+Route::group(['prefix' => 'api'], function() {
+	Route::group(['prefix' => 'v1'], function() {
+		Route::post('apps', 'Api\ApiV1Controller@apps');
+
+		Route::get('accounts/verify_credentials', 'ApiController@verifyCredentials');
+		Route::get('accounts/relationships', 'PublicApiController@relationships');
+		Route::get('accounts/{id}/statuses', 'PublicApiController@accountStatuses');
+		Route::get('accounts/{id}/following', 'PublicApiController@accountFollowing');
+		Route::get('accounts/{id}/followers', 'PublicApiController@accountFollowers');
+		Route::get('accounts/{id}', 'Api\ApiV1Controller@accountById');
+
+		Route::get('instance', 'Api\ApiV1Controller@instance');
+	});
+});
