0756f477fe96bdd9cd80e99534ef5e589a698736
Anton Strömkvist <anton@stromkvist.com>
2019-10-01 14:27:12 +0200

    Translate notification.php to German

--- resources/lang/de/notification.php
+++ resources/lang/de/notification.php
@@ -3,8 +3,10 @@
 return [
 
   'likedPhoto'          => 'gefällt dein Foto.',
+  'likedComment'        => 'gefällt dein Kommentar.',
   'startedFollowingYou' => 'folgt dir nun.',
   'commented'           => 'hat deinen Post kommentiert.',
   'mentionedYou'        => 'hat dich erwähnt.',
+  'shared'              => 'hat deinen Post teilen.',
 
 ];
