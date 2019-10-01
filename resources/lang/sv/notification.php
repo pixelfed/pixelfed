aa914e6f628577f9200d910d771cde628d48abfd
Anton Strömkvist <anton@stromkvist.com>
2019-10-01 14:09:12 +0200

    Translate notification.php to Swedish
    Translate new strings

--- resources/lang/sv/notification.php
+++ resources/lang/sv/notification.php
@@ -3,6 +3,10 @@
 return [
 
   'likedPhoto'          => 'gillade ditt foto.',
+  'likedComment'        => 'gillade din kommentar.',
   'startedFollowingYou' => 'började följa dig.',
+  'commented'           => 'kommenterade ditt foto.',
+  'mentionedYou'        => 'nämnde dig.',
+  'shared'				=> 'delade ditt foto.',
 
 ];
