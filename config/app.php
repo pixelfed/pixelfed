e5af5dd12b0e6f38e6ef4c74a20b0576be48d90f
Daniel Supernault <danielsupernault@gmail.com>
2019-10-01 21:20:18 -0600

    Update service providers

--- config/app.php
+++ config/app.php
@@ -150,8 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Cors\ServiceProvider::class,
-        
+
         /*
          * Application Service Providers...
          */
