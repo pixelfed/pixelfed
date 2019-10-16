31b114cc0befabbb425ab06ba2f5500961320b68
Daniel Supernault <danielsupernault@gmail.com>
2019-10-15 21:37:44 -0600

    Update album presenters, use better carousel

--- resources/assets/js/components/presenter/PhotoAlbumPresenter.vue
+++ resources/assets/js/components/presenter/PhotoAlbumPresenter.vue
@@ -1,11 +1,11 @@
 <template>
 	<div v-if="status.sensitive == true">
 		<details class="details-animated">
-			<summary>
+			<summary @click="loadSensitive">
 				<p class="mb-0 lead font-weight-bold">{{ status.spoiler_text ? status.spoiler_text : 'CW / NSFW / Hidden Media'}}</p>
 				<p class="font-weight-light">(click to show)</p>
 			</summary>
-			<b-carousel :id="status.id + '-carousel'"
+			<!-- <b-carousel :id="status.id + '-carousel'"
 				v-model="cursor"
 				style="text-shadow: 1px 1px 2px #333;min-height: 330px;display: flex;align-items: center;"
 				controls
@@ -20,11 +20,16 @@
 				<span class="badge badge-dark box-shadow" style="position: absolute;top:10px;right:10px;">
 					{{cursor + 1}} / {{status.media_attachments.length}}
 				</span>
-			</b-carousel>
+			</b-carousel> -->
+			<carousel ref="carousel" :centerMode="true" :loop="false" :per-page="1" :paginationPosition="'bottom-overlay'" paginationActiveColor="#3897f0" paginationColor="#dbdbdb">
+				<slide v-for="(img, index) in status.media_attachments" :key="'px-carousel-'+img.id + '-' + index" class="w-100 h-100 d-block mx-auto text-center" style="max-height: 600px;" :title="img.description">
+					<img :class="img.filter_class + ' img-fluid'" style="max-height: 600px;" :src="img.url" :alt="img.description">
+				</slide>
+			</carousel>
 		</details>
 	</div>
-	<div v-else>
-		<b-carousel :id="status.id + '-carousel'"
+	<div v-else class="w-100 h-100 p-0">
+		<!-- <b-carousel :id="status.id + '-carousel'"
 			v-model="cursor"
 			style="text-shadow: 1px 1px 2px #333;min-height: 330px;display: flex;align-items: center;"
 			controls
@@ -39,7 +44,12 @@
 			<span class="badge badge-dark box-shadow" style="position: absolute;top:10px;right:10px;">
 				{{cursor + 1}} / {{status.media_attachments.length}}
 			</span>
-		</b-carousel>
+		</b-carousel> -->
+		<carousel ref="carousel" :centerMode="true" :loop="false" :per-page="1" :paginationPosition="'bottom-overlay'" paginationActiveColor="#3897f0" paginationColor="#dbdbdb" class="p-0 m-0">
+			<slide v-for="(img, index) in status.media_attachments" :key="'px-carousel-'+img.id + '-' + index" class="" style="background: #000; display: flex;align-items: center;max-height: 600px;" :title="img.description">
+				<img :class="img.filter_class + ' img-fluid w-100 p-0'" style="max-height: 600px;" :src="img.url" :alt="img.description">
+			</slide>
+		</carousel>
 	</div>
 </template>
 
@@ -57,6 +67,42 @@
 		data() {
 			return {
 				cursor: 0
+			}
+		},
+
+		created() {
+			// window.addEventListener("keydown", this.keypressNavigation);
+		},
+
+		beforeDestroy() {
+			// window.removeEventListener("keydown", this.keypressNavigation);
+		},
+
+		methods: {
+			loadSensitive() {
+				this.$refs.carousel.onResize();
+				this.$refs.carousel.goToPage(0);
+			},
+
+			keypressNavigation(e) {
+				let ref = this.$refs.carousel;
+				if (e.keyCode == "37") {
+					e.preventDefault();
+
+					let direction = "backward";
+
+					ref.advancePage(direction);
+					ref.$emit("navigation-click", direction);
+				}
+
+				if (e.keyCode == "39") {
+					e.preventDefault();
+
+					let direction = "forward";
+
+					ref.advancePage(direction);
+					ref.$emit("navigation-click", direction);
+				}
 			}
 		}
 	}
