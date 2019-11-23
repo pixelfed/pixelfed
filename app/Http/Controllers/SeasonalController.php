1856dd962d8c371d701af7f266e2f81e157cf474
Daniel Supernault <danielsupernault@gmail.com>
2019-11-22 23:35:51 -0700

    Add SeasonalController

---
+++ app/Http/Controllers/SeasonalController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SeasonalController extends Controller
+{
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+
+    public function yearInReview()
+    {
+    	$profile = Auth::user()->profile;
+    	return view('account.yir', compact('profile'));
+    }
+}
