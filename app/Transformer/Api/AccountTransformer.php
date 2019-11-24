3f0c45f60111df225bcee20547c3e8bf2924f5b2
Daniel Supernault <danielsupernault@gmail.com>
2019-11-24 15:48:02 -0700

    Update AccountTransformer

--- app/Transformer/Api/AccountTransformer.php
+++ app/Transformer/Api/AccountTransformer.php
@@ -33,6 +33,7 @@ class AccountTransformer extends Fractal\TransformerAbstract
 			'website' => $profile->website,
 			'local' => (bool) $local,
 			'is_admin' => (bool) $is_admin,
+			'created_at' => $profile->created_at->timestamp
 		];
 	}
 
