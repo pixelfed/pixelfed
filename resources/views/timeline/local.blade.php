311b9639936c800e21c35cdef3729cad1d22cde8
Daniel Supernault <danielsupernault@gmail.com>
2019-11-24 16:13:48 -0700

    Update timeline layouts

--- resources/views/timeline/local.blade.php
+++ resources/views/timeline/local.blade.php
@@ -2,9 +2,22 @@
 
 @section('content')
 
-<timeline scope="local"></timeline>
+<timeline scope="local" layout="feed"></timeline>
 
 @endsection
+
+@if($layout == 'grid')
+@push('styles')
+<style type="text/css">
+	body {
+		background: #fff !important;
+	}
+	.navbar.border-bottom {
+		border-bottom: none !important;
+	}
+</style>
+@endpush
+@endif
 
 @push('scripts')
 <script type="text/javascript" src="{{ mix('js/timeline.js') }}"></script>
