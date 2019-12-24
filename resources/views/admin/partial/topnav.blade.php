77b3b3894a7a89ff787af31dd5cb674068743ffb
Daniel Supernault <danielsupernault@gmail.com>
2019-12-23 23:53:27 -0700

    Update admin nav menu

--- resources/views/admin/partial/topnav.blade.php
+++ resources/views/admin/partial/topnav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#topbarNav" aria-controls="topbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -10,9 +10,6 @@
         </li>
         <li class="nav-item mx-2 {{request()->is('*messages*')?'active':''}}">
           <a class="nav-link font-weight-lighter text-muted" href="{{route('admin.messages')}}">Messages</a>
-        </li>
-        <li class="nav-item mx-2 {{request()->is('*hashtags*')?'active':''}}">
-          <a class="nav-link font-weight-lighter text-muted" href="{{route('admin.hashtags')}}">Hashtags</a>
         </li>
         <li class="nav-item mx-2 {{request()->is('*instances*')?'active':''}}">
           <a class="nav-link font-weight-lighter text-muted" href="{{route('admin.instances')}}">Instances</a>
@@ -32,13 +29,15 @@
         <li class="nav-item mx-2 {{request()->is('*users*')?'active':''}}">
           <a class="nav-link font-weight-lighter text-muted" href="{{route('admin.users')}}">Users</a>
         </li>
-        <li class="nav-item dropdown mx-2 {{request()->is(['*settings*','*discover*'])?'active':''}}">
+        <li class="nav-item dropdown mx-2 {{request()->is(['*settings*','*discover*', '*site-news*'])?'active':''}}">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             More
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
             <a class="dropdown-item font-weight-bold {{request()->is('*apps*')?'active':''}}" href="{{route('admin.apps')}}">Apps</a>
             <a class="dropdown-item font-weight-bold {{request()->is('*discover*')?'active':''}}" href="{{route('admin.discover')}}">Discover</a>
+            <a class="dropdown-item font-weight-bold {{request()->is('*hashtags*')?'active':''}}" href="{{route('admin.hashtags')}}">Hashtags</a>
+            <a class="dropdown-item font-weight-bold {{request()->is('*site-news*')?'active':''}}" href="/i/admin/site-news">Newsroom</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item font-weight-bold" href="/horizon">Horizon</a>
             {{-- <a class="dropdown-item font-weight-bold" href="#">Websockets</a> --}}
