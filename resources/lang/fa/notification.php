9bebd1452758f5705d06497f623d5489f8096647
amin <djawadi@riseup.net>
2020-01-04 16:21:57 +0330

    Add Persian translation

---
+++ resources/lang/fa/notification.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+  'likedPhoto'          => 'پست شما را پسندید.',
+  'likedComment'        => 'دیدگاه شما را پسندید.',
+  'startedFollowingYou' => 'شما را دنبال می‌کند.',
+  'commented'           => 'دیدگاهی روی پست شما نوشت.',
+  'mentionedYou'        => 'شما را صدا کرد.',
+  'shared'				      => 'پستی منتشر کرد.',
+
+];
---
+++ resources/lang/fa/timeline.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+  'emptyPersonalTimeline' => 'جدول زمانی شما خالی است.',
+
+];
