39296263801c4a1cce63e40ed1019ae5673036d5
Shlee <github@shl.ee>
2020-02-13 02:16:57 +0800

    Create start.fpm.sh

---
+++ contrib/docker/start.fpm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create the storage tree if needed and fix permissions
+cp -r storage.skel/* storage/
+chown -R www-data:www-data storage/ bootstrap/
+
+# Refresh the environment
+php artisan storage:link
+php artisan horizon:assets
+php artisan route:cache
+php artisan view:cache
+php artisan config:cache
+
+# Finally run FPM
+exec php-fpm
