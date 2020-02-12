e8d6007d9029bae9d7d062c5c358f06999680754
Shlee <github@shl.ee>
2020-02-12 23:22:26 +0800

    Update start.sh

--- contrib/docker/start.sh
+++ contrib/docker/start.sh
@@ -11,16 +11,8 @@ php artisan route:cache
 php artisan view:cache
 php artisan config:cache
 
-# Migrate database if the app was upgraded
-# gosu www-data:www-data php artisan migrate --force
-
 # Run other specific migratins if required
 # gosu www-data:www-data php artisan update
-
-# Run a worker if it is set as embedded
-if [ "$HORIZON_EMBED" = "true" ]; then
-  gosu www-data:www-data php artisan horizon &
-fi
 
 # Finally run Apache
 exec apache2-foreground
