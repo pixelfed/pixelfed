b16d2dbefb9a4654aea298f7df161cfac0c1e7b5
Shlee <github@shl.ee>
2020-02-16 19:30:10 +0800

    Update passwords.php

--- resources/lang/ca/passwords.php
+++ resources/lang/ca/passwords.php
@@ -11,7 +11,7 @@ return [
     |
     */
     'password' => 'Cal que la contrasenya sigui d\'un mínim de sis caràcters i que coincideixi amb la confirmació.',
-    'reset'    => 'S\'ha reinicialitzat la vostra contrasenya!',
+    'reset'    => 'S'ha reinicialitzat la vostra contrasenya!',
     'sent'     => 'Si la vostra adreça electrònica existeix a la nostra base de dades, en breus minuts rebreu un enllaç de recuperació a la vostra bústia. Si us plau, si no rebeu el missatge, comproveu la bústia de correu brossa.',
     'token'    => 'Aquest testimoni (token) de reinicialització de la contrasenya no és vàlid.',
     'user'     => 'Si la vostra adreça electrònica existeix a la nostra base de dades, en breus minuts rebreu un enllaç de recuperació a la vostra bústia. Si us plau, si no rebeu el missatge, comproveu la bústia de correu brossa.',
