51642fc40d27ca844ad734fd57086717beaba75d
Daniel Supernault <danielsupernault@gmail.com>
2020-02-21 21:00:00 -0700

    Add modlog notifications

--- app/Services/ModLogService.php
+++ app/Services/ModLogService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use Auth;
 use App\ModLog;
+use App\Notification;
 use App\User;
 
 class ModLogService {
@@ -94,5 +96,47 @@ class ModLogService {
 		} else {
 			return;
 		}
+	}
+
+	public function load($modLog)
+	{
+		$this->log = $modLog;
+		return $this;
+	}
+
+	public function fanout()
+	{
+		$log = $this->log;
+
+		$msg = "{$log->user_username} commented on a modlog";
+		$rendered = "<span class='font-weight-bold'>{$log->user_username}</span> commented on a <a href='/i/admin/users/modlogs/{$log->user_id}}' class='font-weight-bold text-decoration-none'>modlog</a>";
+		$item_id = $log->id;
+		$item_type = 'App\ModLog';
+		$action = 'admin.user.modlog.comment';
+
+		$admins = User::whereNull('status')
+			->whereNotIn('id', [$log->user_id])
+			->whereIsAdmin(true)
+			->pluck('profile_id')
+			->toArray();
+
+		foreach($admins as $user) {
+			$n = new Notification;
+			$n->profile_id = $user;
+			$n->actor_id = $log->admin->profile_id;
+			$n->item_id = $item_id;
+			$n->item_type = $item_type;
+			$n->action = $action;
+			$n->message = $msg;
+			$n->rendered = $rendered;
+			$n->save();
+		}
+	}
+
+	public function unfanout()
+	{
+		Notification::whereItemType('App\ModLog')
+			->whereItemId($this->log->id)
+			->delete();
 	}
 }
