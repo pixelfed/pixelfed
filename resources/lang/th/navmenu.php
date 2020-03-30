603fdbc46a67fee10ecab2da21f327e15cee7194
Valso <warut92@gmail.com>
2020-03-30 16:04:43 +0700

    thai translation, first version

---
+++ resources/lang/th/navmenu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'search'			=> 'ค้นหา',
+	'home'				=> 'หน้าหลัก',
+	'local'				=> 'Local',
+	'network'			=> 'Network',
+	'discover'			=> 'Discover',
+	'viewMyProfile'		=> 'ดูโพรไฟล์',
+	'myProfile'			=> 'โพรไฟล์ของฉัน',
+	'myTimeline'		=> 'ทามไลน์ของฉัน',
+	'publicTimeline'	=> 'ทามไลน์สาธารณะ',
+	'remoteFollow'		=> 'Remote Follow',
+	'settings'			=> 'ตั้งค่า',
+	'admin'				=> 'ผู้ดูแล',
+	'logout'			=> 'ออกจากระบบ',
+	'directMessages'	=> 'ส่งข้อความ',
+	'composePost'		=> 'สร้างโพสต์',
+];
---
+++ resources/lang/th/notification.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+  'likedPhoto'          => 'ถูกใจโพสต์ของคุณ',
+  'likedComment'        => 'ถูกใจความเห็นของคุณ',
+  'startedFollowingYou' => 'ได้ติดตามคุณแล้ว',
+  'commented'           => 'ได้แสดงความเห็นโพสต์ของคุณ',
+  'mentionedYou'        => 'พูดถึงคุณ',
+  'shared'				=> 'แชร์โพสต์ของคุณ',
+
+];
