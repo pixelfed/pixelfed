43c7bda6636a261d03bde0d83a7f0c9ca26253a4
Daniel Supernault <danielsupernault@gmail.com>
2020-04-17 19:35:36 -0600

    Update search results view

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -4,12 +4,9 @@
 	<search-results query="{{request()->query('q')}}" profile-id="{{Auth::user()->profile->id}}"></search-results>
 @endsection
 
+
 @push('scripts')
 <script type="text/javascript" src="{{mix('js/compose.js')}}"></script>
 <script type="text/javascript" src="{{mix('js/search.js')}}"></script>
-<script type="text/javascript">
-	new Vue({
-		el: '#content'
-	})
-</script>
+<script type="text/javascript">App.boot();</script>
 @endpush
