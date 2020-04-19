7282c852319c1fb1e5659b0d89f166f2dee0ddc7
Daniel Supernault <danielsupernault@gmail.com>
2020-04-19 14:01:29 -0600

    Update fediverse view

--- resources/views/site/fediverse.blade.php
+++ resources/views/site/fediverse.blade.php
@@ -11,13 +11,7 @@
     <p class="lead font-weight-bold text-muted mt-4 mb-0">Supported Fediverse Projects</p>
     <p class="small text-muted">Some of the better known fediverse projects include:</p>
     <ul class="lead pl-4">
-      <li><a href="https://anfora.app/" rel="nofollow noopener">Anfora</a> – Self-hosted photo gallery social network.</li>
-      @if(\Illuminate\Support\Str::contains(config('pixelfed.media_types'), 'video/mp4'))
-      <li><a href="https://joinpeertube.org/" rel="nofollow noopener">PeerTube</a> – A federated video streaming network.</li>
-      @endif
-      <li><a href="https://pleroma.social/" rel="nofollow noopener">Pleroma</a> – A federated microblogging alternative.</li>
       <li><a href="https://joinmastodon.org" rel="nofollow noopener">Mastodon</a> – A federated microblogging alternative.</li>
-      <li><a href="https://github.com/syuilo/misskey" rel="nofollow noopener">Misskey</a> – A federated microblogging alternative.</li>
     </ul>
   </section>
 @endsection
