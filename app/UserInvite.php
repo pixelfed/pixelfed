591a1929287396b5ad4677276d0fd66d5a0b0268
Daniel Supernault <danielsupernault@gmail.com>
2020-04-26 23:25:35 -0600

    Update UserInvite model, add sender relation

--- app/UserInvite.php
+++ app/UserInvite.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserInvite extends Model
 {
+	public function sender()
+	{
+		return $this->belongsTo(Profile::class, 'profile_id');
+	}
+
     public function url()
     {
-    	$path = '/i/invite/code';
-    	$url = url($path, [$this->key, $this->token]);
-    	return $url;
+    	return url("/i/invite/code/{$this->key}/{$this->token}");
     }
 }
