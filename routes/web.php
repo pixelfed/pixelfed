cfaa248c722700d845831968221a2c7c26990bde
Daniel Supernault <danielsupernault@gmail.com>
2020-05-15 23:32:54 -0600

    Update SiteController, add legacy profile/webfinger redirect

--- routes/web.php
+++ routes/web.php
@@ -428,5 +428,6 @@ Route::domain(config('pixelfed.domain.app'))->middleware(['validemail', 'twofact
     Route::get('p/{username}/{id}.json', 'StatusController@showObject');
     Route::get('p/{username}/{id}', 'StatusController@show');
     Route::get('{username}/embed', 'ProfileController@embed');
+    Route::get('@{username}', 'SiteController@legacyProfileRedirect');
     Route::get('{username}', 'ProfileController@show');
 });
