59ab7b33553ef04d111293e106665c10c57b8c2b
Xose M <704948+xmgz@users.noreply.github.com>
2020-11-21 18:36:08 +0100

    Update profile.php

--- resources/lang/gl/profile.php
+++ resources/lang/gl/profile.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
-  'emptyTimeline'  => 'Esta usuaria aínda non publicou!',
-  'emptyFollowers' => 'Esta usuaria aínda non ten seguidoras!',
-  'emptyFollowing' => 'Esta usuaria aínda non segue a ninguén!',
-  'savedWarning'   => 'Só vostede pode ver o que gardou',
+  'emptyTimeline'         => 'Esta usuaria aínda non publicou!',
+  'emptyFollowers'        => 'Esta usuaria aínda non ten seguidoras!',
+  'emptyFollowing'        => 'Esta usuaria aínda non segue a ninguén!',
+  'emptySaved'            => 'Aínda non gardaches ningunha publicación!',
+  'savedWarning'          => 'Só podes ver o que gardaches',
+  'privateProfileWarning' => 'Esta conta é Privada',
+  'alreadyFollow'         => 'Xa segues :username?',
+  'loginToSeeProfile'     => 'para ver as súas fotos e vídeos.',
+
+  'status.disabled.header' 	  => 'Perfil non dispoñible',
+  'status.disabled.body'	  => 'O perfil non está dipoñible neste intre, inténtao dentro dun anaco.',
 ];
