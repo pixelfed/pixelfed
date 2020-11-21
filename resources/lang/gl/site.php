26fc787f56d54a8aab3a1fe1f2a3ebbc9829780f
Xose M <704948+xmgz@users.noreply.github.com>
2020-11-21 18:38:32 +0100

    Update site.php

--- resources/lang/gl/site.php
+++ resources/lang/gl/site.php
@@ -1,13 +1,20 @@
 <?php
+
 return [
-	'about'			=> 'Acerca de',
-	'help'			=> 'Axuda',
-	'language'		=> 'Idioma',
-	'fediverse'		=> 'Fediverso',
-	'opensource'	=> 'Código aberto',
-	'terms'			=> 'Termos',
-	'privacy'		=> 'Intimidade',
-	'l10nWip'		=> 'Estamos a traballar no soporte da localización do servizo',
-	'currentLocale' => 'Locale actual',
-	'selectLocale'  => 'Escolla un dos idiomas admitidos',
+
+    'about'             => 'Acerca de',
+    'help'              => 'Axuda',
+    'language'          => 'Idioma',
+    'fediverse'         => 'Fediverso',
+    'opensource'        => 'Código aberto',
+    'terms'             => 'Termos',
+    'privacy'           => 'Privacidade',
+    'l10nWip'           => 'Estamos a traballar no sistema de tradución',
+    'currentLocale'     => 'Idioma actual',
+    'selectLocale'      => 'Elixe un dos idiomas incluídos',
+    'contact'           => 'Contacto',
+    'contact-us'        => 'Contacta con nós',
+    'places'            => 'Lugares',
+    'profiles'          => 'Perfís',
+
 ];
