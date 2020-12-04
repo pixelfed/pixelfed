c4df0070a1a787e253dace372203197869aecb0c
Voryzen <piro.travesura@gmail.com>
2020-12-04 21:52:52 +1000

    refactor(docker): fixes docker issues

--- contrib/docker/start.apache.sh
+++ contrib/docker/start.apache.sh
@@ -10,6 +10,8 @@ php artisan horizon:publish
 php artisan route:cache
 php artisan view:cache
 php artisan config:cache
+php artisan migrate --force -n
+php artisan passport:install
 
 # Finally run Apache
 apache2-foreground
