1b2fd665bf962427636f570cbabd1c1a1defb716
Daniel Supernault <danielsupernault@gmail.com>
2020-12-09 22:22:56 -0700

    Update compiled assets

---
+++ resources/views/profile/memory.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app',['title' => $profile->username . " on " . config('app.name')])
+
+@section('content')
+@if (session('error'))
+    <div class="alert alert-danger text-center font-weight-bold mb-0">
+        {{ session('error') }}
+    </div>
+@endif
+
+<memory-profile profile-id="{{$profile->id}}" profile-username="{{$profile->username}}" :profile-settings="{{json_encode($settings)}}" profile-layout="{{$profile->profile_layout ?? 'metro'}}"></memory-profile>
+@if($profile->website)
+<a class="d-none" href="{{$profile->website}}" rel="me">{{$profile->website}}</a>
+@endif
+
+<noscript>
+  <div class="container">
+    <p class="pt-5 text-center lead">Please enable javascript to view this content.</p>
+  </div>
+</noscript>
+
+@endsection
+
+@push('meta')<meta property="og:description" content="{{$profile->bio}}">
+  @if(false == $settings['crawlable'] || $profile->remote_url)
+  <meta name="robots" content="noindex, nofollow">
+  @else  <meta property="og:image" content="{{$profile->avatarUrl()}}">
+    <link href="{{$profile->permalink('.atom')}}" rel="alternate" title="{{$profile->username}} on Pixelfed" type="application/atom+xml">
+    <link href='{{$profile->permalink()}}' rel='alternate' type='application/activity+json'>
+  @endif
+@endpush
+
+@push('scripts')<script type="text/javascript" src="{{ mix('js/memoryprofile.js') }}"></script>
+    <script type="text/javascript" defer>App.boot();</script>
+
+@endpush
