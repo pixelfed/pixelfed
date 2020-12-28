3751e350e9b30744e979628322f375d5a426c556
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-12-28 13:39:42 +0100

    fix spaces to tabs

--- resources/assets/js/components/Timeline.vue
+++ resources/assets/js/components/Timeline.vue
@@ -833,14 +833,14 @@
 							if(i.href.startsWith(window.location.origin)) {
 								return;
 							}
-                                                        let tag = i.innerText;
-                                                        if(tag.startsWith('#')) {
-                                                          tag = '/discover/tags/'+tag.substr(1);
-                                                        }
-                                                        if(tag.startsWith('@')) {
-                                                          tag = '/'+tag;
-                                                        }
-                                                        i.href = tag+'?src=rph'; 
+							let tag = i.innerText;
+							if(tag.startsWith('#')) {
+								tag = '/discover/tags/'+tag.substr(1);
+							}
+							if(tag.startsWith('@')) {
+								tag = '/'+tag;
+							}
+							i.href = tag+'?src=rph'; 
 						});
 					}, 500);
 				}).catch(err => {
