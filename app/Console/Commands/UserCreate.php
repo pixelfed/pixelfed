2998adb54e9b407498ba7e4bae550897242b1cf9
Voryzen <piro.travesura@gmail.com>
2020-12-28 21:58:38 +1000

    allow user create with flags in CLI

--- app/Console/Commands/UserCreate.php
+++ app/Console/Commands/UserCreate.php
@@ -12,7 +12,7 @@ class UserCreate extends Command
      *
      * @var string
      */
-    protected $signature = 'user:create';
+    protected $signature = 'user:create {--name=} {--username=} {--email=} {--password} {--is_admin=}';
 
     /**
      * The console command description.
@@ -39,40 +39,36 @@ class UserCreate extends Command
     public function handle()
     {
         $this->info('Creating a new user...');
-
-        $name = $this->ask('Name');
-
-        $username = $this->ask('Username');
-
+        @empty ($name)
+            $name = $this->ask('Name');
+        @endempty
+        @empty ($username)
+            $username = $this->ask('Username');
+        @endempty
         if(User::whereUsername($username)->exists()) {
             $this->error('Username already in use, please try again...');
             exit;
         }
-
-        $email = $this->ask('Email');
-
+        @empty ($email)
+            $email = $this->ask('Email');
+            $confirm_email = $this->confirm('Manually verify email address?');
+        @endempty
         if(User::whereEmail($email)->exists()) {
             $this->error('Email already in use, please try again...');
             exit;
         }
-
-        $password = $this->secret('Password');
-        $confirm = $this->secret('Confirm Password');
-
-        if($password !== $confirm) {
-            $this->error('Password mismatch, please try again...');
-            exit;
-        }
-        
+        @empty ($password)
+            $password = $this->secret('Password');
+            $confirm = $this->secret('Confirm Password');
+            if($password !== $confirm) {
+                $this->error('Password mismatch, please try again...');
+                exit;
+            }
+        @endempty
+        @empty ($is_admin)
         $is_admin = $this->confirm('Make this user an admin?');
-        $confirm_email = $this->confirm('Manually verify email address?');
-
-        if($this->confirm('Are you sure you want to create this user?') && 
-            $username &&
-            $name && 
-            $email && 
-            $password
-        ) {
+        @endempty
+        {
             $user = new User;
             $user->username = $username;
             $user->name = $name;
