348692476cc45a8cdcc9d14a4c7bbe83b52f75f6
Daniel Supernault <danielsupernault@gmail.com>
2021-01-31 00:49:40 -0700

    Update DiscoverComponent, add spinner loaders and remove deprecated sections

--- resources/assets/js/components/DiscoverComponent.vue
+++ resources/assets/js/components/DiscoverComponent.vue
@@ -7,24 +7,8 @@
 			<div class="d-block d-md-none border-top-0 pt-3">
 				<input class="form-control rounded-pill shadow-sm" placeholder="Search" v-model="searchTerm" v-on:keyup.enter="searchSubmit">
 			</div>
-			<div class="pt-3">
-				<p class="d-block d-md-none h1 font-weight-bold text-lighter pt-3" style="opacity: 0.4"><i class="far fa-compass"></i> DISCOVER</p>
-				<p class="d-none d-md-block display-3 font-weight-bold text-lighter pt-3" style="opacity: 0.4"><i class="far fa-compass"></i> DISCOVER</p>
-			</div>
-			
 
-			<section v-if="hashtags.length" class="mb-4 pb-5 section-explore mt-4 pt-4">
-					<div class="lead">
-						<i class="fas fa-hashtag text-lighter fa-lg mr-3"></i>
-						<a v-for="(tag, index) in hashtags" :href="tag.url" class="badge badge-light rounded-pill border py-2 px-3 mr-2 mb-2 shadow-sm border-danger text-danger">{{tag.name}}</a>
-					</div>
-					<div class="lead mt-4">
-						<i class="fas fa-map-marker-alt text-lighter fa-lg mr-3"></i>
-						<a v-for="(tag, index) in places" :href="tag.url" class="badge badge-light rounded-pill border py-2 px-3 mr-2 mb-2 shadow-sm border-danger text-danger">{{tag.name}}, {{tag.country}}</a>
-					</div>
-			</section>
-
-			<section v-if="trending.length" class="mb-5 section-explore">
+			<section class="mt-3 mb-5 section-explore">
 				<div class="profile-timeline">
 					<div class="row p-0 mt-5">
 						<div class="col-12 mb-3 d-flex justify-content-between align-items-center">
@@ -38,8 +22,8 @@
 							</div>
 						</div>
 					</div>
-					<div class="row p-0" style="display: flex;">
-						<div v-for="(s, index) in trending.slice(0, 12)" class="col-4 p-1 p-sm-2 p-md-3 pt-0">
+					<div v-if="!trendingLoading" class="row p-0 d-flex">
+						<div v-if="trending.length" v-for="(s, index) in trending.slice(0, 12)" class="col-4 p-1 p-sm-2 p-md-3 pt-0">
 							<a class="card info-overlay card-md-border-0" :href="s.url">
 								<div class="square">
 									<div v-if="s.sensitive" class="square-content">
@@ -83,83 +67,28 @@
 								</div>
 							</a>
 						</div>
+						<div v-else class="col-12 d-flex align-items-center justify-content-center bg-light border" style="min-height: 40vh;">
+							<div class="h2">No posts found :(</div>
+						</div>
+					</div>
+					<div v-else class="row d-flex align-items-center justify-content-center bg-light border" style="min-height: 40vh;">
+						<div class="spinner-border" role="status">
+							<span class="sr-only">Loading...</span>
+						</div>
 					</div>
 				</div>
 			</section>
 
-			<section v-if="categories.length > 0" class="mb-5 section-explore">
+			<section class="pt-5 mb-5 section-explore">
 				<div class="profile-timeline pt-3">
 					<div class="row p-0 mt-5">
-						<div class="col-12 mb-4 d-flex justify-content-between align-items-center">
-							<p class="d-block d-md-none h1 font-weight-bold mb-0">Categories</p>
-							<p class="d-none d-md-block display-4 font-weight-bold mb-0">Categories</p>
-						</div>
-					</div>
-					<section class="d-none d-md-flex mb-md-2 discover-bar" style="width:auto; overflow: auto hidden;">
-						<a v-for="(category, index) in categories" :key="index+'_cat_'" class="bg-dark rounded d-inline-flex align-items-end justify-content-center mr-3 box-shadow card-disc text-decoration-none" :href="category.url" :style="'background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)),url('+category.thumb+');'">
-							<p class="text-white font-weight-bold" style="text-shadow: 3px 3px 16px #272634;">{{category.name}}</p>
-						</a>
-
-					</section>
-				</div>
-			</section>
-			<section v-if="categories.length > 0" class="py-5 mb-5 section-explore bg-warning rounded">
-				<div class="profile-timeline py-3">
-					<div class="row p-0 my-5">
-						<div class="col-12 mb-3 text-center text-dark">
-							<p class="d-none d-md-block display-3 font-weight-bold">Discover. Categories.</p>
-							<p class="d-block d-md-none h1 font-weight-bold">Discover. Categories.</p>
-							<p class="h4 font-weight-light mb-0">Discover amazing posts, people, places and hashtags.</p>
-						</div>
-					</div>
-				</div>
-			</section>
-			<section v-if="posts.length" class="pt-5 mb-5 section-explore">
-				<div class="profile-timeline pt-3">
-					<div class="row p-0 mt-5">
-						<!-- <div class="col-12 mb-3 d-flex justify-content-between align-items-center">
-							<p class="d-block d-md-none h1 font-weight-bold mb-0">Spotlight</p>
-							<p class="d-none d-md-block display-4 font-weight-bold mb-0">Spotlight</p>
-							<div>
-								<div class="btn-group">
-									<button class="btn btn-danger py-1 font-weight-bold px-3 text-uppercase btn-sm">Today</button>
-									<button class="btn btn-outline-danger py-1 font-weight-bold px-3 text-uppercase btn-sm">Yesterday</button>
-								</div>
-							</div>
-						</div> -->
-						<!-- <div class="col-12 col-md-6">
-							<div class="mb-4">
-								<a class="card info-overlay card-md-border-0" :href="posts[10].url">
-									<div class="square">
-										<span v-if="posts[10].type == 'photo:album'" class="float-right mr-3 post-icon"><i class="fas fa-images fa-2x"></i></span>
-										<span v-if="posts[10].type == 'video'" class="float-right mr-3 post-icon"><i class="fas fa-video fa-2x"></i></span>
-										<span v-if="posts[10].type == 'video:album'" class="float-right mr-3 post-icon"><i class="fas fa-film fa-2x"></i></span>
-										<div class="square-content" v-bind:style="{ 'background-image': 'url(' + posts[10].thumb + ')' }">
-										</div>
-									</div>
-								</a>
-							</div>				
-						</div>
-						<div class="col-12 col-md-6 row p-0 m-0">
-							<div v-for="(post, index) in posts.slice(11,15)" class="col-6" style="margin-bottom:1.8rem;">
-								<a class="card info-overlay card-md-border-0" :href="post.url">
-									<div class="square">
-										<span v-if="post.type == 'photo:album'" class="float-right mr-3 post-icon"><i class="fas fa-images fa-2x"></i></span>
-										<span v-if="post.type == 'video'" class="float-right mr-3 post-icon"><i class="fas fa-video fa-2x"></i></span>
-										<span v-if="post.type == 'video:album'" class="float-right mr-3 post-icon"><i class="fas fa-film fa-2x"></i></span>
-										<div class="square-content" v-bind:style="{ 'background-image': 'url(' + post.thumb + ')' }">
-										</div>
-									</div>
-								</a>
-							</div>
-						</div> -->
 						<div class="col-12 mb-3 d-flex justify-content-between align-items-center">
 							<p class="d-block d-md-none h1 font-weight-bold mb-0">For You</p>
 							<p class="d-none d-md-block display-4 font-weight-bold mb-0">For You</p>
 						</div>
 					</div>
-					<div class="row p-0" style="display: flex;">
-						<div v-for="(s, index) in posts" class="col-4 p-1 p-sm-2 p-md-3 pt-0">
+					<div v-if="!recommendedLoading" class="row p-0 d-flex">
+						<div v-if="posts.length" v-for="(s, index) in posts" :key="'rmki:'+index" class="col-4 p-1 p-sm-2 p-md-3 pt-0">
 							<a class="card info-overlay card-md-border-0" :href="s.url">
 								<div class="square">
 									<div v-if="s.sensitive" class="square-content">
@@ -203,87 +132,20 @@
 								</div>
 							</a>
 						</div>
+						<div v-else class="col-12 d-flex align-items-center justify-content-center bg-light border" style="min-height: 40vh;">
+							<div class="h2">No posts found :(</div>
+						</div>
+					</div>
+					<div v-else class="row d-flex align-items-center justify-content-center bg-light border" style="min-height: 40vh;">
+						<div class="spinner-border" role="status">
+							<span class="sr-only">Loading...</span>
+						</div>
 					</div>
 				</div>
 			</section>
-			<!-- <section class="pt-5 mb-5 section-explore">
-				<div class="profile-timeline pt-3">
-					<div class="row p-0 mt-5">
-						<div class="col-12 mb-3 d-flex justify-content-between align-items-center">
-							<p class="display-4 font-weight-bold mb-0">Recommended</p>
-							<!-- <div>
-								<div class="btn-group">
-									<button class="btn btn-dark py-1 font-weight-bold px-3 text-uppercase btn-sm">Today</button>
-									<button class="btn btn-outline-secondary py-1 font-weight-bold px-3 text-uppercase btn-sm">Weekly</button>
-									<button class="btn btn-outline-secondary py-1 font-weight-bold px-3 text-uppercase btn-sm">Monthly</button>
-								</div>
-							</div> - - ->
-						</div>
-						<div class="col-12 col-md-6">
-							<div class="mb-4">
-								<a class="card info-overlay card-md-border-0" :href="posts[20].url">
-									<div class="square">
-										<span v-if="posts[20].type == 'photo:album'" class="float-right mr-3 post-icon"><i class="fas fa-images fa-2x"></i></span>
-										<span v-if="posts[20].type == 'video'" class="float-right mr-3 post-icon"><i class="fas fa-video fa-2x"></i></span>
-										<span v-if="posts[20].type == 'video:album'" class="float-right mr-3 post-icon"><i class="fas fa-film fa-2x"></i></span>
-										<div class="square-content" v-bind:style="{ 'background-image': 'url(' + posts[20].thumb + ')' }">
-										</div>
-									</div>
-								</a>
-							</div>				
-						</div>
-						<div class="col-12 col-md-6 row p-0 m-0">
-							<div v-for="(post, index) in posts.slice(21,25)" class="col-6" style="margin-bottom:1.8rem;">
-								<a class="card info-overlay card-md-border-0" :href="post.url">
-									<div class="square">
-										<span v-if="post.type == 'photo:album'" class="float-right mr-3 post-icon"><i class="fas fa-images fa-2x"></i></span>
-										<span v-if="post.type == 'video'" class="float-right mr-3 post-icon"><i class="fas fa-video fa-2x"></i></span>
-										<span v-if="post.type == 'video:album'" class="float-right mr-3 post-icon"><i class="fas fa-film fa-2x"></i></span>
-										<div class="square-content" v-bind:style="{ 'background-image': 'url(' + post.thumb + ')' }">
-										</div>
-									</div>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="row p-0" style="display: flex;">
-						<div v-for="(post, index) in posts.slice(25, 29)" class="col-3 p-1 p-sm-2 p-md-3 pt-0">
-							<a class="card info-overlay card-md-border-0" :href="post.url">
-								<div class="square">
-									<span v-if="post.type == 'photo:album'" class="float-right mr-3 post-icon"><i class="fas fa-images fa-2x"></i></span>
-									<span v-if="post.type == 'video'" class="float-right mr-3 post-icon"><i class="fas fa-video fa-2x"></i></span>
-									<span v-if="post.type == 'video:album'" class="float-right mr-3 post-icon"><i class="fas fa-film fa-2x"></i></span>
-									<div class="square-content" v-bind:style="{ 'background-image': 'url(' + post.thumb + ')' }">
-									</div>
-								</div>
-							</a>
-						</div>
-					</div>
-				</div>
-			</section> -->
 		</div>
 	</div>
 </template>
-
-<style type="text/css" scoped>
-.discover-bar::-webkit-scrollbar { 
-	display: none; 
-}
-.card-disc {
-	flex: 0 0 160px;
-	width:160px;
-	height:100px;
-	background-size: cover !important;
-}
-.post-icon {
-	color: #fff;
-	position:relative;
-	margin-top: 10px;
-	z-index: 9;
-	opacity: 0.6;
-	text-shadow: 3px 3px 16px #272634;
-}
-</style>
 
 <script type="text/javascript">
 	export default {
@@ -292,43 +154,34 @@
 				loaded: false,
 				config: window.App.config,
 				posts: {},
-				hashtags: {},
-				places: {},
 				trending: {},
 				trendingDaily: {},
 				trendingMonthly: {},
-				categories: {},
-				allCategories: {},
 				searchTerm: '',
-				trendingRange: 'daily'
+				trendingRange: 'daily',
+				trendingLoading: true,
+				recommendedLoading: true
 			}
 		},
 		mounted() {
-			this.fetchData();
-			this.fetchCategories();
+			this.loaded = true;
 			this.loadTrending();
-			this.loadTrendingHashtags();
-			this.loadTrendingPlaces();
+			this.fetchData();
+			axios.get('/api/pixelfed/v1/accounts/verify_credentials').then(res => {
+				window._sharedData.curUser = res.data;
+				window.App.util.navatar();
+			});
 		},
 
 		methods: {
 			fetchData() {
+				if(!this.recommendedLoading) {
+					return;
+				}
 				axios.get('/api/pixelfed/v2/discover/posts')
 				.then((res) => {
 					this.posts = res.data.posts;
-					this.loaded = true;
-				});
-				axios.get('/api/pixelfed/v1/accounts/verify_credentials').then(res => {
-					window._sharedData.curUser = res.data;
-					window.App.util.navatar();
-				});
-			},
-
-			fetchCategories() {
-				axios.get('/api/v2/discover/categories')
-				.then(res => {
-					this.allCategories = res.data;
-					this.categories = res.data;
+					this.recommendedLoading = false;
 				});
 			},
 
@@ -341,10 +194,12 @@
 			loadTrending() {
 				if(this.trendingRange == 'daily' && this.trendingDaily.length) {
 					this.trending = this.trendingDaily;
+					this.trendingLoading = false;
 					return;
 				}
 				if(this.trendingRange == 'monthly' && this.trendingMonthly.length) {
 					this.trending = this.trendingMonthly;
+					this.trendingLoading = false;
 					return;
 				}
 				axios.get('/api/pixelfed/v2/discover/posts/trending', {
@@ -360,26 +215,14 @@
 						this.trendingMonthly = res.data.filter(t => t.sensitive == false);
 					}
 					this.trending = res.data;
+					this.trendingLoading = false;
 				});
 			},
 
 			trendingRangeToggle(r) {
+				this.trendingLoading = true;
 				this.trendingRange = r;
 				this.loadTrending();
-			},
-
-			loadTrendingHashtags() {
-				axios.get('/api/pixelfed/v2/discover/posts/hashtags')
-				.then(res => {
-					this.hashtags = res.data;
-				});
-			},
-
-			loadTrendingPlaces() {
-				axios.get('/api/pixelfed/v2/discover/posts/places')
-				.then(res => {
-					this.places = res.data;
-				});
 			},
 
 			formatCount(s) {
