6b67522e59d200eabf1bcf83e6b9c856cf2ceb7e
trwnh <a@trwnh.com>
2021-02-09 20:20:31 -0600

    Update and rename PasswordResetGC.php to GCPasswordReset.php

--- app/Console/Commands/GCPasswordReset.php
+++ app/Console/Commands/GCPasswordReset.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\EmailVerification;
 
-class PasswordResetGC extends Command
+class GCPasswordReset extends Command
 {
     /**
      * The name and signature of the console command.
