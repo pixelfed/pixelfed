c55f14764f6d5eaef5abd9a31906fa480ac05d5f
Daniel Supernault <danielsupernault@gmail.com>
2021-02-16 22:59:40 -0700

    Update SnowflakeTest, fixes #2661

--- tests/Unit/SnowflakeTest.php
+++ tests/Unit/SnowflakeTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use App\Services\SnowflakeService;
 
-class Snowflake extends TestCase
+class SnowflakeTest extends TestCase
 {
     /** @test */
     public function snowflakeTest()
