9f204e4c23be86b362aab315f83876ac09cf5036
zer0-x <zer0_@tutanota.com>
2021-03-02 21:02:44 +0300

    Improve and update the Arabic language

--- resources/lang/ar/notification.php
+++ resources/lang/ar/notification.php
@@ -2,9 +2,11 @@
 
 return [
 
-  'likedPhoto'          => 'أُعجِب بصورتك.',
-  'startedFollowingYou' => 'يُتابِعك الآن.',
-  'commented'           => 'علّق على منشورك.',
-  'mentionedYou'        => 'أشار إليك.',
+  'likedPhoto'          => 'اُعجِبَ بِمَنشُورِك.',
+  'likedComment'        => 'اُعجِبَ بِتَعلِيقِك.',
+  'startedFollowingYou' => 'بَدَأ بِمُتَابَعَتِك.',
+  'commented'           => 'عَلَّقَ عَلَى مَنشُورِك.',
+  'mentionedYou'        => 'قَد ذَكَرَك.',
+  'shared'				=> 'شَارَكَ مَنشُورَك.',
 
 ];
