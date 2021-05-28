7e0be154043fef62f9064a85bb35aa72008c4821
Daniel Supernault <danielsupernault@gmail.com>
2021-05-27 23:06:47 -0600

    Update exceptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,55 +4,56 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use League\OAuth2\Server\Exception\OAuthServerException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		OAuthServerException::class
+	];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * A list of the inputs that are never flashed for validation exceptions.
+	 *
+	 * @var array
+	 */
+	protected $dontFlash = [
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param \Exception $exception
-     *
-     * @return void
-     */
-    public function report(Throwable $exception)
-    {
-        parent::report($exception);
-    }
+	/**
+	 * Report or log an exception.
+	 *
+	 * @param \Exception $exception
+	 *
+	 * @return void
+	 */
+	public function report(Throwable $exception)
+	{
+		parent::report($exception);
+	}
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Exception               $exception
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Throwable $exception)
-    {
-        if ($request->wantsJson())
-            return response()->json(
-                ['error' => $exception->getMessage()],
-                method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500
-            );
-        return parent::render($request, $exception);
-    }
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Exception               $exception
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Throwable $exception)
+	{
+		if ($request->wantsJson())
+			return response()->json(
+				['error' => $exception->getMessage()],
+				method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500
+			);
+		return parent::render($request, $exception);
+	}
 }
