a4121cb8e43362f5ef15e5632d6877b2458b964e
Daniel Supernault <danielsupernault@gmail.com>
2021-06-01 23:50:21 -0600

    Bump version

--- config/pixelfed.php
+++ config/pixelfed.php
@@ -23,7 +23,7 @@ return [
 	| This value is the version of your Pixelfed instance.
 	|
 	*/
-	'version' => '0.10.10',
+	'version' => '0.11.0',
 
 	/*
 	|--------------------------------------------------------------------------
