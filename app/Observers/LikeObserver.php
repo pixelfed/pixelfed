d3157f2a2d40586319ab04d97a0e80ed992fc372
Daniel Supernault <danielsupernault@gmail.com>
2021-07-02 01:33:49 -0600

    Add LikeObserver

---
+++ app/Observers/LikeObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Like;
+use App\Services\LikeService;
+
+class LikeObserver
+{
+    /**
+     * Handle the Like "created" event.
+     *
+     * @param  \App\Models\Like  $like
+     * @return void
+     */
+    public function created(Like $like)
+    {
+        LikeService::add($like->profile_id, $like->status_id);
+    }
+
+    /**
+     * Handle the Like "updated" event.
+     *
+     * @param  \App\Models\Like  $like
+     * @return void
+     */
+    public function updated(Like $like)
+    {
+        //
+    }
+
+    /**
+     * Handle the Like "deleted" event.
+     *
+     * @param  \App\Models\Like  $like
+     * @return void
+     */
+    public function deleted(Like $like)
+    {
+        LikeService::remove($like->profile_id, $like->status_id);
+    }
+
+    /**
+     * Handle the Like "restored" event.
+     *
+     * @param  \App\Models\Like  $like
+     * @return void
+     */
+    public function restored(Like $like)
+    {
+        LikeService::add($like->profile_id, $like->status_id);
+    }
+
+    /**
+     * Handle the Like "force deleted" event.
+     *
+     * @param  \App\Models\Like  $like
+     * @return void
+     */
+    public function forceDeleted(Like $like)
+    {
+        LikeService::remove($like->profile_id, $like->status_id);
+    }
+}
