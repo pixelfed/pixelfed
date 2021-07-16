49d27caae8171f5377db2c3df27efb8ca597d3df
Daniel Supernault <danielsupernault@gmail.com>
2021-07-15 23:37:23 -0600

    Update remote profile components

--- resources/assets/js/rempro.js
+++ resources/assets/js/rempro.js
@@ -1,4 +1,29 @@
 Vue.component(
+    'photo-presenter',
+    require('./components/presenter/PhotoPresenter.vue').default
+);
+
+Vue.component(
+    'video-presenter',
+    require('./components/presenter/VideoPresenter.vue').default
+);
+
+Vue.component(
+    'photo-album-presenter',
+    require('./components/presenter/PhotoAlbumPresenter.vue').default
+);
+
+Vue.component(
+    'video-album-presenter',
+    require('./components/presenter/VideoAlbumPresenter.vue').default
+);
+
+Vue.component(
+    'mixed-album-presenter',
+    require('./components/presenter/MixedAlbumPresenter.vue').default
+);
+
+Vue.component(
 	'remote-profile',
 	require('./components/RemoteProfile.vue').default
 );
