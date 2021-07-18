e825d73b9a03d9eaeb5eb0125385ddd71ae662f9
shynome <shynome@gmail.com>
2021-07-18 18:19:11 +0800

    add zh-cn lang

---
+++ resources/lang/zh-cn/navmenu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'search'			=> '搜索',
+	'home'				=> '首页',
+	'local'				=> '本地',
+	'network'			=> '网络',
+	'discover'			=> '发现',
+	'viewMyProfile'		=> '浏览我的主页',
+	'myProfile'			=> '我的主页',
+	'myTimeline'		=> '我的时间线',
+	'publicTimeline'	=> '公开的时间线',
+	'remoteFollow'		=> '远程关注',
+	'settings'			=> '设置',
+	'admin'				=> '管理',
+	'logout'			=> '退出',
+	'directMessages'	=> '直接信息',
+	'composePost'		=> '发表文字',
+];
---
+++ resources/lang/zh-cn/site.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'about'             => '关于',
+    'help'              => '帮助',
+    'language'          => '语言',
+    'fediverse'         => 'Fediverse',
+    'opensource'        => '开源代码',
+    'terms'             => '服务条款',
+    'privacy'           => '隐私',
+    'l10nWip'           => '我们仍在进行本地化支持',
+    'currentLocale'     => '当前语言环境',
+    'selectLocale'      => '选择一种支持的语言',
+    'contact'           => '联系',
+    'contact-us'        => '联系我们',
+    'places'            => '地点',
+    'profiles'          => '简介',
+
+];
