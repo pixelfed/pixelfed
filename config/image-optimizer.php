a08c8a29d57222d9ade3102a24ba61dfb27ced4b
Daniel Supernault <danielsupernault@gmail.com>
2021-09-03 22:09:58 -0600

    Update image optimizer

--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -14,7 +14,7 @@ return [
     'optimizers' => [
 
         Jpegoptim::class => [
-            '-m75', // set maximum quality to 75%
+            '-m' . (int) env('IMAGE_QUALITY', 80),
             '--strip-all',  // this strips out all text information such as comments and EXIF data
             '--all-progressive',  // this will make sure the resulting image is a progressive one
         ],
