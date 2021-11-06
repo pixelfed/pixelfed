9bd53524c7f3246b84a40063cf18e22bd1153e4b
Daniel Supernault <danielsupernault@gmail.com>
2021-11-05 21:51:52 -0600

    Update setting layouts

--- resources/views/settings/security/2fa/edit.blade.php
+++ resources/views/settings/security/2fa/edit.blade.php
@@ -1,33 +1,42 @@
-@extends('settings.template')
+@extends('layouts.app')
 
-@section('section')
+@section('content')
+<div class="container">
+  <div class="col-12">
+    <div class="card shadow-none border mt-5">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-12 p-3 p-md-5">
+			  <div class="title">
+			    <h3 class="font-weight-bold">Edit Two-Factor Authentication</h3>
+			  </div>
 
-  <div class="title">
-    <h3 class="font-weight-bold">Edit Two-Factor Authentication</h3>
+			  <hr>
+
+			  <p class="lead pb-3">
+			  	To register a new device, you have to remove any active devices.
+			  </p>
+
+			  <div class="card">
+			  	<div class="card-header bg-light font-weight-bold">
+			  		Authenticator App
+			  	</div>
+			  	<div class="card-body d-flex justify-content-between align-items-center">
+			  		<i class="fas fa-lock fa-3x text-success"></i>
+			  		<p class="font-weight-bold mb-0">
+			  			Added {{$user->{'2fa_setup_at'}->diffForHumans()}}
+			  		</p>
+			  	</div>
+			  	<div class="card-footer bg-white text-right">
+			  		<a class="btn btn-outline-secondary btn-sm px-4 font-weight-bold mr-3" href="{{route('settings.security.2fa.recovery')}}">View Recovery Codes</a>
+			  		<a class="btn btn-outline-danger btn-sm px-4 font-weight-bold remove-device" href="#">Remove</a>
+			  	</div>
+			  </div>
+        </div>
+      </div>
+    </div>
   </div>
-  
-  <hr>
-
-  <p class="lead pb-3">
-  	To register a new device, you have to remove any active devices.
-  </p>
-
-  <div class="card">
-  	<div class="card-header bg-light font-weight-bold">
-  		Authenticator App
-  	</div>
-  	<div class="card-body d-flex justify-content-between align-items-center">
-  		<i class="fas fa-lock fa-3x text-success"></i>
-  		<p class="font-weight-bold mb-0">
-  			Added {{$user->{'2fa_setup_at'}->diffForHumans()}}
-  		</p>
-  	</div>
-  	<div class="card-footer bg-white text-right">
-  		<a class="btn btn-outline-secondary btn-sm px-4 font-weight-bold mr-3" href="{{route('settings.security.2fa.recovery')}}">View Recovery Codes</a>
-  		<a class="btn btn-outline-danger btn-sm px-4 font-weight-bold remove-device" href="#">Remove</a>
-  	</div>
-  </div>
-
+</div>
 @endsection
 
 @push('scripts')
