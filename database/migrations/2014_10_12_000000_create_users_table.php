ddd6258e10a976b4459d7a84793053cb623395b3
Samih Soylu <mail@samihsoylu.nl>
2021-11-21 14:28:30 +0100

    Fix sql_require_primary_key during fresh installation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
             $table->bigIncrements('id');
             $table->string('name')->nullable();
             $table->string('username')->nullable()->unique()->index();
