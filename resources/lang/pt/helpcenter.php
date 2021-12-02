f7f62b3df8e55c790847ba5c751f9a74f9c6cf70
Artur Mancha <joker@nadaradical.com>
2021-12-02 11:14:52 +0100

    Create helpcenter.php
    Added missing translation

---
+++ resources/lang/pt/helpcenter.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+	'helpcenter' => 'Centro de Ajuda',
+	'whatsnew' => 'O que há de novo',
+
+	'gettingStarted' => 'Como Começar',
+	'sharingMedia' => 'Partilhar Mídia',
+	'profile' => 'Perfil',
+	'stories' => 'Stories',
+	'hashtags' => 'Hashtags',
+	'discover' => 'Descobrir',
+	'directMessages' => 'Mensagens Diretas',
+	'timelines' => 'Linha do Tempo',
+	'embed'	=> 'Embed',
+
+	'communityGuidelines' => 'Directrizes da Comunidade',
+	'whatIsTheFediverse' => 'O que é o fediverse?',
+	'controllingVisibility' => 'Controlar a Visibilidade',
+	'blockingAccounts' => 'Bloqueio de contas',
+	'safetyTips' => 'Dicas de Segurança',
+	'reportSomething' => 'Reportar Algo',
+	'dataPolicy' => 'Política de Dados',
+
+	'taggingPeople' => 'Marcação de Pessoas'
+
+];
