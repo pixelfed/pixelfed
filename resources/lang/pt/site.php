5143a4b8771233afc4c84fca9a095063f2ffa1c1
Artur Mancha <joker@nadaradical.com>
2021-12-02 11:04:11 +0100

    Update site.php
    Improved translation

--- resources/lang/pt/site.php
+++ resources/lang/pt/site.php
@@ -5,12 +5,16 @@ return [
 	'about'			=> 'Sobre',
 	'help'			=> 'Ajuda',
 	'language'		=> 'Idioma',
-	'fediverse'		=> 'Fediverso',
+	'fediverse'		=> 'Fediverse',
 	'opensource'	=> 'Código Aberto',
 	'terms'			=> 'Termos',
 	'privacy'		=> 'Privacidade',
-	'l10nWip'		=> 'Ainda estamos trabalhando na tradução',
+	'l10nWip'		=> 'Ainda estamos a trabalhar na tradução',
 	'currentLocale' => 'Idioma atual',
 	'selectLocale'  => 'Escolha um dos idiomas disponíveis',
+    'contact'       => 'Contacto',
+    'contact-us'    => 'Contacte-nos',
+    'places'        => 'Locais',
+    'profiles'      => 'Perfis',
 
 ];
