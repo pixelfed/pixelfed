24db88cd62660cf23933e60c3f12142f0d7e6683
daniel <danielsupernault@gmail.com>
2021-12-25 21:11:35 -0700

    New translations web.php (Occitan) [ci skip]

--- resources/lang/oc/web.php
+++ resources/lang/oc/web.php
@@ -116,6 +116,9 @@ return [
 		'editProfile' => 'Edit Profile',
 		'followRequested' => 'Follow Requested',
 		'joined' => 'Joined',
+
+		'emptyCollections' => 'We can\'t seem to find any collections',
+		'emptyPosts' => 'We can\'t seem to find any posts',
 	],
 
 	'menu' => [
