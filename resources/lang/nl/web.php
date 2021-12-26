9a5619c2dd1269904d5646182addcbcaecde157b
daniel <danielsupernault@gmail.com>
2021-12-25 21:11:53 -0700

    New translations web.php (Dutch) [ci skip]

--- resources/lang/nl/web.php
+++ resources/lang/nl/web.php
@@ -116,6 +116,9 @@ return [
 		'editProfile' => 'Profiel bewerken',
 		'followRequested' => 'Volgen verzocht',
 		'joined' => 'Lid geworden',
+
+		'emptyCollections' => 'We can\'t seem to find any collections',
+		'emptyPosts' => 'We can\'t seem to find any posts',
 	],
 
 	'menu' => [
