aff7456639a2ba8813186d0df528730ac4a96cd3
Daniel Supernault <danielsupernault@gmail.com>
2022-01-22 19:37:50 -0700

    Update WebfingerService. Fixes #3167

--- app/Services/WebfingerService.php
+++ app/Services/WebfingerService.php
@@ -6,7 +6,7 @@ use Cache;
 use App\Profile;
 use Illuminate\Support\Facades\Redis;
 use App\Util\Webfinger\WebfingerUrl;
-use Zttp\Zttp;
+use Illuminate\Support\Facades\Http;
 use App\Util\ActivityPub\Helpers;
 use App\Transformer\Api\AccountTransformer;
 use League\Fractal;
@@ -32,12 +32,35 @@ class WebfingerService
 		if(!Helpers::validateUrl($url)) {
 			return [];
 		}
-		$res = Zttp::get($url);
-		$webfinger = $res->json();
-		if(!isset($webfinger['links'])) {
+
+		$res = Http::retry(3, 500)
+			->acceptJson()
+			->withHeaders([
+				'User-Agent' => '(Pixelfed/' . config('pixelfed.version') . '; +' . config('app.url') . ')'
+			])
+			->timeout(20)
+			->get($url);
+
+		if(!$res->successful()) {
 			return [];
 		}
-		$profile = Helpers::profileFetch($webfinger['links'][0]['href']);
+
+		$webfinger = $res->json();
+		if(!isset($webfinger['links']) || !is_array($webfinger['links']) || empty($webfinger['links'])) {
+			return ['nolinks'];
+		}
+
+		$link = collect($webfinger['links'])
+			->filter(function($link) {
+				return $link &&
+					isset($link['type']) &&
+					isset($link['href']) &&
+					$link['type'] == 'application/activity+json';
+			})
+			->pluck('href')
+			->first();
+
+		$profile = Helpers::profileFetch($link);
 		$fractal = new Fractal\Manager();
 		$fractal->setSerializer(new ArraySerializer());
 		$resource = new Fractal\Resource\Item($profile, new AccountTransformer());
