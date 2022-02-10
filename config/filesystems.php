49a53c27bebcf8b958ae94313186b9b12aba7c24
Daniel Supernault <danielsupernault@gmail.com>
2022-02-09 23:17:19 -0700

    Update filesystems config, set S3 visibility to public by default. Fixes #2913

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,6 +61,7 @@ return [
             'secret'   => env('AWS_SECRET_ACCESS_KEY'),
             'region'   => env('AWS_DEFAULT_REGION'),
             'bucket'   => env('AWS_BUCKET'),
+            'visibility' => 'public',
             'url'      => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
