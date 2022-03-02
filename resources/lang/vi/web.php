557190fed1119a8e5df3883060deeb066e6b079d
daniel <danielsupernault@gmail.com>
2022-03-01 22:15:41 -0700

    New translations web.php (Vietnamese) [ci skip]

--- resources/lang/vi/web.php
+++ resources/lang/vi/web.php
@@ -13,6 +13,7 @@ return [
 		'shared' => 'Đã chia sẻ',
 		'shares' => 'Lượt chia sẻ',
 		'unshare' => 'Hủy chia sẻ',
+		'bookmark' => 'Bookmark',
 
 		'cancel' => 'Hủy',
 		'copyLink' => 'Chép link',
@@ -23,6 +24,10 @@ return [
 		'other' => 'Khác',
 		'readMore' => 'Xem thêm',
 		'success' => 'Hoàn tất',
+		'proceed' => 'Proceed',
+		'next' => 'Next',
+		'close' => 'Close',
+		'clickHere' => 'click here',
 
 		'sensitive' => 'Nhạy cảm',
 		'sensitiveContent' => 'Nội dung nhạy cảm',
@@ -97,6 +102,7 @@ return [
 		'modlog' => 'nhật ký kiểm duyệt',
 		'post' => 'bài đăng',
 		'story' => 'khoảnh khắc',
+		'noneFound' => 'No notifications found',
 	],
 
 	'post' => [
@@ -176,11 +182,27 @@ return [
 	],
 
 	'timeline' => [
-		'peopleYouMayKnow' => 'Những người bạn có thể biết'
+		'peopleYouMayKnow' => 'Những người bạn có thể biết',
+
+		'onboarding' => [
+			'welcome' => 'Welcome',
+			'thisIsYourHomeFeed' => 'This is your home feed, a chronological feed of posts from accounts you follow.',
+			'letUsHelpYouFind' => 'Let us help you find some interesting people to follow',
+			'refreshFeed' => 'Refresh my feed',
+		],
 	],
 
 	'hashtags' => [
 		'emptyFeed' => 'Không tìm thấy ảnh nào với hashtag này'
+	],
+
+	'report' => [
+		'report' => 'Report',
+		'selectReason' => 'Select a reason',
+		'reported' => 'Reported',
+		'sendingReport' => 'Sending report',
+		'thanksMsg' => 'Thanks for the report, people like you help keep our community safe!',
+		'contactAdminMsg' => 'If you\'d like to contact an administrator about this post or report',
 	],
 
 ];
