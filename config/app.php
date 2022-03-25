1c02afb1a1cf43f4bf3aae0644692a3b98c8f064
Daniel Supernault <danielsupernault@gmail.com>
2022-03-24 21:42:58 -0600

    Update oauth flow

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         App\Providers\HorizonServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\PassportServiceProvider::class,
 
     ],
 
