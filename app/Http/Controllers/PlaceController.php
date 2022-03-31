e7783af6865b1fd0c2312c1691a76d77fab5f0bb
Daniel Supernault <danielsupernault@gmail.com>
2022-03-30 23:56:46 -0600

    Update PlaceController, require authentication

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -10,6 +10,11 @@ use App\{
 
 class PlaceController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
     public function show(Request $request, $id, $slug)
     {
     	$place = Place::whereSlug($slug)->findOrFail($id);
