270578bb39c056a9eb04a7c75857c03812630133
Daniel Supernault <danielsupernault@gmail.com>
2022-04-26 22:27:40 -0600

    Update horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -152,7 +152,7 @@ return [
 	|
 	*/
 
-	'memory_limit' => 64,
+	'memory_limit' => env('HORIZON_MEMORY_LIMIT', 64),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -170,12 +170,13 @@ return [
 			'supervisor-1' => [
 				'connection'    => 'redis',
 				'queue'         => ['high', 'default', 'feed', 'low', 'story', 'delete', 'mmo'],
-				'balance'       => 'auto',
-				'maxProcesses'  => 20,
-				'memory'        => 128,
-				'tries'         => 3,
-				'nice'          => 0,
-				'timeout'		=> 300
+				'balance'       => env('HORIZON_BALANCE_STRATEGY', 'auto'),
+				'minProcesses'  => env('HORIZON_MIN_PROCESSES', 1),
+				'maxProcesses'  => env('HORIZON_MAX_PROCESSES', 20),
+				'memory'        => env('HORIZON_SUPERVISOR_MEMORY', 128),
+				'tries'         => env('HORIZON_SUPERVISOR_TRIES', 3),
+				'nice'          => env('HORIZON_SUPERVISOR_NICE', 0),
+				'timeout'		=> env('HORIZON_SUPERVISOR_TIMEOUT', 300),
 			],
 		],
 
@@ -184,6 +185,7 @@ return [
 				'connection'    => 'redis',
 				'queue'         => ['high', 'default', 'feed', 'low', 'story', 'delete', 'mmo'],
 				'balance'       => 'auto',
+				'minProcesses' => 1,
 				'maxProcesses'  => 20,
 				'memory'        => 128,
 				'tries'         => 3,
