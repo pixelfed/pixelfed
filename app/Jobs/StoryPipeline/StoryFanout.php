74cc41d4fae0575ffe6e3d1fd549511f9e9406be
noellabo <noel.yoshiba@gmail.com>
2022-05-02 23:36:12 +0900

    Add Content-Type and User-Agent for activity delivery

--- app/Jobs/StoryPipeline/StoryFanout.php
+++ app/Jobs/StoryPipeline/StoryFanout.php
@@ -79,7 +79,13 @@ class StoryFanout implements ShouldQueue
 
 		$requests = function($audience) use ($client, $activity, $profile, $payload) {
 			foreach($audience as $url) {
-				$headers = HttpSignature::sign($profile, $url, $activity);
+				$version = config('pixelfed.version');
+				$appUrl = config('app.url');
+				$proxy = config('');
+				$headers = HttpSignature::sign($profile, $url, $activity, [
+					'Content-Type'	=> 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"',
+					'User-Agent'	=> "(Pixelfed/{$version}; +{$appUrl})",
+				]);
 				yield function() use ($client, $url, $headers, $payload) {
 					return $client->postAsync($url, [
 						'curl' => [
