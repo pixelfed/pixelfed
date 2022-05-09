153eb6ba4cc6d0797cce784b83a906632215f55d
Daniel Supernault <danielsupernault@gmail.com>
2022-05-09 02:01:33 -0600

    Update HashtagService, use sorted set for followed tags

--- app/Services/HashtagService.php
+++ app/Services/HashtagService.php
@@ -2,11 +2,15 @@
 
 namespace App\Services;
 
-use Cache;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 use App\Hashtag;
 use App\StatusHashtag;
+use App\HashtagFollow;
 
 class HashtagService {
+
+	const FOLLOW_KEY = 'pf:services:hashtag:following:';
 
 	public static function get($id)
 	{
@@ -29,4 +33,35 @@ class HashtagService {
 		});
 	}
 
+	public static function isFollowing($pid, $hid)
+	{
+		$res = Redis::zscore(self::FOLLOW_KEY . $pid, $hid);
+		if($res) {
+			return true;
+		}
+
+		$synced = Cache::get(self::FOLLOW_KEY . $pid . ':synced');
+		if(!$synced) {
+			$tags = HashtagFollow::whereProfileId($pid)
+				->get()
+				->each(function($tag) use($pid) {
+					self::follow($pid, $tag->hashtag_id);
+				});
+			Cache::set(self::FOLLOW_KEY . $pid . ':synced', true, 1209600);
+
+			return (bool) Redis::zscore(self::FOLLOW_KEY . $pid, $hid) > 1;
+		}
+
+		return false;
+	}
+
+	public static function follow($pid, $hid)
+	{
+		return Redis::zadd(self::FOLLOW_KEY . $pid, $hid, $hid);
+	}
+
+	public static function unfollow($pid, $hid)
+	{
+		return Redis::zrem(self::FOLLOW_KEY . $pid, $hid);
+	}
 }
