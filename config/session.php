375604a0defdbacf096fa4d92377b114c89f21a5
Shlee <github@shl.ee>
2022-05-15 18:22:50 +0930

    Update session.php

--- config/session.php
+++ config/session.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
