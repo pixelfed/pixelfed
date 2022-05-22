677bb77b18404c75f31c6558f06fff6ab2766a57
Shlee <github@shl.ee>
2022-05-22 23:30:06 +0930

    Update NotificationTransformer.php

--- app/Transformer/Api/Mastodon/v1/NotificationTransformer.php
+++ app/Transformer/Api/Mastodon/v1/NotificationTransformer.php
@@ -15,34 +15,34 @@ class NotificationTransformer extends Fractal\TransformerAbstract
 		'status',
 	];
 
-	public function transform(Notification $notification)
-	{
-		return [
-			'id'       		=> (string) $notification->id,
-			'type'       	=> $this->replaceTypeVerb($notification->action),
-			'created_at' 	=> (string) $notification->created_at->toJSON(),
-		];
-	}
+// 	public function transform(Notification $notification)
+// 	{
+// 		return [
+// 			'id'       		=> (string) $notification->id,
+// 			'type'       	=> $this->replaceTypeVerb($notification->action),
+// 			'created_at' 	=> (string) $notification->created_at->toJSON(),
+// 		];
+// 	}
 
-	public function includeAccount(Notification $notification)
-	{
-		return $this->item($notification->actor, new AccountTransformer());
-	}
+// 	public function includeAccount(Notification $notification)
+// 	{
+// 		return $this->item($notification->actor, new AccountTransformer());
+// 	}
 
-	public function includeStatus(Notification $notification)
-	{
-		$item = $notification;
-		if($item->item_id && $item->item_type == 'App\Status') {
-			$status = Status::with('media')->find($item->item_id);
-			if($status) {
-				return $this->item($status, new StatusTransformer());
-			} else {
-				return;
-			}
-		} else {
-			return;
-		}
-	}
+// 	public function includeStatus(Notification $notification)
+// 	{
+// 		$item = $notification;
+// 		if($item->item_id && $item->item_type == 'App\Status') {
+// 			$status = Status::with('media')->find($item->item_id);
+// 			if($status) {
+// 				return $this->item($status, new StatusTransformer());
+// 			} else {
+// 				return;
+// 			}
+// 		} else {
+// 			return;
+// 		}
+// 	}
 
 	public function replaceTypeVerb($verb)
 	{
