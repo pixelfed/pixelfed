7c8f76438bcb9b78b172ab33c5500d53de9e1852
Daniel Supernault <danielsupernault@gmail.com>
2022-06-09 00:14:40 -0600

    Update LiveStream model

--- app/Models/LiveStream.php
+++ app/Models/LiveStream.php
@@ -23,6 +23,21 @@ class LiveStream extends Model
     	$port = ':' . config('livestreaming.server.port');
     	$path = '/' . config('livestreaming.server.path') . '?';
     	$query = http_build_query([
+    		'name' => $this->stream_id,
+    		'key' => $this->stream_key,
+    		'ts' => time()
+    	]);
+
+    	return $proto . $host . $port . $path . $query;
+    }
+
+    public function getStreamRtmpUrl()
+    {
+    	$proto = 'rtmp://';
+    	$host = config('livestreaming.server.host');
+    	$port = ':' . config('livestreaming.server.port');
+    	$path = '/' . config('livestreaming.server.path') . '/'. $this->stream_id . '?';
+    	$query = http_build_query([
     		'key' => $this->stream_key,
     		'ts' => time()
     	]);
