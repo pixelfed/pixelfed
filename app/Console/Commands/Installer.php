fc811416f3158f4ec573ac30606171485a00a78e
Shlee <github@shl.ee>
2022-06-15 19:16:11 +0930

    Update Installer.php

--- app/Console/Commands/Installer.php
+++ app/Console/Commands/Installer.php
@@ -382,7 +382,7 @@ class Installer extends Command
             $this->call('instance:actor');
             $this->line('');
             $this->info('Creating Password Keys for API:');
-            $this->call('passport:keys, ['--force' => true]);
+            $this->call('passport:keys', ['--force' => true]);
 
             $confirm = $this->choice('Do you want to create an admin account?', ['Yes', 'No'], 0);
 	        if($confirm === 'Yes') {
