d3946ea548def3eb5c0b851fa6fd4c8e9a33e052
Daniel Supernault <danielsupernault@gmail.com>
2022-10-07 05:12:34 -0600

    Add migration

---
+++ database/migrations/2022_10_07_110644_add_item_id_and_item_type_indexes_to_notifications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddItemIdAndItemTypeIndexesToNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->index('item_id');
+            $table->index('item_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            //
+        });
+    }
+}
