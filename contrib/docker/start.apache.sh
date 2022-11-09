191dc0d11761d5065de9ad9380aefb367ddb6d5d
Ubuntu <ubuntu@hackerspace-zone.labs.lowerlayer.nl>
2022-11-09 15:47:58 +0000

    docker: Remove bootstrap volume and streamline startup
    This patch modifies the `start.apache.sh` script to perform the
    initial setup on first boot of the container so that the database
    and all of the various keys are applied.
    
    The patch also moves the storage, db, and redis volumes into a local
    `data` directory for easier management and removes the bootstrap volume
    since it did not seem necessary for normal operation.

--- contrib/docker/start.apache.sh
+++ contrib/docker/start.apache.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
+info() { echo >&2 "$*" ; }
+
 # Create the storage tree if needed and fix permissions
 cp -r storage.skel/* storage/
 chown -R www-data:www-data storage/ bootstrap/
+
+# Do initial setup if it hasn't been done
+# These are the one-time setup tasks from:
+# https://docs.pixelfed.org/running-pixelfed/installation/#one-time-setup-tasks
+if [ -z "$APP_KEY" ]; then
+	info "creating app key"
+	php artisan key:generate || exit 1
+fi
+
+if [ ! -r storage/.migrated ]; then
+	for i in `seq 1 10`; do
+		info "******* WAITING FOR DB TO COME UP ********"
+		if echo 'exit' | mysql \
+			-u $DB_USERNAME \
+			-p$DB_PASSWORD \
+			-h $DB_HOST \
+			-P $DB_PORT \
+			$DB_DATABASE \
+		; then
+			break
+		fi
+		sleep 10
+	done
+
+	info "Creating database scheme; this might take a while"
+	php artisan migrate --force || exit 1
+	touch storage/.migrated
+fi
+
+if [ ! -r storage/.instance ]; then
+	info "Creating ActivityPub federation id"
+	php artisan instance:actor || exit 1
+	touch storage/.instance
+fi
+
+if [ ! -r storage/.passport ]; then
+	info "Creating OAuth keys"
+	php artisan passport:keys || exit 1
+	touch storage/.passport
+fi
+
 
 # Refresh the environment
 php artisan storage:link
@@ -12,4 +55,4 @@ php artisan view:cache
 php artisan config:cache
 
 # Finally run Apache
-apache2-foreground
+exec apache2-foreground
