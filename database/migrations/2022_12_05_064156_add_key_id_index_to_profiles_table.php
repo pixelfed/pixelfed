3295dfa16e15208d73f917193bb3546d64af05bf
Daniel Supernault <danielsupernault@gmail.com>
2022-12-04 23:53:39 -0700

    Add db migration, add profile indexes

---
+++ database/migrations/2022_12_05_064156_add_key_id_index_to_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->index('key_id');
+            $table->index('remote_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropIndex('profiles_key_id_index');
+            $table->dropIndex('profiles_remote_url_index');
+        });
+    }
+};
