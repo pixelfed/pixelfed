e27d1a4424624a9bc2a3ecbd4215f763094b3705
Daniel Supernault <danielsupernault@gmail.com>
2022-12-20 01:03:19 -0700

    Add index migration

--- database/migrations/2022_12_20_075729_add_action_index_to_notifications_table.php
+++ database/migrations/2022_12_20_075729_add_action_index_to_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddItemIdAndItemTypeIndexesToNotificationsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddItemIdAndItemTypeIndexesToNotificationsTable extends Migration
     public function up()
     {
         Schema::table('notifications', function (Blueprint $table) {
-            $table->index('item_id');
-            $table->index('item_type');
+            $table->index('action');
         });
     }
 
@@ -27,8 +26,7 @@ class AddItemIdAndItemTypeIndexesToNotificationsTable extends Migration
     public function down()
     {
         Schema::table('notifications', function (Blueprint $table) {
-            $table->dropIndex('notifications_item_id_index');
-            $table->dropIndex('notifications_item_type_index');
+            $table->dropIndex('notifications_action_index');
         });
     }
-}
+};
