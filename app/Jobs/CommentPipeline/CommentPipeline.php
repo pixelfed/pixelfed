d4dfa95c3054a544ddbd0a986235848a7cfad177
Daniel Supernault <danielsupernault@gmail.com>
2022-12-24 01:44:39 -0700

    Update ReplyPipelines, use more efficent reply count calculation

--- app/Jobs/CommentPipeline/CommentPipeline.php
+++ app/Jobs/CommentPipeline/CommentPipeline.php
@@ -60,8 +60,7 @@ class CommentPipeline implements ShouldQueue
         $actor = $comment->profile;
 
         if(config('database.default') === 'mysql') {
-            $count = DB::select( DB::raw("select id, in_reply_to_id from statuses, (select @pv := :kid) initialisation where id > @pv and find_in_set(in_reply_to_id, @pv) > 0 and @pv := concat(@pv, ',', id)"), [ 'kid' => $status->id]);
-            $status->reply_count = count($count);
+            $status->reply_count = $status->reply_count + 1;
             $status->save();
         }
 
