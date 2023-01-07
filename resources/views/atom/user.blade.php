31cefa4b9e4116f3c738d1bc1cb997685bdd3dd6
Harm te Molder <mail@harmtemolder.com>
2023-01-07 18:21:21 +0100

    Fix missing curly brace

--- resources/views/atom/user.blade.php
+++ resources/views/atom/user.blade.php
@@ -1,19 +1,19 @@
 <?=
-	/* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
-	'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
+/* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
+'<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
 	<id>{{$permalink}}</id>
 	<title>{{$profile['username']}} on Pixelfed</title>
 	<subtitle type="html">{{$profile['note']}}</subtitle>
-	<updated>{{$items[0]['created_at']}</updated>
+	<updated>{{$items[0]['created_at']}}</updated>
 	<author>
 		<name>{{$profile['username']}}</name>
 		<uri>{{$profile['url']}}</uri>
 	</author>
-	<link rel="alternate" type="text/html" href="{{$profile['url']}}"/>
-	<link rel="self" type="application/atom+xml" href="{{$permalink}}"/>
-@foreach($items as $item)
+	<link rel="alternate" type="text/html" href="{{$profile['url']}}" />
+	<link rel="self" type="application/atom+xml" href="{{$permalink}}" />
+	@foreach($items as $item)
 	<entry>
 		<id>{{ $item['url'] }}</id>
 		<title>{{ $item['content_text'] ? $item['content_text'] : "No caption" }}</title>
@@ -23,14 +23,14 @@
 			<uri>{{$profile['url']}}</uri>
 		</author>
 		<content type="html">
-		<![CDATA[
+			<![CDATA[
 			<img id="rss_item_{{$loop->iteration}}" src="{{ $item['media_attachments'][0]['url'] }}" alt="{{ $item['media_attachments'][0]['description'] }}">
 			<p style="padding:10px;">{{ $item['content'] }}</p>
-		]]>
+			]]>
 		</content>
 		<link rel="alternate" href="{{ $item['url'] }}" />
-		<summary>{{ $item['content'] }}</summary>
+		<summary type="html">{{ $item['content'] }}</summary>
 		<media:content url="{{ $item['media_attachments'][0]['url'] }}" type="image/png" medium="image">
 	</entry>
-@endforeach
+	@endforeach
 </feed>
