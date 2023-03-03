6153b620bfe977750d1211cf7f7334f15a6913f0
Daniel Supernault <danielsupernault@gmail.com>
2023-03-03 04:08:29 -0700

    Update FollowPipeline, fix followers_count and following_count counters

--- app/Jobs/FollowPipeline/FollowPipeline.php
+++ app/Jobs/FollowPipeline/FollowPipeline.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs\FollowPipeline;
 
+use App\Follower;
 use App\Notification;
 use Cache;
 use Illuminate\Bus\Queueable;
@@ -11,6 +12,8 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Log;
 use Illuminate\Support\Facades\Redis;
+use App\Services\AccountService;
+use App\Services\FollowerService;
 
 class FollowPipeline implements ShouldQueue
 {
@@ -46,8 +49,44 @@ class FollowPipeline implements ShouldQueue
 		$actor = $follower->actor;
 		$target = $follower->target;
 
+		if(!$actor || !$target) {
+			return;
+		}
+
 		Cache::forget('profile:following:' . $actor->id);
 		Cache::forget('profile:following:' . $target->id);
+
+		FollowerService::add($actor->id, $target->id);
+
+		$actorProfileSync = Cache::get(FollowerService::FOLLOWING_SYNC_KEY . $actor->id);
+		if(!$actorProfileSync) {
+			FollowServiceWarmCache::dispatch($actor->id)->onQueue('low');
+		} else {
+			if($actor->following_count) {
+				$actor->increment('following_count');
+			} else {
+				$count = Follower::whereProfileId($actor->id)->count();
+				$actor->following_count = $count;
+				$actor->save();
+			}
+			Cache::put(FollowerService::FOLLOWING_SYNC_KEY . $actor->id, 1, 604800);
+			AccountService::del($actor->id);
+		}
+
+		$targetProfileSync = Cache::get(FollowerService::FOLLOWERS_SYNC_KEY . $target->id);
+		if(!$targetProfileSync) {
+			FollowServiceWarmCache::dispatch($target->id)->onQueue('low');
+		} else {
+			if($target->followers_count) {
+				$target->increment('followers_count');
+			} else {
+				$count = Follower::whereFollowingId($target->id)->count();
+				$target->followers_count = $count;
+				$target->save();
+			}
+			Cache::put(FollowerService::FOLLOWERS_SYNC_KEY . $target->id, 1, 604800);
+			AccountService::del($target->id);
+		}
 
 		if($target->domain || !$target->private_key) {
 			return;
---
+++ app/Jobs/FollowPipeline/UnfollowPipeline.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Jobs\FollowPipeline;
+
+use App\Follower;
+use App\FollowRequest;
+use App\Notification;
+use App\Profile;
+use Cache;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Log;
+use Illuminate\Support\Facades\Redis;
+use App\Services\AccountService;
+use App\Services\FollowerService;
+use App\Services\NotificationService;
+
+class UnfollowPipeline implements ShouldQueue
+{
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+	protected $actor;
+	protected $target;
+
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($actor, $target)
+	{
+		$this->actor = $actor;
+		$this->target = $target;
+	}
+
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		$actor = $this->actor;
+		$target = $this->target;
+
+		$actorProfile = Profile::find($actor);
+		if(!$actorProfile) {
+			return;
+		}
+		$targetProfile = Profile::find($target);
+		if(!$targetProfile) {
+			return;
+		}
+
+		FollowerService::remove($actor, $target);
+
+		$actorProfileSync = Cache::get(FollowerService::FOLLOWING_SYNC_KEY . $actor);
+		if(!$actorProfileSync) {
+			FollowServiceWarmCache::dispatch($actor)->onQueue('low');
+		} else {
+			if($actorProfile->following_count) {
+				$actorProfile->decrement('following_count');
+			} else {
+				$count = Follower::whereProfileId($actor)->count();
+				$actorProfile->following_count = $count;
+				$actorProfile->save();
+			}
+			Cache::put(FollowerService::FOLLOWING_SYNC_KEY . $actor, 1, 604800);
+			AccountService::del($actor);
+		}
+
+		$targetProfileSync = Cache::get(FollowerService::FOLLOWERS_SYNC_KEY . $target);
+		if(!$targetProfileSync) {
+			FollowServiceWarmCache::dispatch($target)->onQueue('low');
+		} else {
+			if($targetProfile->followers_count) {
+				$targetProfile->decrement('followers_count');
+			} else {
+				$count = Follower::whereFollowingId($target)->count();
+				$targetProfile->followers_count = $count;
+				$targetProfile->save();
+			}
+			Cache::put(FollowerService::FOLLOWERS_SYNC_KEY . $target, 1, 604800);
+			AccountService::del($target);
+		}
+
+		if($targetProfile->domain == null) {
+			Notification::withTrashed()
+				->whereProfileId($target)
+				->whereAction('follow')
+				->whereActorId($actor)
+				->whereItemId($target)
+				->whereItemType('App\Profile')
+				->get()
+				->each(function($n) {
+					NotificationService::del($n->profile_id, $n->id);
+					$n->forceDelete();
+				});
+		}
+
+		if($actorProfile->domain == null && config('instance.timeline.home.cached')) {
+			Cache::forget('pf:timelines:home:' . $actor);
+		}
+
+		FollowRequest::whereFollowingId($target)
+			->whereFollowerId($actor)
+			->delete();
+
+		return;
+	}
+}
