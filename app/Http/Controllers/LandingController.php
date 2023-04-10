b716926b9c934dd917038d40b3afa19cc2057110
Daniel Supernault <danielsupernault@gmail.com>
2023-04-09 20:06:17 -0600

    Update LandingController, fix configonfig variable names

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -26,14 +26,14 @@ class LandingController extends Controller
     		return redirect('/');
     	}
 
-    	abort_if(config_cache('landing.show_explore_feed') != 1, 404);
+    	abort_if(config('instance.landing.show_explore') == false, 404);
 
     	return view('site.index');
     }
 
     public function getDirectoryApi(Request $request)
     {
-    	abort_if(config_cache('landing.show_directory') != 1, 404);
+    	abort_if(config('instance.landing.show_directory') == false, 404);
 
     	return DirectoryProfile::collection(
     		Profile::whereNull('domain')
