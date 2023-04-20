848181913f77de87d649fb89596535db12a674cb
Daniel Supernault <danielsupernault@gmail.com>
2023-04-19 23:28:42 -0600

    Update http kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
     ];
 
     /**
@@ -42,7 +43,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             'bindings',
-            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
