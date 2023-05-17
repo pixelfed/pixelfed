75db5116b7453115b192af9d4379148509fe3105
Daniel Supernault <danielsupernault@gmail.com>
2023-05-17 03:53:05 -0600

    Add AutospamCustomTokens model + migration

---
+++ config/autospam.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+	/*
+    |--------------------------------------------------------------------------
+    | Enable Autospam
+    |--------------------------------------------------------------------------
+    |
+    | Autospam uses NLP and other techniques to detect and mitigate potential
+    | spam posts from users on your server.
+    | We recommend enabling this when you have open registrations, regardless
+    | of how many users you have.
+    |
+    */
+
+    'enabled' => env('PF_BOUNCER_ENABLED', false),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ignored Tokens
+    |--------------------------------------------------------------------------
+    |
+    | Ignored tokens are for commonly used words that may impact the detection.
+    | These tokens should be lowercase and not contain spaces or non alpha-
+    | numerical characters and be in comma-separated format.
+    |
+    */
+
+    'ignored_tokens' => env('PF_AUTOSPAM_IGNORED_TOKENS', 'the,a,of,and'),
+
+    'nlp' => [
+    	'enabled' => false,
+    	'spam_sample_limit' => env('PF_AUTOSPAM_NLP_SPAM_SAMPLE_LIMIT', 200),
+    ]
+];
