ce54d29c695ce3f783817af194be86af6e3f493e
Daniel Supernault <danielsupernault@gmail.com>
2023-11-11 03:40:59 -0700

    Update delete pipelines, properly invoke StatusHashtag delete events

--- app/Jobs/StatusPipeline/RemoteStatusDelete.php
+++ app/Jobs/StatusPipeline/RemoteStatusDelete.php
@@ -153,7 +153,10 @@ class RemoteStatusDelete implements ShouldQueue, ShouldBeUniqueUntilProcessing
             ->whereObjectId($status->id)
             ->delete();
         StatusArchived::whereStatusId($status->id)->delete();
-        StatusHashtag::whereStatusId($status->id)->delete();
+        $statusHashtags = StatusHashtag::whereStatusId($status->id)->get();
+        foreach($statusHashtags as $stag) {
+        	$stag->delete();
+        }
         StatusView::whereStatusId($status->id)->delete();
         Status::whereInReplyToId($status->id)->update(['in_reply_to_id' => null]);
 
