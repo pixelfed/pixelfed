9f915b5bacf8739e3444ffc1a3fbf8a4bb617666
daniel <danielsupernault@gmail.com>
2023-12-12 17:00:02 -0700

    New translations web.php (Catalan) [ci skip]

--- resources/lang/ca/web.php
+++ resources/lang/ca/web.php
@@ -3,45 +3,45 @@
 return [
 
 	'common' => [
-		'comment' => 'Comentar',
-		'commented' => 'Comentari',
+		'comment' => 'Comenta',
+		'commented' => 'Comentat',
 		'comments' => 'Comentaris',
 		'like' => 'M\'agrada',
 		'liked' => 'M\'ha agradat',
-		'likes' => '"M\'agrada"',
+		'likes' => 'Agradaments',
 		'share' => 'Comparteix',
 		'shared' => 'S\'han compartit',
 		'shares' => 'S\'han compartit',
 		'unshare' => 'Deixa de compartir',
-		'bookmark' => 'Bookmark',
+		'bookmark' => 'Afegeix als marcadors',
 
 		'cancel' => 'Cancel·la',
 		'copyLink' => 'Copia l\'enllaç',
-		'delete' => 'Esborra',
+		'delete' => 'Elimina',
 		'error' => 'Error',
 		'errorMsg' => 'Alguna cosa ha anat malament. Siusplau, intenta-ho més tard.',
-		'oops' => 'Uix!',
+		'oops' => 'Vaja!',
 		'other' => 'Altres',
-		'readMore' => 'Llegiu-ne més',
+		'readMore' => 'Més informació',
 		'success' => 'Completat amb èxit',
-		'proceed' => 'Proceed',
-		'next' => 'Next',
-		'close' => 'Close',
-		'clickHere' => 'click here',
+		'proceed' => 'Continua',
+		'next' => 'Següent',
+		'close' => 'Tanca',
+		'clickHere' => 'fes clic aquí',
 
 		'sensitive' => 'Sensible',
 		'sensitiveContent' => 'Contingut sensible',
-		'sensitiveContentWarning' => 'Aquest article pot contenir contingut sensible',
+		'sensitiveContentWarning' => 'Aquesta publicació pot incloure contingut sensible',
 	],
 
 	'site' => [
 		'terms' => 'Condicions d\'ús',
-		'privacy' => 'Política de Privacitat',
+		'privacy' => 'Política de privadesa',
 	],
 
 	'navmenu' => [
-		'search' => 'Cercar',
-		'admin' => 'Tauler d\'Administració',
+		'search' => 'Cerca',
+		'admin' => 'Tauler d\'administració',
 
 		// Timelines
 		'homeFeed' => 'Línia de temps principal',
@@ -58,8 +58,8 @@ return [
 		// Self links
 		'profile' => 'Perfil',
 		'drive' => 'Unitat',
-		'settings' => 'Paràmetres',
-		'compose' => 'Crea un nou',
+		'settings' => 'Configuració',
+		'compose' => 'Crea una nova',
 		'logout' => 'Tanca la sessió',
 
 		// Nav footer
@@ -70,13 +70,13 @@ return [
 		'terms' => 'Termes',
 
 		// Temporary links
-		'backToPreviousDesign' => 'Tornar al disseny anterior'
+		'backToPreviousDesign' => 'Torna al disseny anterior'
 	],
 
 	'directMessages' => [
 		'inbox' => 'Safata d\'entrada',
-		'sent' => 'Enviat',
-		'requests' => 'Sol·licitud'
+		'sent' => 'Enviats',
+		'requests' => 'Sol·licituds'
 	],
 
 	'notifications' => [
@@ -89,9 +89,9 @@ return [
 		'updatedA' => 'actualitzat a',
 		'sentA' => 'enviat a',
 
-		'followed' => 'seguits',
-		'mentioned' => 'mencionat',
-		'you' => 'vostè',
+		'followed' => 't\'ha seguit',
+		'mentioned' => 'mencions',
+		'you' => 'a vós',
 
 		'yourApplication' => 'La teva sol·licitud per unir-te',
 		'applicationApproved' => 'està aprovat!',
@@ -102,14 +102,14 @@ return [
 		'modlog' => 'modlog',
 		'post' => 'publicació',
 		'story' => 'història',
-		'noneFound' => 'No notifications found',
+		'noneFound' => 'No hi ha notificacions',
 	],
 
 	'post' => [
-		'shareToFollowers' => 'Comparteix amb els seguidors',
-		'shareToOther' => 'Compartits per altres',
-		'noLikes' => 'Cap m\'agrada encara',
-		'uploading' => 'Carregant',
+		'shareToFollowers' => 'Comparteix amb qui et segueix',
+		'shareToOther' => 'Comparteix amb altri',
+		'noLikes' => 'Cap m\'agrada',
+		'uploading' => 'S\'està pujant',
 	],
 
 	'profile' => [
@@ -119,28 +119,28 @@ return [
 		'admin' => 'Administrador',
 		'collections' => 'Col·leccions',
 		'follow' => 'Segueix',
-		'unfollow' => 'Deixeu de seguir',
-		'editProfile' => 'Edita el teu perfil',
-		'followRequested' => 'Sol·licitud de seguidor',
+		'unfollow' => 'Deixa de seguir',
+		'editProfile' => 'Edita el perfil',
+		'followRequested' => 'Sol·licituds de seguiment',
 		'joined' => 'S\'ha unit',
 
-		'emptyCollections' => 'Sembla que no trobem cap col·lecció',
-		'emptyPosts' => 'Sembla que no trobem cap publicació',
+		'emptyCollections' => 'No s\'ha trobat cap col·lecció',
+		'emptyPosts' => 'No s\'ha trobat cap publicació',
 	],
 
 	'menu' => [
-		'viewPost' => 'Veure publicació',
+		'viewPost' => 'Mostra la publicació',
 		'viewProfile' => 'Mostra el perfil',
 		'moderationTools' => 'Eines de moderació',
-		'report' => 'Informe',
-		'archive' => 'Arxiu',
+		'report' => 'Denuncia',
+		'archive' => 'Arxiva',
 		'unarchive' => 'Desarxiva',
 		'embed' => 'Incrusta',
 
-		'selectOneOption' => 'Seleccioneu una de les opcions següents',
+		'selectOneOption' => 'Selecciona una de les següents opcions',
 		'unlistFromTimelines' => 'Desllista de les línies de temps',
-		'addCW' => 'Afegeix advertència de contingut',
-		'removeCW' => 'Esborra advertència de contingut',
+		'addCW' => 'Afegeix avís de contingut',
+		'removeCW' => 'Elimina l\'avís de contingut',
 		'markAsSpammer' => 'Marca com a brossa',
 		'markAsSpammerText' => 'Desllista + CW publicacions existents i futures',
 		'spam' => 'Contingut brossa',
@@ -148,61 +148,61 @@ return [
 		'abusive' => 'Abusiu o nociu',
 		'underageAccount' => 'Compte de menors d\'edat',
 		'copyrightInfringement' => 'Infracció de drets d’autor',
-		'impersonation' => 'Suplantacions',
-		'scamOrFraud' => 'Estafa o Frau',
-		'confirmReport' => 'Confirma l\'informe',
-		'confirmReportText' => 'Esteu segur que voleu informar d\'aquesta publicació?',
-		'reportSent' => 'Informe enviat!',
-		'reportSentText' => 'Hem rebut correctament el vostre informe.',
-		'reportSentError' => 'Hi ha hagut un problema en informar d\'aquesta publicació.',
+		'impersonation' => 'Suplantació d\'identitat',
+		'scamOrFraud' => 'Estafa o frau',
+		'confirmReport' => 'Confirma la denúncia',
+		'confirmReportText' => 'Segur que voleu denunciar aquesta publicació?',
+		'reportSent' => 'S\'ha enviat la denúncia',
+		'reportSentText' => 'Hem rebut correctament la teva denúncia.',
+		'reportSentError' => 'Hi ha hagut un problema en denunciar aquesta publicació.',
 
-		'modAddCWConfirm' => 'Confirmes que vols afegir un avís de contingut a aquesta publicació?',
-		'modCWSuccess' => 'Avís de contingut afegit correctament',
-		'modRemoveCWConfirm' => 'Confirmes que vols esborrar un avís de contingut d\'aquesta publicació?',
+		'modAddCWConfirm' => 'Segur que voleu afegir un avís de contingut a aquesta publicació?',
+		'modCWSuccess' => 'S\'ha afegit l\'avís de contingut correctament',
+		'modRemoveCWConfirm' => 'Segur que voleu eliminar l\'avís de contingut d\'aquesta publicació?',
 		'modRemoveCWSuccess' => 'Avís de contingut esborrat correctament',
 		'modUnlistConfirm' => 'Esteu segur que voleu desllistar d\'aquesta publicació?',
 		'modUnlistSuccess' => 'Entrada desllistada amb èxit',
-		'modMarkAsSpammerConfirm' => 'Esteu segur que voleu marcar aquest usuari com a brossa? Totes les publicacions existents i futures no apareixeran a les cronologies i s\'aplicarà un avís de contingut.',
+		'modMarkAsSpammerConfirm' => 'Segur que voleu marcar aquest usuari com a brossa? Totes les publicacions existents i futures desapareixeran de les cronologies i s\'aplicarà un avís de contingut.',
 		'modMarkAsSpammerSuccess' => 'El compte s\'ha marcat correctament com a brossa',
 
 		'toFollowers' => 'els seguidors',
 
-		'showCaption' => 'Mostra el subtítol',
+		'showCaption' => 'Mostra la descripció',
 		'showLikes' => 'Mostra els m\'agrada',
-		'compactMode' => 'Mode Compacte',
+		'compactMode' => 'Mode compacte',
 		'embedConfirmText' => 'En utilitzar aquesta inserció, accepteu el nostre',
 
-		'deletePostConfirm' => 'Esteu segur que voleu suprimir aquesta publicació?',
-		'archivePostConfirm' => 'Segur que voleu arxivar aquesta publicació?',
-		'unarchivePostConfirm' => 'Segur que voleu desarxivar aquesta publicació?',
+		'deletePostConfirm' => 'Segur que voleu eliminar aquesta publicació?',
+		'archivePostConfirm' => 'Segur que vols arxivar aquesta publicació?',
+		'unarchivePostConfirm' => 'Segur que vols desarxivar aquesta publicació?',
 	],
 
 	'story' => [
-		'add' => 'Afegir història'
+		'add' => 'Afegeix història'
 	],
 
 	'timeline' => [
-		'peopleYouMayKnow' => 'Gent que potser coneixes',
+		'peopleYouMayKnow' => 'Gent que potser coneixeu',
 
 		'onboarding' => [
-			'welcome' => 'Welcome',
-			'thisIsYourHomeFeed' => 'This is your home feed, a chronological feed of posts from accounts you follow.',
-			'letUsHelpYouFind' => 'Let us help you find some interesting people to follow',
-			'refreshFeed' => 'Refresh my feed',
+			'welcome' => 'Us donem la benvinguda',
+			'thisIsYourHomeFeed' => 'Aquesta és la vostra línia de temps principal que mostra cronològicament les publicacions de comptes que seguiu.',
+			'letUsHelpYouFind' => 'Ens agradaria ajudar-vos a trobar gent interessant per seguir',
+			'refreshFeed' => 'Actualitza la meva línia de temps',
 		],
 	],
 
 	'hashtags' => [
-		'emptyFeed' => 'Sembla que no trobem cap publicació per a aquesta etiqueta'
+		'emptyFeed' => 'No hi ha cap publicació per a aquesta etiqueta'
 	],
 
 	'report' => [
-		'report' => 'Report',
-		'selectReason' => 'Select a reason',
-		'reported' => 'Reported',
-		'sendingReport' => 'Sending report',
-		'thanksMsg' => 'Thanks for the report, people like you help keep our community safe!',
-		'contactAdminMsg' => 'If you\'d like to contact an administrator about this post or report',
+		'report' => 'Denuncia',
+		'selectReason' => 'Tria una raó',
+		'reported' => 'Denunciat',
+		'sendingReport' => 'S\'està enviant la denúncia',
+		'thanksMsg' => 'Gràcies per la denúncia. Gent com vós fa que la nostra comunitat sigui segura!',
+		'contactAdminMsg' => 'Si voleu contactar amb un o una administradora sobre aquesta publicació o denúncia',
 	],
 
 ];
