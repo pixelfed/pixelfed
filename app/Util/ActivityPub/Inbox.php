c89dc45e8d8010282a5605537a665151d6c7790c
Daniel Supernault <danielsupernault@gmail.com>
2023-12-18 22:37:34 -0700

    Update Inbox, add user domain blocks to Follow handler

--- app/Util/ActivityPub/Inbox.php
+++ app/Util/ActivityPub/Inbox.php
@@ -514,6 +514,10 @@ class Inbox
             return;
         }
 
+        if(AccountService::blocksDomain($target->id, $actor->domain) == true) {
+            return;
+        }
+
         if(
             Follower::whereProfileId($actor->id)
                 ->whereFollowingId($target->id)
