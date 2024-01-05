c1fbccb07c6419dd809c9a273af33868b6c43a73
Christian Winther <jippignu@gmail.com>
2024-01-05 16:52:00 +0000

    bootstrapping worked

--- contrib/docker/shared/root/docker/entrypoint.d/10-storage.sh
+++ contrib/docker/shared/root/docker/entrypoint.d/10-storage.sh
@@ -3,8 +3,11 @@ source /docker/helpers.sh
 
 entrypoint-set-script-name "$0"
 
+run-as-current-user chown --verbose ${RUNTIME_UID}:${RUNTIME_GID} "./bootstrap/cache"
+run-as-current-user chown --verbose ${RUNTIME_UID}:${RUNTIME_GID} "./storage"
+
 # Copy the [storage/] skeleton files over the "real" [storage/] directory so assets are updated between versions
-cp --recursive storage.skel/. storage/
+run-as-runtime-user cp --recursive storage.skel/. ./storage/
 
 # Ensure storage linkk are correctly configured
-php artisan storage:link
+run-as-runtime-user php artisan storage:link
