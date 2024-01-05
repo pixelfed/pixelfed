052c11882c0da9ea4ddd50b6141242bba99d1e01
Christian Winther <jippignu@gmail.com>
2024-01-05 16:33:08 +0000

    tweak 10-storage.sh

--- contrib/docker/shared/root/docker/entrypoint.d/10-storage.sh
+++ contrib/docker/shared/root/docker/entrypoint.d/10-storage.sh
@@ -4,7 +4,7 @@ source /docker/helpers.sh
 entrypoint-set-script-name "$0"
 
 # Copy the [storage/] skeleton files over the "real" [storage/] directory so assets are updated between versions
-run-as-runtime-user cp --recursive storage.skel/. storage/.
+cp --recursive storage.skel/. storage/
 
 # Ensure storage linkk are correctly configured
-run-as-runtime-user php artisan storage:link
+php artisan storage:link
