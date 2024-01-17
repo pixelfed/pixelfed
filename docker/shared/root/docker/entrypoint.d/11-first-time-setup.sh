44266b950b0b1685df8ce6d57705b6f37cf90cc6
Christian Winther <jippignu@gmail.com>
2024-01-17 14:29:24 +0000

    conditionally initialize passport and instance actor

--- docker/shared/root/docker/entrypoint.d/11-first-time-setup.sh
+++ docker/shared/root/docker/entrypoint.d/11-first-time-setup.sh
@@ -19,7 +19,15 @@ fi
 load-config-files
 await-database-ready
 
-only-once "storage:link" run-as-runtime-user php artisan storage:link
 only-once "key:generate" run-as-runtime-user php artisan key:generate
+only-once "storage:link" run-as-runtime-user php artisan storage:link
 only-once "initial:migrate" run-as-runtime-user php artisan migrate --force
 only-once "import:cities" run-as-runtime-user php artisan import:cities
+
+if is-true "${ACTIVITY_PUB:-false}"; then
+    only-once "instance:actor" run-as-runtime-user php artisan instance:actor
+fi
+
+if is-true "${OAUTH_ENABLED:-false}"; then
+    only-once "passport:keys" run-as-runtime-user php artisan passport:keys
+fi
