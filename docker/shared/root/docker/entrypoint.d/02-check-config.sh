f3284d5be3ce57bcdb086eee178a9edd6f1d0fbd
Christian Winther <jippignu@gmail.com>
2024-01-26 20:14:40 +0000

    fix 02-check-config.sh logic and bad .env.docker syntax

--- docker/shared/root/docker/entrypoint.d/02-check-config.sh
+++ docker/shared/root/docker/entrypoint.d/02-check-config.sh
@@ -8,11 +8,10 @@ entrypoint-set-script-name "$0"
 
 # Validating dot-env files for any issues
 for file in "${dot_env_files[@]}"; do
-    if file-exists "$file"; then
+    if ! file-exists "${file}"; then
         log-warning "Could not source file [${file}]: does not exists"
         continue
     fi
 
-    log-info "Linting dotenv file ${file}"
-    dotenv-linter --skip=QuoteCharacter --skip=UnorderedKey "${file}"
+    run-as-current-user dotenv-linter --skip=QuoteCharacter --skip=UnorderedKey "${file}"
 done
