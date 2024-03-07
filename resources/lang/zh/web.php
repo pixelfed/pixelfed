3601ae88d74947712412b9ddd34597cc453fba5a
daniel <danielsupernault@gmail.com>
2024-03-07 00:11:55 -0700

    New translations web.php (Chinese Simplified) [ci skip]

--- resources/lang/zh/web.php
+++ resources/lang/zh/web.php
@@ -7,8 +7,8 @@ return [
 		'commented' => '已评论',
 		'comments' => '评论',
 		'like' => '赞',
-		'liked' => '已赞',
-		'likes' => '赞',
+		'liked' => '已点赞',
+		'likes' => '点赞',
 		'share' => '分享',
 		'shared' => '已分享的',
 		'shares' => '分享',
@@ -29,9 +29,9 @@ return [
 		'close' => '关闭',
 		'clickHere' => '点击此处',
 
-		'sensitive' => '敏感的',
+		'sensitive' => '敏感',
 		'sensitiveContent' => '敏感内容',
-		'sensitiveContentWarning' => '此帖子可能包含敏感内容',
+		'sensitiveContentWarning' => '此帖文可能包含敏感内容',
 	],
 
 	'site' => [
@@ -45,8 +45,8 @@ return [
 
 		// Timelines
 		'homeFeed' => '主页',
-		'localFeed' => '本站时间轴',
-		'globalFeed' => '跨站时间轴',
+		'localFeed' => '本站动态',
+		'globalFeed' => '跨站动态',
 
 		// Core features
 		'discover' => '探索',
@@ -80,7 +80,7 @@ return [
 	],
 
 	'notifications' => [
-		'liked' => '赞了你的',
+		'liked' => '点赞了你的',
 		'commented' => '评论了你的',
 		'reacted' => '回应了你的',
 		'shared' => '转发了你的',
@@ -98,9 +98,9 @@ return [
 		'applicationRejected' => '的申请被拒绝。您可以在 6 个月后重新申请加入。',
 
 		'dm' => '私信',
-		'groupPost' => '群组帖子',
-		'modlog' => '模组日志',
-		'post' => '帖子',
+		'groupPost' => '群组贴文',
+		'modlog' => '管理日志',
+		'post' => '帖文',
 		'story' => '故事',
 		'noneFound' => '暂无通知',
 	],
@@ -125,56 +125,56 @@ return [
 		'joined' => '已加入',
 
 		'emptyCollections' => '我们似乎找不到任何影集',
-		'emptyPosts' => '我们似乎未能找到任何帖子',
+		'emptyPosts' => '我们似乎未能找到任何帖文',
 	],
 
 	'menu' => [
-		'viewPost' => '查看帖子',
+		'viewPost' => '查看帖文',
 		'viewProfile' => '查看个人资料',
-		'moderationTools' => '运营管理工具',
+		'moderationTools' => '管理工具',
 		'report' => '举报',
 		'archive' => '封存',
 		'unarchive' => '取消封存',
 		'embed' => '嵌入',
 
 		'selectOneOption' => '请从下列选项中选择一个',
-		'unlistFromTimelines' => '从时间轴中隐藏',
+		'unlistFromTimelines' => '从时间线中隐藏',
 		'addCW' => '添加内容警告',
 		'removeCW' => '移除内容警告',
-		'markAsSpammer' => '标记为垃圾信息发送者',
-		'markAsSpammerText' => '将所有未来的帖子隐藏并添加内容警告',
-		'spam' => '垃圾信息',
+		'markAsSpammer' => '标记为骚扰信息发送者',
+		'markAsSpammerText' => '将所有目前及未来的帖子隐藏并添加内容警告',
+		'spam' => '骚扰信息',
 		'sensitive' => '敏感内容',
 		'abusive' => '恶意或有害的',
 		'underageAccount' => '未成年账户',
 		'copyrightInfringement' => '版权侵犯',
 		'impersonation' => '冒充他人',
-		'scamOrFraud' => '欺诈或欺诈行为',
+		'scamOrFraud' => '骚扰或欺诈行为',
 		'confirmReport' => '确认举报',
-		'confirmReportText' => '您确定要举报这篇帖子吗？',
+		'confirmReportText' => '您确定要举报这篇帖文吗？',
 		'reportSent' => '举报已发送！',
 		'reportSentText' => '我们已成功收到您的举报。',
 		'reportSentError' => '举报这个帖子时出现问题。',
 
-		'modAddCWConfirm' => '您确定要为这个帖子添加内容警告？',
+		'modAddCWConfirm' => '您确定要为这个帖文添加内容警告？',
 		'modCWSuccess' => '内容警告已成功添加',
-		'modRemoveCWConfirm' => '您确定要移除这个帖子上的内容警告吗？',
+		'modRemoveCWConfirm' => '您确定要移除这个帖文上的内容警告吗？',
 		'modRemoveCWSuccess' => '内容警告已被成功移除',
-		'modUnlistConfirm' => '您确定要隐藏这个帖子吗？',
-		'modUnlistSuccess' => '帖子已被成功隐藏',
+		'modUnlistConfirm' => '您确定要隐藏这个帖文吗？',
+		'modUnlistSuccess' => '帖文已被成功隐藏',
 		'modMarkAsSpammerConfirm' => '您确定要将此用户标记为垃圾信息发送者？所有此用户的现有和未来的帖子都将被隐藏，并添加内容警告。',
 		'modMarkAsSpammerSuccess' => '成功地将帐户标记为垃圾信息发送者',
 
 		'toFollowers' => '至关注者',
 
 		'showCaption' => '显示标题',
-		'showLikes' => '显示赞',
+		'showLikes' => '显示点赞',
 		'compactMode' => '紧凑模式',
 		'embedConfirmText' => '使用这段嵌入内容代表您将同意我们的',
 
 		'deletePostConfirm' => '您确定要删除该贴吗？',
-		'archivePostConfirm' => '您确定要封存这个帖子吗？',
-		'unarchivePostConfirm' => '您确定要取消封存这个帖子吗？',
+		'archivePostConfirm' => '您确定要封存此贴文吗？',
+		'unarchivePostConfirm' => '您确定要取消封存此贴文吗？',
 	],
 
 	'story' => [
