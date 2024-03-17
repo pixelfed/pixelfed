1eadff9d2ec97a4af80a9ef807e29636a3df5a11
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-03-17 21:43:16 +0100

    Adjust CORS configuration to support API & OAuth Routes
    Fixes #4411 and #3381

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,12 +14,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
     ];
 
     /**
