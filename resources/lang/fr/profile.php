773718332ebf17ab449efbbac56b22b9f6f8b2db
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-01-26 15:56:52 +0100

    Update profile.php

--- resources/lang/fr/profile.php
+++ resources/lang/fr/profile.php
@@ -4,7 +4,7 @@ return [
     'emptyTimeline'             =>    'Cet·te utilisateur·rice n\'a pas encore de publications !',
     'emptyFollowers'            =>    'Cet·te utilisateur·rice n`\'a pas encore d\'abonné·e·s !',
     'emptyFollowing'            =>    'Cet·te utilisateur·rice ne suit personne pour le moment !',
-    'emptySaved'                =>    'Vous n\'avez sauvegardé aucune publication pour le moment !',
+    'emptySaved'                =>    'Vous n\'avez enregistré aucune publication pour le moment !',
     'savedWarning'              =>    'Vous seul pouvez voir ce que vous avez enregistré',
     'privateProfileWarning'     =>    'Ce compte est privé',
     'alreadyFollow'             =>    'N\'êtes vous pas déjà abonné·e à :username ?',
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header'    =>    'Profil indisponible',
     'status.disabled.body'      =>    'Désolé, ce profil n’est pas disponible pour le moment. Veuillez réessayer plus tard.',
+
+    'block.domain.max'          =>    'Vous avez atteint la limite maximale de blocages de domaine ! Vous pouvez bloquer jusqu\'à :max domaines à la fois. Veuillez demander à votre admin d\'ajuster cette limite.',
+
+    'mutedAccounts'             =>    'Comptes masqués',
+    'blockedAccounts'           =>    'Comptes bloqués',
+    'blockedDomains'            =>    'Domaines bloqués',
 ];
