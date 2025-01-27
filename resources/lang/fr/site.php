560c1bd290fd0002e238ffdce1492d227862a397
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-01-27 18:03:23 +0100

    Update site.php

--- resources/lang/fr/site.php
+++ resources/lang/fr/site.php
@@ -13,4 +13,56 @@ return [
     'selectLocale'     =>    'Sélectionnez l\'une des langues prises en charge',
     'contact'          =>    'Contact',
     'contact-us'       =>    'Nous contacter',
+    'places'           =>    'Lieux',
+    'profiles'         =>    'Profils',
+
+    // site/contact
+    'you_can_contact_the_admins'                            => 'Vous pouvez contacter les admins',
+    'by_using_the_form_below'                               => 'en utilisant le formulaire ci-dessous',
+    'or'                                                    => 'ou',
+    'by_sending_an_email_to'                                => 'en envoyant un e-mail à',
+    'the_admins_have_not_set_a_contact_email_address'       => 'Les admins n\'ont pas fourni d\'adresse e-mail de contact',
+    'Message'                                               => 'Message',
+    'request_response_from_admins'                          => 'Demander une réponse des admins',
+    'Submit'                                                => 'Valider',
+    'log_in_to_send_a_message'                              => 'vous connecter pour envoyer un message',
+    'Please'                                                => 'Veuillez',
+
+    // site/about
+    'photo_sharing_for_everyone'                            => 'Partage de photos. Pour toutes et tous.',
+    'pixelfed_is_an_image_sharing_platform_etc'             => 'Pixelfed est une plateforme de partage d\'images et vidéos, une alternative éthique aux plateformes centralisées.', // this is actually never used because it's a fallback for config_cache('app.description') and config_cache('app.short_description') which seem to be impossible to set to empty when saved via /admin/settings?t=branding
+    'feature_packed'                                        => 'Avec des fonctionnalités natives.',
+    'the_best_for_the_brightest'                            => 'Le meilleur pour les plus belles 📸',
+    'albums'                                                => 'Albums',
+    'share_posts_with_up_to'                                => 'Partagez des publications contenant jusqu\'à',
+    'photos'                                                => 'photos',
+    'comments'                                              => 'Commentaires',
+    'comment_on_a_post_or_send_a_reply'                     => 'Commentez sur une publication, ou envoyez une réponse',
+    'collections'                                           => 'Collections',
+    'organize_and_share_collections_of_multiple_posts'      => 'Organisez et partagez des collections de plusieurs publications',
+    'discover'                                              => 'Découvrir',
+    'explore_categories_hashtags_and_topics'                => 'Explorez les catégories, hashtags et sujets',
+    'photo_filters'                                         => 'Filtres photo',
+    'add_a_special_touch_to_your_photos'                    => 'Ajoutez une touche spéciale à vos photos',
+    'stories'                                               => 'Stories',
+    'share_moments_with_your_followers_that_disappear_etc'  => 'Partagez des moments avec vos followers qui disparaissent après 24 heures',
+    'people_have_shared'                                    => 'personnes ont partagé',
+    'photos_and_videos_on'                                  => 'photos et vidéos sur',
+    'sign_up_today'                                         => 'Inscrivez-vous aujourd\'hui',
+    'and_join_our_community_of_photographers_from_etc'      => 'et rejoignez notre communauté de photographes à travers le monde.',
+
+    // site/fediverse
+    'is_a_portmanteau_of_federation_and_universe_etc'       => 'est un mot-valise de « fédération » et « univers ». C\'est un nom commun et informel pour une fédération de serveurs de réseaux sociaux, spécialisé dans différents types de médias.',
+    'supported_fediverse_projects'                          => 'Projets soutenus du Fediverse',
+    'some_of_the_better_known_fediverse_projects_include'   => 'Quelques-uns des projets populaires du Fediverse incluent :',
+    'a_federated_microblogging_alternative'                 => 'Une alternative fédérée de microblogage.',
+
+    // site/opensource
+    'the_software_that_powers_this_website_is_called'       => 'Le logiciel qui propulse ce site web s\'appelle',
+    'and_anyone_can'                                        => 'et n\'importe qui peut',
+    'download'                                              => 'télécharger',
+    'opensource.or'                                         => 'ou',
+    'view'                                                  => 'voir',
+    'the_source_code_and_run_their_own_instance'            => 'le code source et démarrer sa propre instance !',
+    'open_source_in_pixelfed'                               => 'Open Source chez Pixelfed',
 ];
