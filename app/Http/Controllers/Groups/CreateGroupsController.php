37e5162d40cda62e5dc2e89bab36e9064db37a22
Shlee <github@shl.ee>
2025-11-07 21:20:41 +1300

    Update CreateGroupsController.php

--- app/Http/Controllers/Groups/CreateGroupsController.php
+++ app/Http/Controllers/Groups/CreateGroupsController.php
@@ -44,12 +44,13 @@ class CreateGroupsController extends Controller
 
         $config = GroupService::config();
         abort_if($config['limits']['user']['create']['new'] == false && $request->user()->is_admin == false, 422, 'Invalid operation');
+        // $max = $config['limits']['user']['create']['max'];
         // abort_if(Group::whereProfileId($pid)->count() <= $max, 422, 'Group limit reached');
 
         $group = new Group;
         $group->profile_id = $pid;
         $group->name = $request->input('name');
-        $group->description = $request->input('description');
+        $group->description = $request->input('description', null);
         $group->is_private = $request->input('membership') == 'private';
         $group->local_only = $request->input('membership') == 'local';
         $group->metadata = $request->input('configuration');
