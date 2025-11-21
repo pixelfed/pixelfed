775b9d5c68cbbf57ba0fdb1483efe2d3a73a7066
Shlee <github@shl.ee>
2025-11-21 18:40:26 +1030

    Update AvatarOptimize.php

--- app/Jobs/AvatarPipeline/AvatarOptimize.php
+++ app/Jobs/AvatarPipeline/AvatarOptimize.php
@@ -134,7 +134,7 @@ class AvatarOptimize implements ShouldQueue
             $disk = Storage::disk(config('filesystems.cloud'));
 
             // Delete existing avatar folder in the cloud (if it exists)
-            //// Catches: "AvatarOptimize: uploadToCloud exception for profile: 492561745372893187 - Unable to delete directory located at: cache/avatars/xxxxxxxxxxxxxxxxxxxxxxxx"
+            //// Catches: "AvatarOptimize: uploadToCloud exception for profile: xxxxxxxxxxxxxxxxxxxxxxxx - Unable to delete directory located at: cache/avatars/xxxxxxxxxxxxxxxxxxxxxxxx"
             if ($disk->exists($base)) {
                 try {
                     $disk->deleteDirectory($base);
