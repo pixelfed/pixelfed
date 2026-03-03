b453d96eda306b289d25c7e3e564908640432ee9
David Heidelberg <david@ixit.cz>
2026-03-03 15:02:42 +0100

    New translations web.php (German) [ci-skip]
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- resources/lang/cs/auth.php
+++ resources/lang/cs/auth.php
@@ -2,18 +2,33 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
+    'verifyYourEmailAddress' => ' - Ověřte svoji emailovou adresu',
+    'loginTitle'       =>   'Přihlášení k účtu',
+    'failed'           =>   'Tyto přihlašovací údaje se neshodují s našemi záznamy.',
+    'throttle'         =>   'Příliš mnoho pokusů o přihlášení. Prosím zkuste to znovu za :seconds sekund.',
+    'password'         =>   'Heslo',
+    'remember'         =>   'Zapamatuj si mne',
+    'forgot'           =>   'Zapomenuté heslo',
+    'login'            =>   'Přihlášení',
 
-    'failed'   => 'Tyto přihlašovací údaje se neshodují s našemi záznamy.',
-    'throttle' => 'Příliš mnoho pokusů o přihlášení. Prosím zkuste to znovu za :seconds sekund.',
+    'register'         =>   'Registrace',
+    'reset'            =>   'Obnovení hesla',
+
+    'name'             =>   'Jméno',
+    'username'         =>   'Uživatelské jméno',
+    'confirm-password' =>   'Potvrdit heslo',
+
+    'age'              =>   'Je mi nejméně 16 let',
+    'terms'            =>   "Přihlášením souhlasíte s našimi <a href=\"".route('site.terms')."\" class=\"font-weight-bold text-dark\">Podmínkami použití</a> a <a href=\"".route('site.privacy')."\" class=\"font-weight-bold text-dark\">Privacy Policy</a>.",
+
+    'emailAddress'     =>   "E-mailová adresa",
+
+    'registerTitle'    =>   'Zaregistruj nový účet',
+
+    'sendReset'        =>   'Zaslat odkaz pro obnovení hesla',
+    'backLogin'        =>   'Zpět k přihlášení',
+
+    'signInMastodon'  => 'Přihlásit se s Mastodonem',
+
 
 ];
--- resources/lang/cs/site.php
+++ resources/lang/cs/site.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     'about'             => 'O nás',
     'help'              => 'Nápověda',
     'language'          => 'Jazyk',
@@ -15,4 +16,54 @@ return [
     'contact-us'        => 'Kontaktujte nás',
     'places'            => 'Místa',
     'profiles'          => 'Profily',
+
+    // site/contact
+    'you_can_contact_the_admins'                            => 'Můžete kontaktovat administrátory',
+    'by_using_the_form_below'                               => 'použitím formuláře níže',
+    'or'                                                    => 'nebo',
+    'by_sending_an_email_to'                                => 'napsáním emailu na ',
+    'the_admins_have_not_set_a_contact_email_address'       => 'Administrátor nenastavil kontaktní emailovou adresu',
+    'Message'                                               => 'Zpráva',
+    'request_response_from_admins'                          => 'Vyžádat odpověď od administátorů',
+    'Submit'                                                => 'Odeslat',
+    'log_in_to_send_a_message'                              => 'přihlašte se pro odeslání zprávy',
+    'Please'                                                => 'Prosím',
+
+    // site/about
+    'photo_sharing_for_everyone'                            => 'Sdílení fotek. Pro všechny',
+    'pixelfed_is_an_image_sharing_platform_etc'             => 'Pixelfed je platforma pro sdílení fotek, etická alternative k centralizovaným platformám.', // this is actually never used because it's a fallback for config_cache('app.description') and config_cache('app.short_description') which seem to be impossible to set to empty when saved via /admin/settings?t=branding
+    'feature_packed'                                        => 'Balíček vlastností.',
+    'the_best_for_the_brightest'                            => 'Nejlepší pro nejjasnější 📸',
+    'albums'                                                => 'Albumy',
+    'share_posts_with_up_to'                                => 'Sdílejte příspěvky s',
+    'photos'                                                => 'fotky',
+    'comments'                                              => 'Komentáře',
+    'comment_on_a_post_or_send_a_reply'                     => 'Komentuj příspěvek nebo napiš odpověď',
+    'collections'                                           => 'Kolekce',
+    'organize_and_share_collections_of_multiple_posts'      => 'Organizuj a sdílej kolekce více příspěvků',
+    'discover'                                              => 'Objev',
+    'explore_categories_hashtags_and_topics'                => 'Prozkoumej kategorie, hashtagy a témata',
+    'photo_filters'                                         => '',
+    'add_a_special_touch_to_your_photos'                    => 'Přidej třešničku na dortu ke svým fotkám',
+    'stories'                                               => 'Příběhy',
+    'share_moments_with_your_followers_that_disappear_etc'  => 'Sdílej okamžiky se svými sledujícími, které zmizí po 24 hodinách',
+    'people_have_shared'                                    => 'lidé sdíleli',
+    'photos_and_videos_on'                                  => 'fotky a videa na',
+    'sign_up_today'                                         => 'Zaregistruj se dnes',
+    'and_join_our_community_of_photographers_from_etc'      => 'a připoj se do naší komunity fotografů z celého světa.',
+
+    // site/fediverse
+    'is_a_portmanteau_of_federation_and_universe_etc'       => 'je spolek “federací” a “vesmírů”. Je běžné jméno pro federaci serverů sociálních sítí, specializujících se na různé sociální média.',
+    'supported_fediverse_projects'                          => 'Podporované projekty Fedivesmírů',
+    'some_of_the_better_known_fediverse_projects_include'   => 'Některé populárnější projekty Fedivesmírů zahrnují:',
+    'a_federated_microblogging_alternative'                 => 'Federovaná alternativa pro microblogging.',
+
+    // site/opensource
+    'the_software_that_powers_this_website_is_called'       => 'Software co pohání tuto stránku se nazývá',
+    'and_anyone_can'                                        => 'a každý může',
+    'download'                                              => 'stáhnout',
+    'opensource.or'                                         => 'nebo',
+    'view'                                                  => 'prohlížet',
+    'the_source_code_and_run_their_own_instance'            => 'otevřený zdrojový kód a provozovat vlastní instanci!',
+    'open_source_in_pixelfed'                               => 'Otevřený software v Pixelfedu',
 ];
