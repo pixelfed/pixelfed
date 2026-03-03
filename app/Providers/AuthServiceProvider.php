b635582d6f5bc422b38bfa97ba8334900883d56b
Severin <savewish@icloud.com>
2026-03-03 23:41:04 +0800

    Fix passport issues with new uuid
    remove uuid change as descibed in upgrade.md for v13.x

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('pixelfed.oauth_enabled') == true) {
+            Passport::$clientUuids = false;
             Passport::ignoreRoutes();
             Passport::tokensExpireIn(now()->addDays(config('instance.oauth.token_expiration', 356)));
             Passport::refreshTokensExpireIn(now()->addDays(config('instance.oauth.refresh_expiration', 400)));
