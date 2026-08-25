d9e55199f51d4af5c8aeee1e668afbf58f3b6cff
Shlee <github@shl.ee>
2026-08-25 13:01:11 +0930

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -80,6 +80,7 @@ return [
         // 	'database' => [
         // 		'model' => App\User::class,
         // 		'sync_passwords' => false,
+        //      'locate_users_by' => 'mail',
         // 		'sync_attributes' => [
         // 			'name' => 'cn',
         // 			'email' => 'mail',
