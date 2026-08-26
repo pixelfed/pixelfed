30db57448f96c764d1a0cacb776b4ce003ee62d8
Your Name <you@example.com>
2026-08-26 22:45:16 +0930

    fix: replace str_random/str_limit/str_slug in Blade templates and tests
    These deprecated helpers will throw 'undefined function' errors at
    runtime since laravel/helpers was removed. Replace with Str::random(),
    Str::limit(), and Str::slug() respectively.

--- resources/views/admin/curated-register/index.blade.php
+++ resources/views/admin/curated-register/index.blade.php
@@ -81,11 +81,11 @@
                         </td>
                         @endif
                         <td class="align-middle">
-                            {{ str_limit($record->reason_to_join, 100) }}
+                            {{ Str::limit($record->reason_to_join, 100) }}
                         </td>
                         <td class="align-middle">
                             <p class="mb-0">
-                                {{ str_limit(\Illuminate\Support\Str::mask($record->email, '*', 5, 10), 10) }}
+                                {{ Str::limit(\Illuminate\Support\Str::mask($record->email, '*', 5, 10), 10) }}
                             </p>
                         </td>
                         <td class="align-middle">{{ $record->created_at->diffForHumans() }}</td>
--- resources/views/settings/parental-controls/checkbox.blade.php
+++ resources/views/settings/parental-controls/checkbox.blade.php
@@ -1,5 +1,5 @@
 @php
-$id = str_random(6) . '_' . str_slug($name);
+$id = Str::random(6) . '_' . Str::slug($name);
 $defaultChecked = isset($checked) && $checked ? 'checked=""' : '';
 @endphp<div class="custom-control custom-checkbox">
                 <input type="checkbox" class="custom-control-input" id="{{$id}}" name="{{$name}}" {!!$defaultChecked!!}>
