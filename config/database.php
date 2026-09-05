690d710693a1d9f05313c1d9fe1ad845fc3ccaac
Shlee <github@shl.ee>
2026-09-05 20:28:34 +0930

    Update database.php

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
