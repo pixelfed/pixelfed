b852554152a732d42620eca351278aada6961637
Shlee <github@shl.ee>
2026-09-05 22:54:11 +0930

    Update session.php

--- config/session.php
+++ config/session.php
@@ -148,7 +148,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', env('APP_DOMAIN', null)),
+    'domain' => env('SESSION_DOMAIN'),
 
     /*
     |--------------------------------------------------------------------------
