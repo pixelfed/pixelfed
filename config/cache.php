5383c2ecc9018389d61f0a274e8b71ff2b5f73c5
Shlee <github@shl.ee>
2026-09-05 20:26:13 +0930

    Update cache.php

--- config/cache.php
+++ config/cache.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
+    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel'), '_').'_cache'),
 
     /*
     |--------------------------------------------------------------------------
