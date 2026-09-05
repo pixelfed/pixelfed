1d712e14782eb08efa3be0c343d3544a46e7b37c
Shlee <github@shl.ee>
2026-09-05 20:32:02 +0930

    Update default mailer configuration to use MAIL_DRIVER

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', env('MAIL_DRIVER', 'smtp')),
 
     /*
     |--------------------------------------------------------------------------
