d7cfd072049811dd63f2d1d4f09593e3adca000f
Daniel Supernault <danielsupernault@gmail.com>
2026-09-07 00:00:06 -0600

    Fix PostEditModal. Closes #7084

--- app/Http/Controllers/StatusEditController.php
+++ app/Http/Controllers/StatusEditController.php
@@ -15,7 +15,7 @@ class StatusEditController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:sanctum,api');
     }
 
     public function store(StoreStatusEditRequest $request, $id)
--- config/cors.php
+++ config/cors.php
@@ -26,6 +26,8 @@ return [
         'api/*',
         'oauth/*',
         'sanctum/csrf-cookie',
+        'login',
+        'logout',
     ],
 
     /*
@@ -36,7 +38,7 @@ return [
     /*
      * Matches the request origin. `[*]` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => ['*'],
+    'allowed_origins' =>  env('PF_CORS_ALLOWED_ORIGINS') ? explode(',', env('PF_CORS_ALLOWED_ORIGINS', '')) : [],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
