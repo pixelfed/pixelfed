13aa36efb4533f9a77dc79f0928f0cbb226a7aa7
Daniel Supernault <danielsupernault@gmail.com>
2026-09-10 02:09:23 -0600

    Update ApiV1Controller.php

--- app/Http/Controllers/Api/ApiV1Controller.php
+++ app/Http/Controllers/Api/ApiV1Controller.php
@@ -2770,7 +2770,7 @@ class ApiV1Controller extends Controller
                 })
                 ->values();
 
-            $baseUrl = config('app.url').'/api/v1/timelines/home?limit='.$limit.'&';
+            $baseUrl = $napi ? config('app.url').'/api/v1/timelines/home?_pe=1limit='.$limit.'&' : config('app.url').'/api/v1/timelines/home?limit='.$limit.'&';
             $minId = $res->map(function ($s) {
                 return ['id' => $s['id']];
             })->min('id');
