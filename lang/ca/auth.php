157c33d3baae8470445209cd2928fbbe6311c268
Shlee <github@shl.ee>
2026-09-14 20:41:33 +0930

    New translations auth.php (Catalan)
    [ci skip]
    [ci skip]

--- lang/ca/auth.php
+++ lang/ca/auth.php
@@ -1,16 +1,35 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
+
+    'verifyYourEmailAddress' => '',
+    'loginTitle' => '',
     'failed' => 'Aquestes dades d\'accés no coincideixen amb els nostres registres.',
     'throttle' => 'Massa intents d\'iniciar sessió. Si us plau, torneu-ho a provar d\'aquí a :seconds segons.',
+    'password' => '',
+    'remember' => '',
+    'forgot' => '',
+    'login' => '',
+
+    'register' => '',
+    'reset' => '',
+
+    'name' => '',
+    'username' => '',
+    'confirm-password' => '',
+
+    'age' => '',
+    'terms' => ''.route('site.terms').''.route('site.privacy').'',
+
+    'emailAddress' => '',
+    'email' => '',
+    'forgotEmail' => '',
+
+    'registerTitle' => '',
+
+    'sendReset' => '',
+    'backLogin' => '',
+
+    'signInMastodon' => '',
+
 ];
