43c2c44b30055e68bbb8559558c99f655884395d
Shlee <github@shl.ee>
2026-09-14 20:09:35 +0930

    New translations profile.php (Thai)
    [ci skip]
    [ci skip]

--- lang/th/profile.php
+++ lang/th/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'บัญชีไม่สามารถใช้งานได้',
     'status.disabled.body' => 'เสียใจด้วยนะ บัญชีนี้ไม่สามารถใช้งานได้ โปรดลองใหม่อีกครั้ง',
+
+    'block.domain.max' => '',
+
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
