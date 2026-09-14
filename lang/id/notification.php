663465d16f53f9a89f833dbc5f40cd50d35e6864
Shlee <github@shl.ee>
2026-09-14 19:44:46 +0930

    New translations notification.php (Indonesian)
    [ci skip]
    [ci skip]

--- lang/id/notification.php
+++ lang/id/notification.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
+
     'likedPhoto' => 'menyukai foto kamu.',
+    'likedComment' => 'liked your comment.',
     'startedFollowingYou' => 'mulai mengikuti kamu.',
     'commented' => 'mengomentari posting kamu.',
     'mentionedYou' => 'menyebut nama kamu.',
+    'shared' => 'shared your post.',
+
 ];
