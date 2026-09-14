3a7e5d461bef24cccf29bf9582b83d7e6871f6b1
Shlee <github@shl.ee>
2026-09-14 20:42:06 +0930

    New translations auth.php (Chinese Simplified)
    [ci skip]
    [ci skip]

--- lang/zh-cn/auth.php
+++ lang/zh-cn/auth.php
@@ -2,18 +2,34 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
+    'verifyYourEmailAddress' => '',
+    'loginTitle' => '',
     'failed' => '这些凭证与我们的记录不符。',
     'throttle' => '您尝试得错误次数过多，请在 :seconds 秒后重试。',
+    'password' => '',
+    'remember' => '',
+    'forgot' => '',
+    'login' => '',
+
+    'register' => '',
+    'reset' => '',
+
+    'name' => '',
+    'username' => '',
+    'confirm-password' => '',
+
+    'age' => '',
+    'terms' => ''.route('site.terms').''.route('site.privacy').'',
+
+    'emailAddress' => '',
+    'email' => '',
+    'forgotEmail' => '',
+
+    'registerTitle' => '',
+
+    'sendReset' => '',
+    'backLogin' => '',
+
+    'signInMastodon' => '',
 
 ];
