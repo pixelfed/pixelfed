accf292e5b3903b00d112ca66bbe8adc9b754f11
Shlee <github@shl.ee>
2026-09-14 19:43:39 +0930

    New translations navmenu.php (Korean)
    [ci skip]
    [ci skip]

---
+++ lang/ko/navmenu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'search' => 'Search',
+    'home' => 'Home',
+    'local' => 'Local',
+    'network' => 'Network',
+    'discover' => 'Discover',
+    'viewMyProfile' => 'View my profile',
+    'myProfile' => 'My Profile',
+    'myTimeline' => 'My Timeline',
+    'publicTimeline' => 'Public Timeline',
+    'remoteFollow' => 'Remote Follow',
+    'settings' => 'Settings',
+    'admin' => 'Admin',
+    'logout' => 'Logout',
+    'directMessages' => 'Direct Messages',
+    'composePost' => 'Compose Post',
+];
