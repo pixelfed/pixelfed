150f236bd6482ddbc4db89597220e98ec805825d
Shlee <github@shl.ee>
2026-09-15 08:52:43 +0930

    New translations exception.php (Pirate English)
    [ci skip]
    [ci skip]

--- lang/en/exception.php
+++ lang/en/exception.php
@@ -4,7 +4,7 @@ return [
 
     'compose' => [
         'invalid' => [
-            'album' => 'Must contain a single photo or video or multiple photos.',
+            'album' => '',
         ],
     ],
 
