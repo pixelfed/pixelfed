7799e1971b194ef2e92f1bfef8b647216342c811
Shlee <github@shl.ee>
2026-09-14 20:46:31 +0930

    New translations profile.php (Danish)
    [ci skip]
    [ci skip]

--- lang/da/profile.php
+++ lang/da/profile.php
@@ -2,4 +2,20 @@
 
 return [
     'emptyTimeline' => 'Denne bruger har ikke postet noget endnu!',
+    'emptyFollowers' => '',
+    'emptyFollowing' => '',
+    'emptySaved' => '',
+    'savedWarning' => '',
+    'privateProfileWarning' => '',
+    'alreadyFollow' => '',
+    'loginToSeeProfile' => '',
+
+    'status.disabled.header' => '',
+    'status.disabled.body' => '',
+
+    'block.domain.max' => '',
+
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
