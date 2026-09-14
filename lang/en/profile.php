b7c5493e2c92089bf59899406bd411cf1e7d5349
Shlee <github@shl.ee>
2026-09-15 08:52:49 +0930

    New translations profile.php (Pirate English)
    [ci skip]
    [ci skip]

--- lang/en/profile.php
+++ lang/en/profile.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-    'emptyTimeline' => 'This user has no posts yet!',
-    'emptyFollowers' => 'This user has no followers yet!',
-    'emptyFollowing' => 'This user is not following anyone yet!',
-    'emptySaved' => 'You haven’t saved any post yet!',
-    'savedWarning' => 'Only you can see what you’ve saved',
-    'privateProfileWarning' => 'This Account is Private',
-    'alreadyFollow' => 'Already follow :username?',
-    'loginToSeeProfile' => 'to see their photos and videos.',
+    'emptyTimeline' => '',
+    'emptyFollowers' => '',
+    'emptyFollowing' => '',
+    'emptySaved' => '',
+    'savedWarning' => '',
+    'privateProfileWarning' => '',
+    'alreadyFollow' => '',
+    'loginToSeeProfile' => '',
 
-    'status.disabled.header' => 'Profile Unavailable',
-    'status.disabled.body' => 'Sorry, this profile is not available at the moment. Please try again shortly.',
+    'status.disabled.header' => '',
+    'status.disabled.body' => '',
 
-    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+    'block.domain.max' => '',
 
-    'mutedAccounts' => 'Muted Accounts',
-    'blockedAccounts' => 'Blocked Accounts',
-    'blockedDomains' => 'Blocked Domains',
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
