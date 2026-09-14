cecbd676e98dce4ffdc0dd641908d341d64660ac
Shlee <github@shl.ee>
2026-09-14 19:45:55 +0930

    New translations profile.php (Finnish)
    [ci skip]
    [ci skip]

--- lang/fi/profile.php
+++ lang/fi/profile.php
@@ -4,5 +4,18 @@ return [
     'emptyTimeline' => 'Tällä käyttäjällä ei ole vielä päivityksiä!',
     'emptyFollowers' => 'Tällä käyttäjällä ei ole vielä seuraajia!',
     'emptyFollowing' => 'Tämä käyttäjä ei vielä seuraa ketään!',
+    'emptySaved' => 'You haven’t saved any post yet!',
     'savedWarning' => 'Vain sinä voit nähdä, mitä olet tallentanut',
+    'privateProfileWarning' => 'This Account is Private',
+    'alreadyFollow' => 'Already follow :username?',
+    'loginToSeeProfile' => 'to see their photos and videos.',
+
+    'status.disabled.header' => 'Profile Unavailable',
+    'status.disabled.body' => 'Sorry, this profile is not available at the moment. Please try again shortly.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
