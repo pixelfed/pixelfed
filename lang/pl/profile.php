2a1628ba8618a5c281b5561ddda8b47f38126476
Shlee <github@shl.ee>
2026-09-14 19:46:07 +0930

    New translations profile.php (Polish)
    [ci skip]
    [ci skip]

--- lang/pl/profile.php
+++ lang/pl/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'Profil jest niedostępny',
     'status.disabled.body' => 'Przepraszamy, ten profil nie jest obecnie dostępny. Spróbuj ponownie za jakiś czas.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
