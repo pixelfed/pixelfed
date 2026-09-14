9a3596f53219b71af28611bd618c0d02bdad348f
Shlee <github@shl.ee>
2026-09-14 20:08:54 +0930

    New translations profile.php (German)
    [ci skip]
    [ci skip]

--- lang/de/profile.php
+++ lang/de/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'Profil nicht verfügbar',
     'status.disabled.body' => 'Entschuldigung, dieses Profil ist im Moment nicht verfügbar. Bitte versuche es in Kürze noch einmal.',
+
+    'block.domain.max' => '',
+
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
