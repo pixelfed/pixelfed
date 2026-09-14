de0b2725e4de2d5e4d0b5b85752839d0c8dfb5a8
Shlee <github@shl.ee>
2026-09-14 20:08:20 +0930

    New translations passwords.php (Polish)
    [ci skip]
    [ci skip]

--- lang/pl/passwords.php
+++ lang/pl/passwords.php
@@ -16,7 +16,7 @@ return [
     'password' => 'Hasło musi składać się z przynajmniej 6 znaków i być równe powtórzonemu.',
     'reset' => 'Twoje hasło zostało zresetowane!',
     'sent' => 'Jeżeli ten adres e-mail jest obecny w naszej bazie danych, otrzymasz wiadomość z odnośnikiem resetowania hasła w ciągu kilku minut. Jeżeli nie dostałeś(-aś) tego maila, sprawdź zawartość folderu Spam.',
-    'token' => 'This password reset token is invalid.',
+    'token' => '',
     'user' => 'Jeżeli ten adres e-mail jest obecny w naszej bazie danych, otrzymasz wiadomość z odnośnikiem resetowania hasła w ciągu kilku minut. Jeżeli nie dostałeś(-aś) tego maila, sprawdź zawartość folderu Spam.',
 
 ];
