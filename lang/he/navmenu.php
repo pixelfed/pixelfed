159dd3b4a424d9ab209e50f4571270fe8b0d92c6
Shlee <github@shl.ee>
2026-09-14 19:43:35 +0930

    New translations navmenu.php (Hebrew)
    [ci skip]
    [ci skip]

--- lang/he/navmenu.php
+++ lang/he/navmenu.php
@@ -1,8 +1,13 @@
 <?php
 
 return [
-
+    'search' => 'Search',
+    'home' => 'Home',
+    'local' => 'Local',
+    'network' => 'Network',
+    'discover' => 'Discover',
     'viewMyProfile' => 'צפה בפרופיל שלי',
+    'myProfile' => 'My Profile',
     'myTimeline' => 'ציר הזמן שלי',
     'publicTimeline' => 'ציר הזמן הציבורי',
     'remoteFollow' => 'עקיבה מרחוק',
@@ -10,5 +15,5 @@ return [
     'admin' => 'מנהל',
     'logout' => 'התנתק',
     'directMessages' => 'הודעות ישירות',
-
+    'composePost' => 'Compose Post',
 ];
