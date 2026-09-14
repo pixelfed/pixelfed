755606d7aa5e08d3e5161754ae0b339f2ea1af6c
Shlee <github@shl.ee>
2026-09-15 08:53:04 +0930

    New translations profile.php (Esperanto)
    [ci skip]
    [ci skip]

--- lang/eo/profile.php
+++ lang/eo/profile.php
@@ -1,15 +1,21 @@
 <?php
 
 return [
-    'emptyTimeline' => 'Ĉi tiu uzanto ankoraŭ ne havas afiŝojn!',
-    'emptyFollowers' => 'Ĉi tiu uzanto ankoraŭ ne havas sekvantojn!',
-    'emptyFollowing' => 'Ĉi tiu uzanto ankoraŭ ne sekvas iu!',
-    'emptySaved' => 'Vi ankoraŭ ne konservis neniujn afiŝojn!',
-    'savedWarning' => 'Sola vi permisas vidi kion vi konservis',
-    'privateProfileWarning' => 'Ĉi tiu konto estas privata',
-    'alreadyFollow' => 'Vi jam sekvas :username?',
-    'loginToSeeProfile' => 'vidi lian fotojn kaj videojn.',
+    'emptyTimeline' => '',
+    'emptyFollowers' => '',
+    'emptyFollowing' => '',
+    'emptySaved' => '',
+    'savedWarning' => '',
+    'privateProfileWarning' => '',
+    'alreadyFollow' => '',
+    'loginToSeeProfile' => '',
 
-    'status.disabled.header' => 'Profilo Maldisponebla',
-    'status.disabled.body' => 'Mi bedaŭras, ĉi tiu profilo momente estas maldisponebla. Bonvolu, momente provu denove.',
+    'status.disabled.header' => '',
+    'status.disabled.body' => '',
+
+    'block.domain.max' => '',
+
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
