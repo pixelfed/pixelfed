0eac402500bf89c02c080143863129f0189d8fb4
Shlee <github@shl.ee>
2026-09-14 19:43:33 +0930

    New translations navmenu.php (Basque)
    [ci skip]
    [ci skip]

--- lang/eu/navmenu.php
+++ lang/eu/navmenu.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-
     'search' => 'Bilatu',
     'home' => 'Sarrera',
     'local' => 'Lokala',
+    'network' => 'Network',
     'discover' => 'Esploratu',
     'viewMyProfile' => 'Ikusi nire profila',
+    'myProfile' => 'My Profile',
     'myTimeline' => 'Nire denboralerroa',
     'publicTimeline' => 'Denboralerro publikoa',
     'remoteFollow' => 'Urruneko Jarraipena',
@@ -14,5 +15,5 @@ return [
     'admin' => 'Admin',
     'logout' => 'Irten',
     'directMessages' => 'Mezu zuzenak',
-
+    'composePost' => 'Compose Post',
 ];
