51adb3c2dbcf5f6b7473f17ec5606e0c742b084b
Shlee <github@shl.ee>
2026-09-14 20:09:12 +0930

    New translations profile.php (Portuguese)
    [ci skip]
    [ci skip]

--- lang/pt/profile.php
+++ lang/pt/profile.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-    'emptyTimeline' => 'Este utilizador não fez nenhuma publicação!',
-    'emptyFollowers' => 'Este utilizador não tem seguidores!',
-    'emptyFollowing' => 'Este utilizador ainda não segue ninguém!',
-    'emptySaved' => 'Ainda não salvaste nenhuma publicação!',
-    'savedWarning' => 'Apenas tu podes ver o que foi salvo',
-    'privateProfileWarning' => 'Esta conta é privada',
-    'alreadyFollow' => 'Já segues :username?',
-    'loginToSeeProfile' => 'para veres as suas fotos e vídeos.',
+    'emptyTimeline' => '',
+    'emptyFollowers' => '',
+    'emptyFollowing' => '',
+    'emptySaved' => '',
+    'savedWarning' => '',
+    'privateProfileWarning' => '',
+    'alreadyFollow' => '',
+    'loginToSeeProfile' => '',
 
-    'status.disabled.header' => 'Perfil Indisponível',
-    'status.disabled.body' => 'Desculpe, este perfil não está disponível neste momento. Por favor, tente novamente mais tarde.',
+    'status.disabled.header' => '',
+    'status.disabled.body' => '',
 
-    'block.domain.max' => 'Limite máximo de bloqueio de domínios atingido! Você só pode bloquear :max domínios por vez. Peça ao seu administrador para ajustar este limite.',
+    'block.domain.max' => '',
 
-    'mutedAccounts' => 'Contas Silenciadas',
-    'blockedAccounts' => 'Contas Bloqueadas',
-    'blockedDomains' => 'Domínios Bloqueados',
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
