9f91c45eb348c320a2dad4c8a153de7162b2202e
Shlee <github@shl.ee>
2026-09-14 19:44:24 +0930

    New translations notification.php (Finnish)
    [ci skip]
    [ci skip]

--- lang/fi/notification.php
+++ lang/fi/notification.php
@@ -3,6 +3,10 @@
 return [
 
     'likedPhoto' => 'tykkäsi kuvastasi.',
+    'likedComment' => 'liked your comment.',
     'startedFollowingYou' => 'alkoi seuraamaan sinua.',
+    'commented' => 'commented on your post.',
+    'mentionedYou' => 'mentioned you.',
+    'shared' => 'shared your post.',
 
 ];
