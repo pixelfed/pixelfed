451039cbf972ad9ef3f6be1b9f62ea142663afe2
Shlee <github@shl.ee>
2026-09-14 19:45:47 +0930

    New translations profile.php (Catalan)
    [ci skip]
    [ci skip]

--- lang/ca/profile.php
+++ lang/ca/profile.php
@@ -9,6 +9,13 @@ return [
     'privateProfileWarning' => 'Aquest compte és privat',
     'alreadyFollow' => 'Ja seguiu a :username?',
     'loginToSeeProfile' => 'per veure les seves fotos i vídeos.',
+
     'status.disabled.header' => 'Perfil no disponible',
     'status.disabled.body' => 'Perdoneu, aquest perfil no està disponible en aquest moment. Proveu d\'accedir-hi d\'aquí a una estona.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
