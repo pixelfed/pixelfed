997906edc31aa5c939662886fb154168ace32594
Shlee <github@shl.ee>
2026-09-14 19:45:57 +0930

    New translations profile.php (Hebrew)
    [ci skip]
    [ci skip]

--- lang/he/profile.php
+++ lang/he/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'פרופיל לא זמין',
     'status.disabled.body' => 'מצטערים, פרופיל זה אינו זמין כרגע. אנא נסו שוב בקרוב.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
