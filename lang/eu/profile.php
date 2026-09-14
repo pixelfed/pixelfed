ecab80639176cabe8e88b0423674cf076041be69
Shlee <github@shl.ee>
2026-09-14 19:45:54 +0930

    New translations profile.php (Basque)
    [ci skip]
    [ci skip]

--- lang/eu/profile.php
+++ lang/eu/profile.php
@@ -4,5 +4,18 @@ return [
     'emptyTimeline' => 'Erabiltzaile honek oraindik ez du ezer bidali!',
     'emptyFollowers' => 'Erabiltzaile honek ez du jarraitzailerik!',
     'emptyFollowing' => 'Erabiltzaile honek oraindik ez du inor jarraitzen',
+    'emptySaved' => 'You haven’t saved any post yet!',
     'savedWarning' => 'Gordetakoa zuk bakarrik ikusi dezakezu',
+    'privateProfileWarning' => 'This Account is Private',
+    'alreadyFollow' => 'Already follow :username?',
+    'loginToSeeProfile' => 'to see their photos and videos.',
+
+    'status.disabled.header' => 'Profile Unavailable',
+    'status.disabled.body' => 'Sorry, this profile is not available at the moment. Please try again shortly.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
