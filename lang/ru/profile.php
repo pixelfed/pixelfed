c195c832f77009a94129a0112e3eb29f418d82a9
Shlee <github@shl.ee>
2026-09-14 19:46:10 +0930

    New translations profile.php (Russian)
    [ci skip]
    [ci skip]

--- lang/ru/profile.php
+++ lang/ru/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'Учётная запись недоступна',
     'status.disabled.body' => 'К сожалению, эта учётная запись недоступна на данный момент. Пожалуйста, попробуйте ещё раз позднее.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
