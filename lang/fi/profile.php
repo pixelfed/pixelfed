33572e87158a1295c4c626a8af7526cee2175f72
Shlee <github@shl.ee>
2026-09-14 20:46:37 +0930

    New translations profile.php (Finnish)
    [ci skip]
    [ci skip]

--- lang/fi/profile.php
+++ lang/fi/profile.php
@@ -4,5 +4,18 @@ return [
     'emptyTimeline' => 'Tällä käyttäjällä ei ole vielä päivityksiä!',
     'emptyFollowers' => 'Tällä käyttäjällä ei ole vielä seuraajia!',
     'emptyFollowing' => 'Tämä käyttäjä ei vielä seuraa ketään!',
+    'emptySaved' => '',
     'savedWarning' => 'Vain sinä voit nähdä, mitä olet tallentanut',
+    'privateProfileWarning' => '',
+    'alreadyFollow' => '',
+    'loginToSeeProfile' => '',
+
+    'status.disabled.header' => '',
+    'status.disabled.body' => '',
+
+    'block.domain.max' => '',
+
+    'mutedAccounts' => '',
+    'blockedAccounts' => '',
+    'blockedDomains' => '',
 ];
