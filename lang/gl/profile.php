f05954b8c82a1d5eb9aa958405bd4ad34e09c58f
Shlee <github@shl.ee>
2026-09-14 19:46:22 +0930

    New translations profile.php (Galician)
    [ci skip]
    [ci skip]

--- lang/gl/profile.php
+++ lang/gl/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'Perfil non dispoñible',
     'status.disabled.body' => 'O perfil non está dipoñible neste intre, inténtao dentro dun anaco.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
