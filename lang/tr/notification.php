80e6349eb88a438e0af2f7308a367ec206cbb1af
Shlee <github@shl.ee>
2026-09-14 19:44:39 +0930

    New translations notification.php (Turkish)
    [ci skip]
    [ci skip]

--- lang/tr/notification.php
+++ lang/tr/notification.php
@@ -3,6 +3,7 @@
 return [
 
     'likedPhoto' => 'gönderini beğendi.',
+    'likedComment' => 'liked your comment.',
     'startedFollowingYou' => 'seni takip etmeye başladı.',
     'commented' => 'gönderine yorum yaptı.',
     'mentionedYou' => 'seni etiketledi.',
