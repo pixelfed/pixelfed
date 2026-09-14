b155a7aa05ce6571d11293092a8960aa4bf35787
Shlee <github@shl.ee>
2026-09-14 19:44:25 +0930

    New translations notification.php (Hebrew)
    [ci skip]
    [ci skip]

--- lang/he/notification.php
+++ lang/he/notification.php
@@ -3,8 +3,10 @@
 return [
 
     'likedPhoto' => 'אהבו את התמונה שלך.',
+    'likedComment' => 'liked your comment.',
     'startedFollowingYou' => 'התחיל לעקוב אחריך.',
     'commented' => 'הגיב על הפוסט שלך.',
     'mentionedYou' => 'הזכיר אותך.',
+    'shared' => 'shared your post.',
 
 ];
