455d37aa39787c7d6bd2878d92155b82b26ddfd2
Shlee <github@shl.ee>
2026-09-14 19:40:51 +0930

    New translations auth.php (French)
    [ci skip]
    [ci skip]

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -1,21 +1,11 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-     */
 
-    'failed' => 'Ces informations d’identification ne correspondent à aucune se trouvant dans notre base de données.',
-    'throttle' => 'Trop de tentatives de connexion ont été effectuées. Veuillez réessayer dans :seconds secondes.',
     'verifyYourEmailAddress' => ' - Vérifiez votre adresse de courrier électronique',
     'loginTitle' => 'Connexion à mon compte',
+    'failed' => 'Ces informations d’identification ne correspondent à aucune se trouvant dans notre base de données.',
+    'throttle' => 'Trop de tentatives de connexion ont été effectuées. Veuillez réessayer dans :seconds secondes.',
     'password' => 'Mot de passe',
     'remember' => 'Se souvenir de moi',
     'forgot' => 'Mot de passe oublié',
@@ -41,4 +31,5 @@ return [
     'backLogin' => 'Retour à l’identification',
 
     'signInMastodon' => 'S’inscrire avec Mastodon',
+
 ];
