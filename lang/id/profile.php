e00e3f21675411285b6a8c84ed42439fe5315850
Shlee <github@shl.ee>
2026-09-14 19:46:24 +0930

    New translations profile.php (Indonesian)
    [ci skip]
    [ci skip]

--- lang/id/profile.php
+++ lang/id/profile.php
@@ -9,6 +9,13 @@ return [
     'privateProfileWarning' => 'Akun ini Privat',
     'alreadyFollow' => 'Sudah mengikuti :username?',
     'loginToSeeProfile' => 'untuk melihat foro dan videonya.',
+
     'status.disabled.header' => 'Profil Tidak Tersedia',
     'status.disabled.body' => 'Maaf, profil ini sedang tidak tersedia. Silakan coba lagi kemudian.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
