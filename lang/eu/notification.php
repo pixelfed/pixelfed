522461985ae0063849305b0b7ba5f11815111b73
Shlee <github@shl.ee>
2026-09-14 19:44:23 +0930

    New translations notification.php (Basque)
    [ci skip]
    [ci skip]

--- lang/eu/notification.php
+++ lang/eu/notification.php
@@ -3,6 +3,10 @@
 return [
 
     'likedPhoto' => '-(e)k argazkia gustuko du.',
+    'likedComment' => 'liked your comment.',
     'startedFollowingYou' => 'jarraitzen hasi zaizu.',
+    'commented' => 'commented on your post.',
+    'mentionedYou' => 'mentioned you.',
+    'shared' => 'shared your post.',
 
 ];
