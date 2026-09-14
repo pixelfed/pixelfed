0112aecb6615a9def8e5df87532261d569aaceaa
Shlee <github@shl.ee>
2026-09-14 20:03:51 +0930

    New translations auth.php (Greek)
    [ci skip]
    [ci skip]

--- lang/el/auth.php
+++ lang/el/auth.php
@@ -2,18 +2,34 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
+    'verifyYourEmailAddress' => '',
+    'loginTitle' => '',
     'failed' => 'Αυτά τα στοιχεία δεν υπάρχουν στα κατάστιχά μας.',
     'throttle' => 'Λόγω πολλαπλών δοκιμών, παρακαλώ δοκιμάστε ξανά σε :seconds δευτερόλεπτα.',
+    'password' => '',
+    'remember' => '',
+    'forgot' => '',
+    'login' => '',
+
+    'register' => '',
+    'reset' => '',
+
+    'name' => '',
+    'username' => '',
+    'confirm-password' => '',
+
+    'age' => '',
+    'terms' => ''.route('site.terms').''.route('site.privacy').'',
+
+    'emailAddress' => '',
+    'email' => '',
+    'forgotEmail' => '',
+
+    'registerTitle' => '',
+
+    'sendReset' => '',
+    'backLogin' => '',
+
+    'signInMastodon' => '',
 
 ];
