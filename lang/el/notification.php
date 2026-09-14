787c7774b0eb0b5ada4790a353143b9110bd0ae4
Shlee <github@shl.ee>
2026-09-14 19:44:21 +0930

    New translations notification.php (Greek)
    [ci skip]
    [ci skip]

--- lang/el/notification.php
+++ lang/el/notification.php
@@ -2,12 +2,11 @@
 
 return [
 
-    'likedPhoto' => 'έδειξε προτίμηση στη φωτογραφία σου.', // 'preferred your photo'
-    // 'likedPhoto' => 'αγάπησε τη φωτογραφία σου.', //'loved your photo' (this is too much, we don't use 'love' like that)
-    // 'likedPhoto' => 'έκανε like στη φωτογραφία σου.', //'did a like on your photo' (this sounds cheesy)
-
-    // In greek "like" is only passive (i.e. you can say "Your photo was liked by user" but not "user liked your photo").
-    // So we either can use "loved", use the english word ("performed a 'like' on your photo"), or (the one I chose) say
-    // "user preferred your photo"
+    'likedPhoto' => 'έδειξε προτίμηση στη φωτογραφία σου.',
+    'likedComment' => 'liked your comment.',
+    'startedFollowingYou' => 'started following you.',
+    'commented' => 'commented on your post.',
+    'mentionedYou' => 'mentioned you.',
+    'shared' => 'shared your post.',
 
 ];
