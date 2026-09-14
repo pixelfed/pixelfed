7cc4741fe2665b01bbee5cc9bc899cfc14bb3cb6
Shlee <github@shl.ee>
2026-09-14 20:43:00 +0930

    New translations exception.php (Portuguese, Brazilian)
    [ci skip]
    [ci skip]

--- lang/pt/exception.php
+++ lang/pt/exception.php
@@ -4,7 +4,7 @@ return [
 
     'compose' => [
         'invalid' => [
-            'album' => '',
+            'album' => 'Deve conter uma única fotografia ou vídeo, ou várias fotografias.',
         ],
     ],
 
