3265fb76598386e10dded2722a886abec470240c
Shlee <github@shl.ee>
2026-09-14 19:41:19 +0930

    New translations auth.php (Russian)
    [ci skip]
    [ci skip]

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -2,18 +2,34 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
+    'verifyYourEmailAddress' => ' - Verify Your Email Address',
+    'loginTitle' => 'Account Login',
     'failed' => 'Учётные данные не совпадают с нашими записями.',
     'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте снова через :seconds сек.',
+    'password' => 'Password',
+    'remember' => 'Remember Me',
+    'forgot' => 'Forgot Password',
+    'login' => 'Login',
+
+    'register' => 'Register',
+    'reset' => 'Password Reset',
+
+    'name' => 'Name',
+    'username' => 'Username',
+    'confirm-password' => 'Confirm Password',
+
+    'age' => 'I am at least 16 years old',
+    'terms' => 'By signing up, you agree to our <a href="'.route('site.terms').'" class="font-weight-bold text-dark">Terms of Use</a> and <a href="'.route('site.privacy').'" class="font-weight-bold text-dark">Privacy Policy</a>.',
+
+    'emailAddress' => 'E-Mail Address',
+    'email' => 'E-Mail',
+    'forgotEmail' => 'Forgot E-Mail',
+
+    'registerTitle' => 'Register a new account',
+
+    'sendReset' => 'Send Password Reset Link',
+    'backLogin' => 'Back to Login',
+
+    'signInMastodon' => 'Sign-in with Mastodon',
 
 ];
