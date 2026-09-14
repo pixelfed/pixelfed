7440ea964f84cacd8b8c8a81767d3e0db7435974
Shlee <github@shl.ee>
2026-09-14 19:46:28 +0930

    New translations profile.php (Thai)
    [ci skip]
    [ci skip]

--- lang/th/profile.php
+++ lang/th/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'บัญชีไม่สามารถใช้งานได้',
     'status.disabled.body' => 'เสียใจด้วยนะ บัญชีนี้ไม่สามารถใช้งานได้ โปรดลองใหม่อีกครั้ง',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
