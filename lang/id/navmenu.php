b0b3017e09151fab5e0f9bcf2f68d34ed2720300
Shlee <github@shl.ee>
2026-09-14 19:43:58 +0930

    New translations navmenu.php (Indonesian)
    [ci skip]
    [ci skip]

--- lang/id/navmenu.php
+++ lang/id/navmenu.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
+    'search' => 'Search',
+    'home' => 'Home',
+    'local' => 'Local',
+    'network' => 'Network',
+    'discover' => 'Discover',
     'viewMyProfile' => 'Lihat profil saya',
+    'myProfile' => 'My Profile',
     'myTimeline' => 'Linimasa Saya',
     'publicTimeline' => 'Linimasa Publik',
     'remoteFollow' => 'Ikuti Jarak Jauh',
@@ -9,4 +15,5 @@ return [
     'admin' => 'Admin',
     'logout' => 'Keluar',
     'directMessages' => 'Pesan Langsung',
+    'composePost' => 'Compose Post',
 ];
