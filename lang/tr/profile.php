6167a7242879ae38d69c60ff09a7e6ab65a101f7
Shlee <github@shl.ee>
2026-09-14 19:46:15 +0930

    New translations profile.php (Turkish)
    [ci skip]
    [ci skip]

--- lang/tr/profile.php
+++ lang/tr/profile.php
@@ -12,4 +12,10 @@ return [
 
     'status.disabled.header' => 'Profil kullanım dışı',
     'status.disabled.body' => 'Üzgünüz, bu profile şu anda ulaşılamıyor. Kısa süre içinde tekrar deneyiniz.',
+
+    'block.domain.max' => 'Max limit of domain blocks reached! You can only block :max domains at a time. Ask your admin to adjust this limit.',
+
+    'mutedAccounts' => 'Muted Accounts',
+    'blockedAccounts' => 'Blocked Accounts',
+    'blockedDomains' => 'Blocked Domains',
 ];
