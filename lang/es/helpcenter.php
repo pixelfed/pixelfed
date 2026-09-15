995ea1f447a25627092f5d0be402b0a96cefa7e9
Shlee <github@shl.ee>
2026-09-15 09:34:23 +0930

    New translations helpcenter.php (Spanish (Modern))
    [ci skip]
    [ci skip]

--- lang/es/helpcenter.php
+++ lang/es/helpcenter.php
@@ -2,27 +2,27 @@
 
 return [
 
-    'helpcenter' => 'Centro de Ayuda',
-    'whatsnew' => 'Novedades',
+    'helpcenter' => '',
+    'whatsnew' => '',
 
-    'gettingStarted' => 'Primeros Pasos',
-    'sharingMedia' => 'Compartiendo Medios',
-    'profile' => 'Perfil',
-    'stories' => 'Historias',
-    'hashtags' => 'Hashtags',
-    'discover' => 'Descubre',
-    'directMessages' => 'Mensajes Directos',
-    'timelines' => 'Timelines',
-    'embed' => 'Incrustar',
+    'gettingStarted' => '',
+    'sharingMedia' => '',
+    'profile' => '',
+    'stories' => '',
+    'hashtags' => '',
+    'discover' => '',
+    'directMessages' => '',
+    'timelines' => '',
+    'embed' => '',
 
-    'communityGuidelines' => 'Normas de la comunidad',
-    'whatIsTheFediverse' => '¿Qué es el fediverso?',
-    'controllingVisibility' => 'Controlando la Visibilidad',
-    'blockingAccounts' => 'Bloqueando Cuentas',
-    'safetyTips' => 'Tips de Seguridad',
-    'reportSomething' => 'Reportar Algo',
-    'dataPolicy' => 'Política sobre datos',
+    'communityGuidelines' => '',
+    'whatIsTheFediverse' => '',
+    'controllingVisibility' => '',
+    'blockingAccounts' => '',
+    'safetyTips' => '',
+    'reportSomething' => '',
+    'dataPolicy' => '',
 
-    'taggingPeople' => 'Etiquetando Personas',
+    'taggingPeople' => '',
 
 ];
