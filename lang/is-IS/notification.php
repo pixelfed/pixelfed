0f5a0e24f570fec4bcf2a8c14784774bc22fb686
Shlee <github@shl.ee>
2026-09-16 19:46:36 +0930

    New translations notification.php (Icelandic)
    [ci skip]

--- lang/is-IS/notification.php
+++ lang/is-IS/notification.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'likedPhoto' => '',
-    'likedComment' => '',
-    'startedFollowingYou' => '',
-    'commented' => '',
-    'mentionedYou' => '',
-    'shared' => '',
+    'likedPhoto' => 'líkaði við færsluna þína.',
+    'likedComment' => 'líkaði við athugasemdina þína.',
+    'startedFollowingYou' => 'byrjaði að fylgjast með þér.',
+    'commented' => 'gerði athugasemd við færsluna þína.',
+    'mentionedYou' => 'minntist á þig.',
+    'shared' => 'deildi færslunni þinni.',
 
 ];
