4d65db9fc52fdf6c15baecf05e4763cae28ef4f9
Daniel Supernault <danielsupernault@gmail.com>
2026-09-17 03:00:09 -0600

    Fix ActivityPubDeliveryService

--- app/Services/ActivityPubDeliveryService.php
+++ app/Services/ActivityPubDeliveryService.php
@@ -374,7 +374,8 @@ class ActivityPubDeliveryService
                                 'body' => $payload,
                             ]);
                     }
-                }
+                },
+                self::concurrency()
             );
 
             $deliveriesByIndex = [];
@@ -484,6 +485,14 @@ class ActivityPubDeliveryService
         DeliveryHostService::recordSuccesses(array_keys($hostSuccesses));
 
         return $result;
+    }
+
+    private static function concurrency(): int
+    {
+        return max(
+            1,
+            (int) config('federation.activitypub.delivery.concurrency', 10)
+        );
     }
 
     /**
