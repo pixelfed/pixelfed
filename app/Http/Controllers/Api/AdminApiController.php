09c02881c4f7c20f320e0ad618a13a78a4450127
Your Name <you@example.com>
2026-09-25 15:51:41 +0930

    Stop inflating instance status_count with zero-status profiles

--- app/Http/Controllers/Api/AdminApiController.php
+++ app/Http/Controllers/Api/AdminApiController.php
@@ -803,7 +803,7 @@ class AdminApiController extends Controller
         $id = $request->input('id');
         $instance = Instance::findOrFail($id);
         $instance->user_count = Profile::whereDomain($instance->domain)->count();
-        $instance->status_count = Profile::whereDomain($instance->domain)->leftJoin('statuses', 'profiles.id', '=', 'statuses.profile_id')->count();
+        $instance->status_count = Profile::whereDomain($instance->domain)->leftJoin('statuses', 'profiles.id', '=', 'statuses.profile_id')->count('statuses.id');
         $instance->save();
 
         return new AdminInstance($instance);
